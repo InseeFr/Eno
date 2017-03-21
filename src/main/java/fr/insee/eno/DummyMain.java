5dc3b426110c090af56e063e25f1d3230494fc93
romaintailhurat <romain.tailhurat@gmail.com>
2017-03-21 11:00:15 +0100

    Adding a method to clean the temporary directory

--- src/main/java/fr/insee/eno/DummyMain.java
+++ src/main/java/fr/insee/eno/DummyMain.java
@@ -1,7 +1,9 @@
 package fr.insee.eno;
 
 import java.io.File;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,6 +19,7 @@ public class DummyMain {
 
 	public static void main(String[] args) {
 		logger.info("Starting generation program");
+		cleanTempDirectory();		
 		Injector injector = Guice.createInjector(new DDI2FRContext());
 		GenerationService service = injector.getInstance(GenerationService.class);
 		try {
@@ -27,6 +30,24 @@ public class DummyMain {
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 		}
+	}
+	
+	private static boolean cleanTempDirectory() {
+		logger.debug("Cleaning temp directory at " + Constants.TEMP_FOLDER);
+		File tempDir = new File(Constants.TEMP_FOLDER);
+		if(!tempDir.exists()) {
+			logger.debug("Temp dir does not exist.");
+			return false;
+		}
+		try {
+			FileUtils.forceMkdir(tempDir);
+			FileUtils.cleanDirectory(tempDir);
+			return true;
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+			return false;
+		}
+		
 	}
 
 }
