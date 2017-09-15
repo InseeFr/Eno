64b3013a4adbccf022447668475c3a4c2816095d
a-cordier <com.acordier@gmail.com>
2017-09-15 09:38:25 +0200

    Route saxon error to log4j logger

--- src/main/java/fr/insee/eno/transform/xsl/EnoErrorListener.java
+++ src/main/java/fr/insee/eno/transform/xsl/EnoErrorListener.java
@@ -1,4 +1,28 @@
 package fr.insee.eno.transform.xsl;
 
-public class EnoErrorListener {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.xml.transform.ErrorListener;
+import javax.xml.transform.TransformerException;
+
+public class EnoErrorListener implements ErrorListener {
+
+    private final static Logger logger = LogManager.getLogger(EnoErrorListener.class);
+
+    @Override
+    public void warning(TransformerException e) throws TransformerException {
+        logger.warn(e.getMessage(), e);
+    }
+
+    @Override
+    public void error(TransformerException e) throws TransformerException {
+        logger.error(e.getMessage(), e);
+    }
+
+    @Override
+    public void fatalError(TransformerException e) throws TransformerException {
+        logger.fatal(e.getMessage(), e);
+    }
+
 }
