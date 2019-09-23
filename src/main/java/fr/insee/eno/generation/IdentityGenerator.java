eef9cc33390ab4a8a0c17030a408aaff97fbaa36
Laurent Caouissin <laurent.caouissin@insee.fr>
2019-09-23 12:01:50 +0200

    Create Java transforamtion : DDI32ToDDI33
    - add path to xslSheet
    - add extension "ddi33.xml"
    - add dummy Test (file and java test)

--- src/main/java/fr/insee/eno/generation/IdentityGenerator.java
+++ src/main/java/fr/insee/eno/generation/IdentityGenerator.java
@@ -2,8 +2,11 @@ package fr.insee.eno.generation;
 
 import java.io.File;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import fr.insee.eno.Constants;
 
 public class IdentityGenerator implements Generator {
 
@@ -11,12 +14,15 @@ public class IdentityGenerator implements Generator {
 
 	@Override
 	public File generate(File finalInput, byte[] parameters, String surveyName) throws Exception {
-		logger.info("Indentity generation : START");
-		logger.info("The output file is the input file.");
-		return finalInput;
+		logger.info("Identity generation : START");
+		logger.info("Identity generation : simply copying input file in another file");
+		String outputForm = Constants.TEMP_FOLDER_PATH + "/" + surveyName + "/ddi33/out.xml";
+		FileUtils.copyFile(finalInput, new File(outputForm));
+		logger.info("Identity generation : END");
+		return new File(outputForm);
 	}
 
 	public String in2out() {
 		return "identity";
-	}
+	}	
 }
