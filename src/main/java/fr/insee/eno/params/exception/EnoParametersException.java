529af5bf80ef812a789f906ffe1547fb6d9852dc
Laurent Caouissin <laurent.caouissin@insee.fr>
2019-10-02 17:16:53 +0200

    Eno params : add Exception for ENO params

---
+++ src/main/java/fr/insee/eno/params/exception/EnoParametersException.java
@@ -0,0 +1,9 @@
+package fr.insee.eno.params.exception;
+public class EnoParametersException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public EnoParametersException(String message) {
+        super(message);
+    }
+}
