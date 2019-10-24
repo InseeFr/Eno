14c3ed879e5899f0494e7f69b9e742799a9b85b8
Laurent Caouissin <laurent.caouissin@insee.fr>
2019-10-24 15:02:33 +0200

    ddi2js : add test with xml file

--- src/test/java/fr/insee/eno/test/TestVTLPostProcessor.java
+++ src/test/java/fr/insee/eno/test/TestVTLPostProcessor.java
@@ -1,13 +1,23 @@
 package fr.insee.eno.test;
 
-import org.junit.Test;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-import fr.insee.eno.postprocessing.JSVTLParserPostprocessor;
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
+import org.junit.Test;
+import org.xmlunit.diff.Diff;
+
+import fr.insee.eno.Constants;
+import fr.insee.eno.postprocessing.JSVTLParserPostprocessor;
 
 public class TestVTLPostProcessor {
 
 	private JSVTLParserPostprocessor jsvtlParserPostprocessor = new JSVTLParserPostprocessor();
+	private XMLDiff xmlDiff = new XMLDiff();
 		
 	@Test
 	public void simpleTest() {
@@ -28,6 +38,41 @@ public class TestVTLPostProcessor {
 		Assert.assertEquals(expected2, jsvtlParserPostprocessor.parseToVTL(test2));
 		Assert.assertEquals(expected3, jsvtlParserPostprocessor.parseToVTL(test3));
 		Assert.assertEquals(expected4, jsvtlParserPostprocessor.parseToVTL(test4));
+	}
+	
+	@Test
+	public void simpleWithFileTest() {
+		try {
+			String basePath = "src/test/resources/vtl-parsing";
+			
+			Path outPath = Paths.get(Constants.TEMP_FOLDER_PATH + "/test-vtl.xml");
+			Files.deleteIfExists(outPath);
+			Path outputFilePath = Files.createFile(outPath);			
+			File in = new File(String.format("%s/in.xml", basePath));
+			File outPostProcessing = jsvtlParserPostprocessor.process(in, null, "test");
+			FileUtils.copyFile(outPostProcessing,outputFilePath.toFile());
+			FileUtils.forceDelete(outPostProcessing);
+			File expectedFile = new File(String.format("%s/out.xml", basePath));
+			Diff diff = xmlDiff.getDiff(outputFilePath.toFile(),expectedFile);
+			Assert.assertFalse(getDiffMessage(diff, basePath), diff.hasDifferences());
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			Assert.fail();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			Assert.fail();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			Assert.fail();
+		} 
+	}
+	
+	private String getDiffMessage(Diff diff, String path) {
+		return String.format("Transformed output for %s should match expected XML document:\n %s", path,
+				diff.toString());
 	}
 
 	
