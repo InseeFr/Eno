babf7d3bc90b511f955fdb8f6fe5b7316ef87269
Laurent Caouissin <laurent.caouissin@insee.fr>
2019-10-24 15:02:14 +0200

    ddi2js : improve vtl-parsing

--- src/main/java/fr/insee/eno/postprocessing/JSVTLParserPostprocessor.java
+++ src/main/java/fr/insee/eno/postprocessing/JSVTLParserPostprocessor.java
@@ -8,7 +8,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,8 +26,9 @@ public class JSVTLParserPostprocessor implements Postprocessor {
 	@Override
 	public File process(File input, byte[] parameters, String surveyName) throws Exception {
 
-		File outputCustomFOFile = new File(
-				input.getPath().replace(Constants.EXTERNALIZE_VARIABLES_JS_EXTENSION, Constants.FINAL_JS_EXTENSION));
+		File outputCustomFOFile = new File(input.getParent(),
+				"form" +
+				Constants.FINAL_JS_EXTENSION);
 		logger.info("Start JS parsing xpath to vtl post-processing");
 
 		String inputString = FileUtils.readFileToString(input, StandardCharsets.UTF_8);
@@ -47,10 +47,11 @@ public class JSVTLParserPostprocessor implements Postprocessor {
 	public static final String XML_NODE_LABEL = "label";
 	public static final String XML_NODE_CONDITIONFILTER = "conditionFilter";
 	public static final String XML_NODE_VALUE = "value";
+	public static final String XML_NODE_EXPRESSION = "expression";
 	
 	public String fasterReplacement(String input) {
-		String possibleNodes = "("+XML_NODE_LABEL+"|"+XML_NODE_CONDITIONFILTER+"|"+XML_NODE_VALUE+")";
-		Pattern pattern = Pattern.compile("(<"+possibleNodes+">)(.*|\n)(</"+possibleNodes+">)");
+		String possibleNodes = "("+XML_NODE_LABEL+"|"+XML_NODE_CONDITIONFILTER+"|"+XML_NODE_VALUE+"|"+XML_NODE_EXPRESSION+")";
+		Pattern pattern = Pattern.compile("(<"+possibleNodes+">)((.|\\s)*?)(</"+possibleNodes+">)");
 		
 		Matcher matcher = pattern.matcher(input);
 		StringBuffer stringBuffer = new StringBuffer();
@@ -59,12 +60,13 @@ public class JSVTLParserPostprocessor implements Postprocessor {
 			//matcher.group(1)=start of xml node ex:<label>
 			//matcher.group(2)=name of xml node ex:label
 			//matcher.group(3)=content of xml node
-			//matcher.group(4)=end of xml node ex:</label>
-			//matcher.group(5)=name of xml node ex:label
+			//matcher.group(4)=last char in group(3) ?
+			//matcher.group(5)=end of xml node ex:</label>
+			//matcher.group(6)=name of xml node ex:label
 			matcher.appendReplacement(stringBuffer,
 					matcher.group(1)
 				  + parseToVTL(matcher.group(3))
-				  + matcher.group(4));
+				  + matcher.group(5));
 		}
 		matcher.appendTail(stringBuffer);
 		return stringBuffer.toString();
@@ -74,9 +76,9 @@ public class JSVTLParserPostprocessor implements Postprocessor {
 
 	public String parseToVTL(String input) {
 		input = input.replaceAll("!=", " &lt;&gt; ");
-		String finalString="";
-		List<String> list = new ArrayList<String>();
-		String context="";
+		String finalString="";String context="";
+		List<String> listContext = new ArrayList<String>();
+		boolean isBetweenRealQuote=false;
 		for(int i=0;i<input.length();i++) {
 			char c = input.charAt(i);
 			context = (c==',') ? "" : context+c;
@@ -85,24 +87,33 @@ public class JSVTLParserPostprocessor implements Postprocessor {
 				context = context.replace("(", "");
 				if(context.contains(CONCAT_FUNCTION)) {
 					finalString = finalString.replaceFirst(CONCAT_FUNCTION, "");
-					list.add(CONCAT_FUNCTION);
+					listContext.add(CONCAT_FUNCTION);
 				}
 				else if(context.contains(SUBSTRING_FUNCTION)) {
 					finalString = finalString.replaceFirst(SUBSTRING_FUNCTION, "substr")+c;
-					list.add(SUBSTRING_FUNCTION);
+					listContext.add(SUBSTRING_FUNCTION);
 				}
 				else {
 					finalString+=c;
-					list.add(context);
+					listContext.add(context);
 				}
 				context="";
 				break;
+			case '\"':
+				if(getLastChar(finalString)!='\\') {
+					isBetweenRealQuote=!isBetweenRealQuote;
+				}				
+				finalString+=c;
+				break;
+			case '\\':
+				finalString+="\\\\";
+				break;
 			case ',':
-				finalString += getContext(list).equals(CONCAT_FUNCTION) ? " || " : c;
+				finalString += getLastElement(listContext).equals(CONCAT_FUNCTION) && !isBetweenRealQuote ? " || " : c;
 				break;
 			case ')':
-				finalString += getContext(list).equals(CONCAT_FUNCTION) ? "" : c;
-				removeLast(list);
+				finalString += getLastElement(listContext).equals(CONCAT_FUNCTION) ? "" : c;
+				removeLast(listContext);
 				context="";
 				break;
 			default:
@@ -113,7 +124,11 @@ public class JSVTLParserPostprocessor implements Postprocessor {
 		return finalString;
 	}
 
-	public String getContext(List<String> contexts) {
+	public char getLastChar(String text) {
+		return text.isEmpty() ? 0: text.charAt(text.length()-1);
+	}
+	
+	public String getLastElement(List<String> contexts) {
 		return contexts.isEmpty() ?"": contexts.get(contexts.size()-1);
 	}
 
