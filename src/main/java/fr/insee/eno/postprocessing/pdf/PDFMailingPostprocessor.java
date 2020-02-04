5da36136a245eda93f6adc56f9c703aaeab3ccdf
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-02-04 11:13:28 +0100

    Update PDFMailingPostprocessor.java
    - close input and outputStream

--- src/main/java/fr/insee/eno/postprocessing/pdf/PDFMailingPostprocessor.java
+++ src/main/java/fr/insee/eno/postprocessing/pdf/PDFMailingPostprocessor.java
@@ -2,6 +2,7 @@ package fr.insee.eno.postprocessing.pdf;
 
 import java.io.File;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
@@ -28,18 +29,23 @@ public class PDFMailingPostprocessor implements Postprocessor {
 		File outputForFOFile = new File(input.getParent(),
 				Constants.BASE_NAME_FORM_FILE +
 				Constants.MAILING_FO_EXTENSION);
-		logger.debug("Output folder for basic-form : " + outputForFOFile.getAbsolutePath());
+		logger.debug("Output folder for basic-form : " + outputForFOFile.getAbsolutePath());	
+		
 
 		InputStream FO_XSL = Constants.getInputStreamFromPath(Constants.TRANSFORMATIONS_CUSTOMIZATION_FO_4PDF_2);
+		
+		InputStream inputStream = FileUtils.openInputStream(input);
+		OutputStream outputStream = FileUtils.openOutputStream(outputForFOFile);
 		try {
-			saxonService.transformFOToStep1FO(FileUtils.openInputStream(input),
-					FileUtils.openOutputStream(outputForFOFile), FO_XSL);
+			saxonService.transformFOToStep1FO(inputStream, outputStream, FO_XSL);
 		}catch(Exception e) {
 			String errorMessage = "An error was occured during the " + toString() + " transformation. "+e.getMessage();
 			logger.error(errorMessage);
 			throw new EnoGenerationException(errorMessage);
 		}
 		
+		inputStream.close();
+		outputStream.close();
 		FO_XSL.close();
 		logger.info("End of Mailing post-processing : ");
 
