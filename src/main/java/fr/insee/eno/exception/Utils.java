9442100fe4f596a5bb3efbefa92cacef918f6e89
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-11-06 12:04:26 +0100

    Improve utils function for xslt exception

--- src/main/java/fr/insee/eno/exception/Utils.java
+++ src/main/java/fr/insee/eno/exception/Utils.java
@@ -7,9 +7,8 @@ public class Utils {
     public static String getErrorLocation(String styleSheet, Exception e){
         String errorMessage="";
         try{
-            int line = ((XPathException) e).getLocator().getLineNumber();
-            int column = ((XPathException) e).getLocator().getColumnNumber();
-            errorMessage += String.format("Error in :%s [line :%d - column:%d]", styleSheet,line,column);
+            String location = ((XPathException) e).getLocationAsString();
+            errorMessage += String.format("Error in :%s %s", styleSheet, location);
         } catch (Exception exception){
         }
         return errorMessage;
