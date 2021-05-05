1f20b284ff43c1650540f422dcad1e8443d76357
Athemane Dahmouh <athemane.dahmouh@insee.fr>
2021-05-05 13:35:44 +0200

    Modification of simple ddi2out tests to include multimodal-selection pre-processing

--- src/test/java/fr/insee/eno/test/TestDDI2LunaticXML.java
+++ src/test/java/fr/insee/eno/test/TestDDI2LunaticXML.java
@@ -15,6 +15,7 @@ import fr.insee.eno.postprocessing.Postprocessor;
 import fr.insee.eno.service.GenerationService;
 import fr.insee.eno.preprocessing.DDICleaningPreprocessor;
 import fr.insee.eno.preprocessing.DDIDereferencingPreprocessor;
+import fr.insee.eno.preprocessing.DDIMultimodalSelectionPreprocessor;
 import fr.insee.eno.preprocessing.DDITitlingPreprocessor;
 import fr.insee.eno.preprocessing.Preprocessor;
 
@@ -29,6 +30,7 @@ public class TestDDI2LunaticXML {
 		try {
 			String basePath = "src/test/resources/ddi-to-lunatic-xml";
 			Preprocessor[] preprocessors = {
+					new DDIMultimodalSelectionPreprocessor(),
 					new DDIDereferencingPreprocessor(),
 					new DDICleaningPreprocessor(),
 					new DDITitlingPreprocessor()};
