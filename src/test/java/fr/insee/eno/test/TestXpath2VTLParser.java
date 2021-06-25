3606009f25a1bbdc4f9df58af2a18ebfa9b16bb8
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2021-06-25 08:43:03 +0200

    Refactor tests for Xpath2VTL parser to be more readable

--- src/test/java/fr/insee/eno/test/TestXpath2VTLParser.java
+++ src/test/java/fr/insee/eno/test/TestXpath2VTLParser.java
@@ -1,120 +1,295 @@
 package fr.insee.eno.test;
 
-import org.junit.jupiter.api.Assertions;
+import fr.insee.eno.utils.Xpath2VTLParser;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import fr.insee.eno.utils.Xpath2VTLParser;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestXpath2VTLParser {
-	
-	// concat
-	private String test1 = "concat(x,y,z, \"concat(a,b,c)\")";
-	private String expected1 = "x || y || z ||  \"concat(a,b,c)\"";		
 
-	private String test2 = "substring(x,y,z)";
-	private String expected2 = "substr(x,y,z)";
-	
-	// different
-	private String test3 = "x!=y";
-	private String expected3 = "x &lt;&gt; y"; // x <> y
+	@Test
+	@DisplayName("concat(x,y,z, \"concat(a,b,c)\")")
+	public void parseToVTLTest_concat() {
+		String xpathExpression = "concat(x,y,z, \"concat(a,b,c)\")";
+		String vtlExpected = "x || y || z ||  \"concat(a,b,c)\"";
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
 
-	// division
-	private String test4 = "x div y";
-	private String expected4 = "x / y";
-	
-	private String test5 = "x != '1'";
-	private String expected5 ="x  &lt;&gt;  \"1\"";
-	
-	private String test6 = "\"x != '1'\"";
-	private String expected6 ="\"x != '1'\"";
-	
-	//substring - concat
-	private String test7 = "concat(substring(x,y,z),a,concat(x,substring(x1,y1,z1)),b)!='abc'";
-	private String expected7 = "substr(x,y,z) || a || x || substr(x1,y1,z1) || b &lt;&gt; \"abc\"";
 
-	private String test8 = "cast(cast(ABCD,integer),string) = '3'";
-	private String expected8 = "cast(cast(ABCD,integer),string) = \"3\"";
-	
-	private String test9 = "cast(cast(ABCD,string),integer) = '3'";
-	private String expected9 = "cast(cast(ABCD,string),integer) = 3";
+	@Test
+	@DisplayName("substring(x,y,z)")
+	public void parseToVTLTest_substr() {
+		String xpathExpression = "substring(x,y,z)";
+		String vtlExpected = "substr(x,y,z)";
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
 
-	private String test10 = "cast(cast(ABCD,string),integer) = null";
-	private String expected10 = "isnull(cast(cast(ABCD,string),integer))";
+	@Test
+	@DisplayName("x!=y")
+	public void parseToVTLTest_different() {
+		String xpathExpression = "x!=y";
+		String vtlExpected = "x &lt;&gt; y"; // x <> y
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
 
-	// modulo
-	private String test11 = "10 mod 2";
-	private String test11bis = "10mod2";
-	private String expected11 = "mod(10,2)";
-	private String test20 ="kiki $VAR1$ mod $VAR2$ lolo";
-	private String expected20 ="kiki mod($VAR1$,$VAR2$) lolo";
-	
-	// current date
-	private String test12 = "current-date()";
-	private String expected12 = "current_date()";
+	@Test
+	@DisplayName("x div y")
+	public void parseToVTLTest_division() {
+		String xpathExpression = "x div y";
+		String vtlExpected = "x / y";
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
 
-	// string length
-	private String test13 = "string-length()";
-	private String expected13 = "length()";
+	@Test
+	@DisplayName("x != '1'")
+	public void parseToVTLTest_different_litteral() {
+		String xpathExpression = "x != '1'";
+		String vtlExpected ="x  &lt;&gt;  \"1\"";
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
 
-	// inclusion and not inclusion
-	private String expected14 = "cast(VAR,number) in {\"1\", \"2\"}";
-	private String expected15 = "cast(VAR,number) not_in {\"1\", \"2\"}";
+	@Test
+	@DisplayName("concat(substring(x,y,z),a,concat(x,substring(x1,y1,z1)),b)!='abc'")
+	public void parseToVTLTest_substring_concat() {
+		String xpathExpression = "concat(substring(x,y,z),a,concat(x,substring(x1,y1,z1)),b)!='abc'";
+		String vtlExpected = "substr(x,y,z) || a || x || substr(x1,y1,z1) || b &lt;&gt; \"abc\"";
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
+
+	@Test
+	@DisplayName("cast(cast(ABCD,integer),string) = '3'")
+	public void parseToVTLTest_cast_string() {
+		String xpathExpression = "cast(cast(ABCD,integer),string) = '3'";
+		String vtlExpected = "cast(cast(ABCD,integer),string) = \"3\"";
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
+
+	@Test
+	@DisplayName("cast(cast(ABCD,string),integer) = '3'")
+	public void parseToVTLTest_cast_integer() {
+		String xpathExpression = "cast(cast(ABCD,string),integer) = '3'";
+		String vtlExpected = "cast(cast(ABCD,string),integer) = 3";
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
+
+	@Test
+	@DisplayName("cast(cast(ABCD,string),integer) = null")
+	public void parseToVTLTest_cast_null() {
+		String xpathExpression = "cast(cast(ABCD,string),integer) = null";
+		String vtlExpected = "isnull(cast(cast(ABCD,string),integer))";
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
+
+	@Test
+	@DisplayName("10mod2")
+	public void parseToVTLTest_mod() {
+		String xpathExpression = "10mod2";
+		String vtlExpected = "mod(10,2)";
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
+
+	@Test
+	@DisplayName("10 mod 2")
+	public void parseToVTLTest_mod_space() {
+		String xpathExpression = "10 mod 2";
+		String vtlExpected = "mod(10,2)";
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
+
+	@Test
+	@DisplayName("kiki $VAR1$ mod $VAR2$ lolo")
+	public void parseToVTLTest_mod_dollar() {
+		String xpathExpression ="kiki $VAR1$ mod $VAR2$ lolo";
+		String vtlExpected ="kiki mod($VAR1$,$VAR2$) lolo";
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
+
+
+	@Test
+	@DisplayName("current-date()")
+	public void parseToVTLTest_current_date() {
+		String xpathExpression = "current-date()";
+		String vtlExpected = "current_date()";
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
+
+	@Test
+	@DisplayName("string-length()")
+	public void parseToVTLTest_length() {
+		String xpathExpression = "string-length()";
+		String vtlExpected= "length()";
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
+
+	@Test
+	@DisplayName("cast(VAR,number) in {\"1\", \"2\"}")
+	public void parseToVTLTest_in() {
+		String xpathExpression = "cast(VAR,number) in {\"1\", \"2\"}";
+		String vtlExpected = "cast(VAR,number) in {\"1\", \"2\"}";
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
+
+	@Test
+	@DisplayName("cast(VAR,number) not_in {\"1\", \"2\"}")
+	public void parseToVTLTest_not_in() {
+		String xpathExpression = "cast(VAR,number) not_in {\"1\", \"2\"}";
+		String vtlExpected = "cast(VAR,number) not_in {\"1\", \"2\"}";
+		assertEquals(vtlExpected, Xpath2VTLParser.parseToVTL(xpathExpression));
+	}
+
 
 	// null condition
-	//private String test16 = "if ($SEXE$= \"2\") then \"elle\" else \"il\"";
-	private String expected16 = "if isnull($SEXE$) then \"il\" else if ($SEXE$= \"2\") then \"elle\" else \"il\"";	
+	// String xpathExpression16 = "if ($SEXE$= \"2\") then \"elle\" else \"il\"";
+	 //String vtlExpected = "if isnull($SEXE$) then \"il\" else if ($SEXE$= \"2\") then \"elle\" else \"il\"";
 
 	// conversion d’une date AAAA-MM-JJ en nombre
-    private String expected17 = "cast(substr(cast($DATENAIS$,string),1,4)||substr(cast($DATENAIS$,string),6,2)||substr(cast($DATENAIS$,string),9,2),number)";
 
-	// âge en différence de millésime ($AGEMILLESIME$)
-	private String expected18 = "cast(substr(cast(current_date(),string),1,4),number)  - cast(substr(cast($DATENAIS$,string),1,4),number)";
-
-	// Booléen anniversaire à venir dans l’année ($FUTURANNIVERSAIRE$)
-	private String expected19 = "cast(substr(cast(current_date(),string),6,2) || substr(cast(current_date(),string),9,2),number) < cast(substr(cast($DATENAIS$,string),6,2) || substr(cast($DATENAIS$,string),9,2),number)";
 
 	@Test
-	public void simpleTestXpath2VTL() {
-		Assertions.assertEquals(expected1, Xpath2VTLParser.parseToVTL(test1));
-		Assertions.assertEquals(expected2, Xpath2VTLParser.parseToVTL(test2));
-		Assertions.assertEquals(expected3, Xpath2VTLParser.parseToVTL(test3));
-		Assertions.assertEquals(expected4, Xpath2VTLParser.parseToVTL(test4));
-		Assertions.assertEquals(expected5, Xpath2VTLParser.parseToVTL(test5));
-		Assertions.assertEquals(expected6, Xpath2VTLParser.parseToVTL(test6));
-		Assertions.assertEquals(expected7, Xpath2VTLParser.parseToVTL(test7));
-		Assertions.assertEquals(expected8, Xpath2VTLParser.parseToVTL(test8));
-		Assertions.assertEquals(expected9, Xpath2VTLParser.parseToVTL(test9));
-		Assertions.assertEquals(expected10, Xpath2VTLParser.parseToVTL(test10));
-		Assertions.assertEquals(expected11, Xpath2VTLParser.parseToVTL(test11));
-		Assertions.assertEquals(expected11, Xpath2VTLParser.parseToVTL(test11bis));
-		Assertions.assertEquals(expected12, Xpath2VTLParser.parseToVTL(test12));
-		Assertions.assertEquals(expected13, Xpath2VTLParser.parseToVTL(test13));
-		Assertions.assertEquals(expected20, Xpath2VTLParser.parseToVTL(test20));
+	@DisplayName("x || y || z ||  \"concat(a,b,c)\"")
+	public void parseToVTLTest_vtlIsUnchanged_concat() {
 
+		String vtl = "x || y || z ||  \"concat(a,b,c)\"";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
 	}
+
+
 	@Test
-	public void simpleTestVTL2VTL() {
-		Assertions.assertEquals(expected1, Xpath2VTLParser.parseToVTL(expected1));
-		Assertions.assertEquals(expected2, Xpath2VTLParser.parseToVTL(expected2));
-		Assertions.assertEquals(expected3, Xpath2VTLParser.parseToVTL(expected3));
-		Assertions.assertEquals(expected4, Xpath2VTLParser.parseToVTL(expected4));
-		Assertions.assertEquals(expected5, Xpath2VTLParser.parseToVTL(expected5));
-		Assertions.assertEquals(expected6, Xpath2VTLParser.parseToVTL(expected6));
-		Assertions.assertEquals(expected7, Xpath2VTLParser.parseToVTL(expected7));
-		Assertions.assertEquals(expected8, Xpath2VTLParser.parseToVTL(expected8));
-		Assertions.assertEquals(expected9, Xpath2VTLParser.parseToVTL(expected9));
-		Assertions.assertEquals(expected10, Xpath2VTLParser.parseToVTL(expected10));
-		Assertions.assertEquals(expected11, Xpath2VTLParser.parseToVTL(expected11));
-		Assertions.assertEquals(expected12, Xpath2VTLParser.parseToVTL(expected12));
-		Assertions.assertEquals(expected13, Xpath2VTLParser.parseToVTL(expected13));
-		Assertions.assertEquals(expected14, Xpath2VTLParser.parseToVTL(expected14));
-		Assertions.assertEquals(expected15, Xpath2VTLParser.parseToVTL(expected15));
-		Assertions.assertEquals(expected16, Xpath2VTLParser.parseToVTL(expected16));
-		Assertions.assertEquals(expected17, Xpath2VTLParser.parseToVTL(expected17));	
-		Assertions.assertEquals(expected18, Xpath2VTLParser.parseToVTL(expected18));
-		Assertions.assertEquals(expected19, Xpath2VTLParser.parseToVTL(expected19));
-		Assertions.assertEquals(expected20, Xpath2VTLParser.parseToVTL(expected20));
+	@DisplayName("substr(x,y,z)")
+	public void parseToVTLTest_vtlIsUnchanged_substr() {
+
+		String vtl = "substr(x,y,z)";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
 	}
+
+	@Test
+	@DisplayName("x &lt;&gt; y")
+	public void parseToVTLTest_vtlIsUnchanged_different() {
+
+		String vtl = "x &lt;&gt; y"; // x <> y
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
+	@Test
+	@DisplayName("x / y")
+	public void parseToVTLTest_vtlIsUnchanged_division() {
+
+		String vtl = "x / y";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
+	@Test
+	@DisplayName("x  &lt;&gt;  \"1\"")
+	public void parseToVTLTest_vtlIsUnchanged_different_litteral() {
+
+		String vtl ="x  &lt;&gt;  \"1\"";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
+	@Test
+	@DisplayName("substr(x,y,z) || a || x || substr(x1,y1,z1) || b &lt;&gt; \"abc\"")
+	public void parseToVTLTest_vtlIsUnchanged_substring_concat() {
+
+		String vtl = "substr(x,y,z) || a || x || substr(x1,y1,z1) || b &lt;&gt; \"abc\"";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
+	@Test
+	@DisplayName("cast(cast(ABCD,integer),string) = \"3\"")
+	public void parseToVTLTest_vtlIsUnchanged_cast_string() {
+
+		String vtl = "cast(cast(ABCD,integer),string) = \"3\"";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
+	@Test
+	@DisplayName("cast(cast(ABCD,string),integer) = 3")
+	public void parseToVTLTest_vtlIsUnchanged_cast_integer() {
+
+		String vtl = "cast(cast(ABCD,string),integer) = 3";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
+	@Test
+	@DisplayName("isnull(cast(cast(ABCD,string),integer))")
+	public void parseToVTLTest_vtlIsUnchanged_cast_null() {
+
+		String vtl = "isnull(cast(cast(ABCD,string),integer))";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
+
+	@Test
+	@DisplayName("mod(10,2)")
+	public void parseToVTLTest_vtlIsUnchanged_mod() {
+
+		String vtl = "mod(10,2)";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
+	@Test
+	@DisplayName("kiki $VAR1$ mod $VAR2$ lolo")
+	public void parseToVTLTest_vtlIsUnchanged_mode_dollar() {
+
+		String vtl ="kiki mod($VAR1$,$VAR2$) lolo";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
+
+	@Test
+	@DisplayName("current_date()")
+	public void parseToVTLTest_vtlIsUnchanged_current_date() {
+
+		String vtl = "current_date()";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
+	@Test
+	@DisplayName("length()")
+	public void parseToVTLTest_vtlIsUnchanged_length() {
+
+		String vtl= "length()";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
+	@Test
+	@DisplayName("cast(VAR,number) in {\"1\", \"2\"}")
+	public void parseToVTLTest_vtlIsUnchanged_in() {
+
+		String vtl = "cast(VAR,number) in {\"1\", \"2\"}";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
+	@Test
+	@DisplayName("cast(VAR,number) not_in {\"1\", \"2\"}")
+	public void parseToVTLTest_vtlIsUnchanged_not_in() {
+
+		String vtl = "cast(VAR,number) not_in {\"1\", \"2\"}";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
+	@Test
+	@DisplayName("cast(substr(cast($DATENAIS$,string),1,4)||substr(cast($DATENAIS$,string),6,2)||substr(cast($DATENAIS$,string),9,2),number)")
+	public void parseToVTLTest_vtlIsUnchanged_date() {
+		String vtl = "cast(substr(cast($DATENAIS$,string),1,4)||substr(cast($DATENAIS$,string),6,2)||substr(cast($DATENAIS$,string),9,2),number)";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
+	@Test
+	@DisplayName("cast(substr(cast(current_date(),string),1,4),number)  - cast(substr(cast($DATENAIS$,string),1,4),number)")
+	public void parseToVTLTest_vtlIsUnchanged_age() {
+		String vtl = "cast(substr(cast(current_date(),string),1,4),number)  - cast(substr(cast($DATENAIS$,string),1,4),number)";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
+	@Test
+	@DisplayName("cast(substr(cast(current_date(),string),6,2) || substr(cast(current_date(),string),9,2),number) < cast(substr(cast($DATENAIS$,string),6,2) || substr(cast($DATENAIS$,string),9,2),number)")
+	public void parseToVTLTest_vtlIsUnchanged_birthday() {
+		String vtl = "cast(substr(cast(current_date(),string),6,2) || substr(cast(current_date(),string),9,2),number) < cast(substr(cast($DATENAIS$,string),6,2) || substr(cast($DATENAIS$,string),9,2),number)";
+		assertEquals(vtl, Xpath2VTLParser.parseToVTL(vtl));
+	}
+
 	
 
 	
