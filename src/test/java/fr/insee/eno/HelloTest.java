37b86da8d0b495474b3c7f3f5f359a4d0d33d043
nsenave <nicolas.senave@gmail.com>
2022-03-14 13:02:53 +0100

    hello

---
+++ src/test/java/fr/insee/eno/HelloTest.java
@@ -0,0 +1,13 @@
+package fr.insee.eno;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloTest {
+
+    @Test
+    public void hello() {
+        assertEquals(1,1);
+    }
+}
