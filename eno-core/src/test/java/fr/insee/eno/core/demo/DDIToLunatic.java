33c3add1876db9862109a8e4ad076f081ff6867f
nsenave <nicolas.senave@gmail.com>
2022-06-03 15:56:18 +0200

    ddi to lunatic test feature

---
+++ eno-core/src/test/java/fr/insee/eno/core/demo/DDIToLunatic.java
@@ -0,0 +1,45 @@
+package fr.insee.eno.core.demo;
+
+import fr.insee.eno.core.mappers.DDIMapper;
+import fr.insee.eno.core.mappers.LunaticMapper;
+import fr.insee.eno.core.model.EnoQuestionnaire;
+import fr.insee.eno.core.parsers.DDIParser;
+import fr.insee.eno.core.writers.LunaticWriter;
+import fr.insee.lunatic.model.flat.Questionnaire;
+import instance33.DDIInstanceDocument;
+import org.apache.xmlbeans.XmlException;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class DDIToLunatic {
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "l10xmg2l",
+            "questionnaire-avec-filtre-eno-java",
+    })
+    public void writeJsonLunaticFromDDI(String fileName) throws XmlException, IOException, JAXBException {
+        //
+        DDIInstanceDocument ddiInstanceDocument = DDIParser.parse(
+                this.getClass().getClassLoader().getResourceAsStream("in/ddi/" + fileName + ".xml"));
+        //
+        EnoQuestionnaire enoQuestionnaire = new EnoQuestionnaire();
+        //
+        DDIMapper ddiMapper = new DDIMapper(ddiInstanceDocument);
+        ddiMapper.mapDDI(enoQuestionnaire);
+
+        //
+        Questionnaire lunaticQuestionnaire = new Questionnaire();
+        //
+        LunaticMapper.map(enoQuestionnaire, lunaticQuestionnaire);
+
+        //
+        LunaticWriter.writeJsonQuestionnaire(lunaticQuestionnaire,
+               Path.of("src/test/resources/out/lunatic/" + fileName + ".json"));
+
+    }
+}
