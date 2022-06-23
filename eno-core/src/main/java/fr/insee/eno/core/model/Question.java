7f12470437d9bffa5c87bd12d62faa5e0eb2ff3b
nsenave <nicolas.senave@gmail.com>
2022-06-23 17:38:21 +0200

    map question label from DDI

--- eno-core/src/main/java/fr/insee/eno/core/model/Question.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/Question.java
@@ -9,9 +9,16 @@ import lombok.Setter;
 @Setter
 public class Question {
 
+    @DDI(contextType = QuestionItemType.class, field = "getIDArray(0).getStringValue()")
+    String id;
+
     @DDI(contextType = QuestionItemType.class,
             field = "getQuestionItemNameArray(0).getStringArray(0).getStringValue()")
     String name;
+
+    @DDI(contextType = QuestionItemType.class,
+            field = "getQuestionTextArray(0).getTextContentArray(0).getText().getStringValue()") //TODO: unsafe superclass method call
+    String label;
 
     QuestionType type;
 
