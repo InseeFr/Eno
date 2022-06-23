dd8a5509216d98fe12f25d67d0b2e3e5fa583a45
nsenave <nicolas.senave@gmail.com>
2022-06-23 17:37:13 +0200

    Create Declaration.java

---
+++ eno-core/src/main/java/fr/insee/eno/core/model/Declaration.java
@@ -0,0 +1,24 @@
+package fr.insee.eno.core.model;
+
+import datacollection33.StatementItemType;
+import fr.insee.eno.core.annotations.DDI;
+import fr.insee.eno.core.annotations.Lunatic;
+import fr.insee.lunatic.model.flat.DeclarationType;
+import lombok.Getter;
+import lombok.Setter;
+
+/** Text displayed before a question or sequence. */
+@Getter
+@Setter
+public class Declaration {
+
+    //@DDI(contextType = StatementItemType.class, field = "") //TODO
+    String id;
+
+    String label;
+
+    @Lunatic(contextType = DeclarationType.class,
+            field = "setDeclarationType(T(fr.insee.lunatic.model.flat.DeclarationPositionEnum).valueOf(#param))")
+    String position = "BEFORE_QUESTION_TEXT";
+
+}
