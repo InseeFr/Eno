131248b5db6961691b82243ad527268686b6df09
nsenave <nicolas.senave@gmail.com>
2022-06-23 17:37:01 +0200

    Instruction (mapped between DDI and Lunatic)

---
+++ eno-core/src/main/java/fr/insee/eno/core/model/Instruction.java
@@ -0,0 +1,41 @@
+package fr.insee.eno.core.model;
+
+import datacollection33.InstructionType;
+import fr.insee.eno.core.annotations.DDI;
+import fr.insee.eno.core.annotations.Lunatic;
+import fr.insee.lunatic.model.flat.DeclarationType;
+import lombok.Getter;
+import lombok.Setter;
+
+/** Text displayed after a question or sequence. */
+@Getter
+@Setter
+public class Instruction {
+
+    @DDI(contextType = InstructionType.class,
+            field = "getIDArray(0).getStringValue()")
+    @Lunatic(contextType = DeclarationType.class, field = "setId(#param)")
+    String id;
+    // TODO: why does Lunatic concatenate sequence id and declaration id ?
+
+    /**
+     * In DDI, the declaration type (instruction / help / warning / ...) is written in the first
+     * InstructionName element.
+     * Warning: the value in DDI is lower case.
+     */
+    @DDI(contextType = InstructionType.class,
+            field = "getInstructionNameArray(0).getStringArray(0).getStringValue()")
+    @Lunatic(contextType = DeclarationType.class,
+            field = "setDeclarationType(T(fr.insee.lunatic.model.flat.DeclarationTypeEnum).valueOf(#param.toUpperCase()))")
+    String declarationType;
+
+    @DDI(contextType = InstructionType.class,
+            field = "getInstructionTextArray(0).getTextContentArray(0).getText().getStringValue()") //TODO: unsafe superclass method call
+    @Lunatic(contextType = DeclarationType.class, field = "setLabel(#param)")
+    String label;
+
+    @Lunatic(contextType = DeclarationType.class,
+            field = "setPosition(T(fr.insee.lunatic.model.flat.DeclarationPositionEnum).valueOf(#param))")
+    String position = "AFTER_QUESTION_TEXT";
+
+}
