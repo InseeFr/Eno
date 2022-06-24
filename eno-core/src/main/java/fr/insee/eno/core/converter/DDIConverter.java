761ad0ce517290abda0cf863c07bc8fc30bb3c88
nsenave <nicolas.senave@gmail.com>
2022-06-24 17:20:52 +0200

    create DDI converter for complex types

---
+++ eno-core/src/main/java/fr/insee/eno/core/converter/DDIConverter.java
@@ -0,0 +1,15 @@
+package fr.insee.eno.core.converter;
+
+import datacollection33.QuestionItemType;
+import fr.insee.eno.core.model.QuestionType;
+
+public class DDIConverter {
+
+    /**
+     * Return the question type corresponding to the given DDI question object.
+     * @return A Eno model QuestionType.
+     */
+    public QuestionType questionType(QuestionItemType questionItemType) {
+        return null;
+    }
+}
