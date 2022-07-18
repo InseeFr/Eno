4492afb9208b74da0739a94acbb102c4b6a6ad2d
nsenave <nicolas.senave@gmail.com>
2022-07-18 14:16:06 +0200

    map date question ddi lunatic

--- eno-core/src/main/java/fr/insee/eno/core/model/question/DateQuestion.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/question/DateQuestion.java
@@ -1,4 +1,25 @@
 package fr.insee.eno.core.model.question;
 
+import datacollection33.QuestionItemType;
+import fr.insee.eno.core.annotations.DDI;
+import fr.insee.eno.core.annotations.Lunatic;
+import fr.insee.lunatic.model.flat.Datepicker;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DateQuestion extends SingleResponseQuestion {
+
+    @DDI(contextType = QuestionItemType.class, field = "getResponseDomain().getRangeArray(0).getMinimumValue().getStringValue()")
+    @Lunatic(contextType = Datepicker.class, field = "setMin(#param)")
+    private String minValue;
+
+    @DDI(contextType = QuestionItemType.class, field = "getResponseDomain().getRangeArray(0).getMaximumValue().getStringValue()")
+    @Lunatic(contextType = Datepicker.class, field = "setMax(#param)")
+    private String maxValue;
+
+    @DDI(contextType = QuestionItemType.class, field = "getResponseDomain().getDateFieldFormat().getStringValue()")
+    @Lunatic(contextType = Datepicker.class, field = "setDateFormat(#param)")
+    private String format;
 }
