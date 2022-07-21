9ae91514c2b10b234329a037e370eddb5d29aa8f
nsenave <nicolas.senave@gmail.com>
2022-07-21 10:06:13 +0200

    create abstract EnoObject

---
+++ eno-core/src/main/java/fr/insee/eno/core/model/EnoObject.java
@@ -0,0 +1,16 @@
+package fr.insee.eno.core.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+public abstract class EnoObject {
+
+    @Getter
+    @Setter
+    @JsonIgnore
+    private EnoObject parent;
+
+    //TODO: id mapping might be refactored here
+
+}
