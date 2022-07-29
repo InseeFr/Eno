4f49c4f9738aeb59aad054f39c322e0cb5468cf6
nsenave <nicolas.senave@gmail.com>
2022-07-29 16:49:33 +0200

    map Declaration into Lunatic

--- eno-core/src/main/java/fr/insee/eno/core/model/Declaration.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/Declaration.java
@@ -13,15 +13,20 @@ import lombok.Setter;
 public class Declaration extends EnoObject {
 
     @DDI(contextType = StatementItemType.class, field = "getIDArray(0).getStringValue()")
+    @Lunatic(contextType = DeclarationType.class, field = "setId(#param)")
     String id;
 
     @DDI(contextType = StatementItemType.class,
             field = "getDisplayTextArray(0).getTextContentArray(0).getText().getStringValue()")
+    @Lunatic(contextType = DeclarationType.class, field = "setLabel(#param)")
     String label;
 
     @Lunatic(contextType = DeclarationType.class,
-            field = "setDeclarationType(T(fr.insee.lunatic.model.flat.DeclarationPositionEnum).valueOf(#param))")
+            field = "setDeclarationType(T(fr.insee.lunatic.model.flat.DeclarationTypeEnum).valueOf(#param))")
     String declarationType = "STATEMENT";
+
+    @Lunatic(contextType = DeclarationType.class,
+            field = "setPosition(T(fr.insee.lunatic.model.flat.DeclarationPositionEnum).valueOf(#param))")
     String position = "BEFORE_QUESTION_TEXT";
 
 }
