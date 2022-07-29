0337ddd70c42bbf79a968a87aa59d5c187ad37ef
nsenave <nicolas.senave@gmail.com>
2022-07-29 16:51:52 +0200

    improve declarationType mapping + javadoc

--- eno-core/src/main/java/fr/insee/eno/core/model/Instruction.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/Instruction.java
@@ -21,20 +21,23 @@ public class Instruction extends EnoObject {
     String id;
     // TODO: why does Lunatic concatenate sequence id and declaration id ? -> no concrete reason, ok
 
-    /**
-     * In DDI, the declaration type (instruction / help / warning / ...) is written in the first
-     * InstructionName element.
-     * TODO: warning: not intended to be the first element (even if it actually is in practice), the element to get is the one that does not correspond to an enum that concerns the collection mode.
-     * Warning: the value in DDI is lower case.
+    /** Type of instruction (either instruction / help / warning / ...)
+     * In DDI, this information is on the same level as the information on the modes concerned by the instruction.
+     * (In other words: concerned modes and instruction type are all "instruction name" elements.)
+     * To get the instruction type, the list of instruction names is filtered from the concerned modes,
+     * then there should be only remaining instruction name which the one we want.
+     * TODO: conversion between DDI and Lunatic using toUpperCase works, but is weak, dedicated static methods in the class would be better.
      */
     @DDI(contextType = InstructionType.class,
-            field = "getInstructionNameArray(0).getStringArray(0).getStringValue()")
+            field = "getInstructionNameList()" +
+                    ".?[T(fr.insee.eno.core.model.Mode).isDDIMode(#this.getStringArray(0).getStringValue())].get(0)" +
+                    ".getStringArray(0).getStringValue()")
     @Lunatic(contextType = DeclarationType.class,
             field = "setDeclarationType(T(fr.insee.lunatic.model.flat.DeclarationTypeEnum).valueOf(#param.toUpperCase()))")
     String declarationType;
 
     @DDI(contextType = InstructionType.class,
-            field = "getInstructionTextArray(0).getTextContentArray(0).getText().getStringValue()") //TODO: unsafe superclass method call
+            field = "getInstructionTextArray(0).getTextContentArray(0).getText().getStringValue()")
     @Lunatic(contextType = DeclarationType.class, field = "setLabel(#param)")
     String label;
 
