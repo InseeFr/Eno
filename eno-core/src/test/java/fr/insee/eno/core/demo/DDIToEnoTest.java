2b679c7946d87d358a96eda78468dc60da419853
nsenave <nicolas.senave@gmail.com>
2022-07-29 16:54:27 +0200

    add demo class for DDI to Eno model

---
+++ eno-core/src/test/java/fr/insee/eno/core/demo/DDIToEnoTest.java
@@ -0,0 +1,39 @@
+package fr.insee.eno.core.demo;
+
+import fr.insee.eno.core.mappers.DDIMapper;
+import fr.insee.eno.core.model.EnoQuestionnaire;
+import fr.insee.eno.core.parsers.DDIParser;
+import fr.insee.eno.core.processing.EnoProcessing;
+import fr.insee.eno.core.writers.EnoWriter;
+import instance33.DDIInstanceDocument;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class DDIToEnoTest {
+
+    @Test
+    public void writeEnoFileFromDDI() throws IOException {
+        //
+        String fileName = "l10xmg2l_qcm_obligatoires_instructions";
+        //String fileName = "controles";
+
+        //
+        DDIInstanceDocument ddiInstanceDocument = DDIParser.parse(
+                this.getClass().getClassLoader().getResource("in/ddi/" + fileName + ".xml"));
+        //
+        EnoQuestionnaire enoQuestionnaire = new EnoQuestionnaire();
+        //
+        DDIMapper ddiMapper = new DDIMapper(ddiInstanceDocument);
+        ddiMapper.mapDDI(enoQuestionnaire);
+
+        //
+        EnoProcessing enoProcessing = new EnoProcessing();
+        enoProcessing.applyProcessing(enoQuestionnaire);
+
+        //
+        EnoWriter.writeJson(enoQuestionnaire,
+                Path.of("src/test/resources/out/eno/" + fileName + ".json"));
+    }
+}
