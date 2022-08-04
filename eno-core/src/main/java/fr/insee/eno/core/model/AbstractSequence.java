20a741cd7cb667f475975e43f13e57b942f24386
nsenave <nicolas.senave@gmail.com>
2022-08-04 11:26:07 +0200

    javadoc for sequences

--- eno-core/src/main/java/fr/insee/eno/core/model/AbstractSequence.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/AbstractSequence.java
@@ -10,6 +10,9 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
+/** Abstract object for sequence and subsequence.
+ * In DDI, a sequence or subsequence is a SequenceType object.
+ * In Lunatic, a sequence is a SequenceType, a subsequence is a Subsequence object. */
 @Getter
 @Setter
 public abstract class AbstractSequence extends EnoObject {
@@ -18,21 +21,33 @@ public abstract class AbstractSequence extends EnoObject {
     @Lunatic(contextType = {fr.insee.lunatic.model.flat.SequenceType.class, Subsequence.class}, field = "setId(#param)")
     private String id;
 
+    /** Sequence / subsequence label. */
     @DDI(contextType = SequenceType.class, field = "getLabelArray(0).getContentArray(0).getStringValue()")
     @Lunatic(contextType = {fr.insee.lunatic.model.flat.SequenceType.class, Subsequence.class}, field = "setLabel(#param)")
     private String label;
 
+    /** Sequence / subsequence instructions.
+     * In DDI, the SequenceType object contains the list of references to the instructions.
+     * In Lunatic, instructions and declarations belongs to the same list. */
     @DDI(contextType = SequenceType.class,
             field = "getInterviewerInstructionReferenceList().![#index.get(#this.getIDArray(0).getStringValue())]")
     @Lunatic(contextType = {fr.insee.lunatic.model.flat.SequenceType.class, Subsequence.class}, field = "getDeclarations()")
     private final List<Instruction> instructions = new ArrayList<>();
 
+    /** Sequence / subsequence declarations.
+     * In DDI, the declarations are mapped in the questionnaire object, and are put here through a 'processing' class.
+     * In Lunatic, instructions and declarations belongs to the same list. */
     @Lunatic(contextType = {fr.insee.lunatic.model.flat.SequenceType.class, Subsequence.class}, field = "getDeclarations()")
     private final List<Declaration> declarations = new ArrayList<>();
 
+    /** Sequence / subsequence controls.
+     * In DDI, the controls are mapped in the questionnaire object, and are put here through a 'processing' class.
+     * In Lunatic, the sequence / subsequence object has a list of controls. */
     @Lunatic(contextType = {fr.insee.lunatic.model.flat.SequenceType.class, Subsequence.class}, field = "getControls()")
     private final List<Control> controls = new ArrayList<>();
 
+    /** Ordered list of references to all objects in the sequence / subsequence
+     * that corresponds to a subsequence (if any) or a question. */
     @DDI(contextType = SequenceType.class,
             field = "getControlConstructReferenceList()" +
                     ".?[#this.getTypeOfObject().toString() == 'QuestionConstruct'" +
@@ -50,6 +65,8 @@ public abstract class AbstractSequence extends EnoObject {
                     ": null]")
     private final List<String> componentReferences = new ArrayList<>();
 
+    /** Ordered list of all items in the sequence / subsequence.
+     * Note: the 'componentReference' attribute usages could be replaced with this one. */
     @DDI(contextType = SequenceType.class, field = "getControlConstructReferenceList()")
     private final List<SequenceItem> sequenceItems = new ArrayList<>();
 
--- eno-core/src/main/java/fr/insee/eno/core/model/Sequence.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/Sequence.java
@@ -4,10 +4,16 @@ import fr.insee.eno.core.annotations.Lunatic;
 import lombok.Getter;
 import lombok.Setter;
 
+/** Eno object for sequence.
+ * Note: a 'sequence' is sometimes called a 'module' in Eno.
+ * In DDI, a sequence is a SequenceType object that has the value 'module' in its 'type of sequence' field.
+ * In Lunatic, a sequence is a SequenceType object. */
 @Getter
 @Setter
 public class Sequence extends AbstractSequence {
 
+    /** Field specific to Lunatic.
+     * Note: maybe redundant with "type" field in Lunatic serialized documents. */
     @Lunatic(contextType = fr.insee.lunatic.model.flat.SequenceType.class,
             field = "setComponentType(T(fr.insee.lunatic.model.flat.ComponentTypeEnum).valueOf(#param))")
     private String componentType = "SEQUENCE";
