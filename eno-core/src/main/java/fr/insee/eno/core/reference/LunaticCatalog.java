359bf138488514a3bc31edd4c7d50a73465ff3bb
nsenave <nicolas.senave@gmail.com>
2022-09-08 11:14:29 +0200

    Create LunaticCatalog.java

---
+++ eno-core/src/main/java/fr/insee/eno/core/reference/LunaticCatalog.java
@@ -0,0 +1,34 @@
+package fr.insee.eno.core.reference;
+
+import fr.insee.lunatic.model.flat.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/** Class designed to be used in processing to easily access different kinds of Lunatic objects. */
+public class LunaticCatalog {
+
+    public final static List<Class<?>> sequenceClassTypes =
+            List.of(SequenceType.class, Subsequence.class);
+    public final static List<Class<?>> questionClassTypes =
+            List.of(Input.class, Textarea.class, InputNumber.class, CheckboxBoolean.class, Datepicker.class,
+                    CheckboxOne.class, Radio.class, Dropdown.class, CheckboxGroup.class, Table.class);
+
+    /** Map to get Lunatic components by id.*/
+    private final Map<String, ComponentType> componentMap = new HashMap<>();
+
+    public LunaticCatalog(Questionnaire lunaticQuestionnaire) {
+        indexLunaticComponents(lunaticQuestionnaire);
+    }
+
+    private void indexLunaticComponents(Questionnaire lunaticQuestionnaire) {
+        lunaticQuestionnaire.getComponents().forEach(component ->
+                componentMap.put(component.getId(), component));
+    }
+
+    public ComponentType getComponent(String componentId) {
+        return componentMap.get(componentId);
+    }
+
+}
