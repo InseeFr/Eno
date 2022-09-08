03c476ab89bdbe1060a2433e3d2e3a7b07727315
nsenave <nicolas.senave@gmail.com>
2022-09-08 13:29:20 +0200

    Create EnoIndexTest.java

---
+++ eno-core/src/test/java/fr/insee/eno/core/reference/EnoIndexTest.java
@@ -0,0 +1,30 @@
+package fr.insee.eno.core.reference;
+
+import fr.insee.eno.core.mappers.DDIMapper;
+import fr.insee.eno.core.model.EnoQuestionnaire;
+import fr.insee.eno.core.parsers.DDIParser;
+import instance33.DDIInstanceDocument;
+import org.apache.xmlbeans.XmlException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class EnoIndexTest {
+
+    @Test
+    public void indexingDoneByMapper() throws XmlException, IOException {
+        //
+        DDIInstanceDocument ddiInstanceDocument = DDIParser.parse(
+                this.getClass().getClassLoader().getResourceAsStream("in/ddi/l10xmg2l.xml"));
+        EnoQuestionnaire enoQuestionnaire = new EnoQuestionnaire();
+        //
+        DDIMapper ddiMapper = new DDIMapper();
+        ddiMapper.mapDDI(ddiInstanceDocument, enoQuestionnaire);
+        //
+        EnoIndex enoIndex = enoQuestionnaire.getIndex();
+        assertNotNull(enoIndex);
+        //TODO: proper unit test
+    }
+}
