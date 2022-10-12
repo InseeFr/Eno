b1ae4cf7ab44e33f792cabf9a9c81a10aee7ed26
nsenave <nicolas.senave@gmail.com>
2022-10-12 12:19:26 +0200

    update static label mapping (Lunatic V2)

--- eno-core/src/main/java/fr/insee/eno/core/model/CodeItem.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/CodeItem.java
@@ -2,6 +2,7 @@ package fr.insee.eno.core.model;
 
 import fr.insee.eno.core.annotations.DDI;
 import fr.insee.eno.core.annotations.Lunatic;
+import fr.insee.eno.core.model.label.Label;
 import fr.insee.lunatic.model.flat.Options;
 import logicalproduct33.CodeType;
 import lombok.Getter;
@@ -13,9 +14,9 @@ public class CodeItem extends EnoObject {
 
     @DDI(contextType = CodeType.class,
             field = "#index.get(#this.getCategoryReference().getIDArray(0).getStringValue())" +
-                    ".getLabelArray(0).getContentArray(0).getStringValue()")
+                    ".getLabelArray(0)")
     @Lunatic(contextType = Options.class, field = "setLabel(#param)")
-    String label;
+    Label label;
 
     @DDI(contextType = CodeType.class, field = "getValue().getStringValue()")
     @Lunatic(contextType = Options.class, field = "setValue(#param)")
--- eno-core/src/main/java/fr/insee/eno/core/model/CodeResponse.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/CodeResponse.java
@@ -2,6 +2,7 @@ package fr.insee.eno.core.model;
 
 import fr.insee.eno.core.annotations.DDI;
 import fr.insee.eno.core.annotations.Lunatic;
+import fr.insee.eno.core.model.label.Label;
 import fr.insee.lunatic.model.flat.ResponsesCheckboxGroup;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,9 +16,9 @@ public class CodeResponse extends EnoIdentifiableObject {
             field = "#index.get(#index.get(#index.getParent(#this.getIDArray(0).getStringValue())" +
                     ".getGridDimensionArray(0).getCodeDomain().getCodeListReference().getIDArray(0).getStringValue())" +
                     ".getCodeArray(#listIndex).getCategoryReference().getIDArray(0).getStringValue())" +
-                    ".getLabelArray(0).getContentArray(0).getStringValue()") //TODO: >_< see todo in DDIMapper....
+                    ".getLabelArray(0)") //TODO: >_< see todo in DDIMapper....
     @Lunatic(contextType = ResponsesCheckboxGroup.class, field = "setLabel(#param)")
-    private String label;
+    private Label label;
 
     @DDI(contextType = ParameterType.class, field = "#this")
     @Lunatic(contextType = ResponsesCheckboxGroup.class, field = "setResponse(#param)")
