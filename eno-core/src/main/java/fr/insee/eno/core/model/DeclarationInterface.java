220de9d3276ffaaa4dc68a18b91491ffd9502d71
nsenave <nicolas.senave@gmail.com>
2022-10-12 12:09:48 +0200

    update label mapping (Lunatic V2)
    dynamic labels

--- eno-core/src/main/java/fr/insee/eno/core/model/DeclarationInterface.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/DeclarationInterface.java
@@ -1,5 +1,7 @@
 package fr.insee.eno.core.model;
 
+import fr.insee.eno.core.model.label.DynamicLabel;
+
 import java.util.List;
 
 /** Interface to use common properties of Declaration and Instruction e.g. in during processing. */
@@ -7,8 +9,7 @@ public interface DeclarationInterface {
 
     String getId();
     String getDeclarationType(); //TODO: maybe an enum instead of string would be appropriated here (see comments in implementations)
-    String getLabel();
-    void setLabel(String label);
+    DynamicLabel getLabel();
     List<String> getVariableNames();
     String getPosition();
     List<Mode> getModes();
