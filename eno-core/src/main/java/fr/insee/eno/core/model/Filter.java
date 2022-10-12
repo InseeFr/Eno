e96143714eb12aa3108dda667c67b8c377715e1b
nsenave <nicolas.senave@gmail.com>
2022-10-12 11:01:06 +0200

    expression mapping
    Note: a bit dirty since Lunatic-Model doesn't use the LabelType object in filters

--- eno-core/src/main/java/fr/insee/eno/core/model/Filter.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/Filter.java
@@ -16,22 +16,34 @@ public class Filter extends EnoIdentifiableObject implements EnoObjectWithExpres
 
     /** Default expression is "true". */
     public Filter() {
-        expression = "true";
+        stringExpression = "true";
     }
 
     /** Lunatic filters doesn't have an identifier. */
     @DDI(contextType = IfThenElseType.class, field = "getIDArray(0).getStringValue()")
     private String id;
 
+    //TODO: Asked Lunatic-Model to use LabelType in ConditionFilterType to refactor this.
+
     /** In DDI, the expression contains variable references instead of variables names.
      * This list contains the references of these variables. */
-    @DDI(contextType = IfThenElseType.class, field = "getIfCondition().getCommandArray(0).getInParameterList()")
+    @DDI(contextType = IfThenElseType.class, field = "getIfCondition().getCommandArray(0)")
     private List<BindingReference> bindingReferences = new ArrayList<>();
 
     /** Command that determines if the filter is applied or not. */
-    @DDI(contextType = IfThenElseType.class, field = "getIfCondition().getCommandArray(0).getCommandContent()")
+    @DDI(contextType = IfThenElseType.class, field = "getIfCondition().getCommandArray(0)")
     @Lunatic(contextType = ConditionFilterType.class, field = "setValue(#param)")
-    private String expression;
+    private String stringExpression;
+
+    private CalculatedExpression expression = new CalculatedExpression();
+
+    public CalculatedExpression getExpression() {
+        expression.setValue(stringExpression);
+        expression.setBindingReferences(bindingReferences);
+        return expression;
+    }
+
+    //
 
     /** List of the identifier of components that are in the scope of the filter.
      * In DDI, we expect to have 'QuestionConstruct' and 'Sequence' references.
