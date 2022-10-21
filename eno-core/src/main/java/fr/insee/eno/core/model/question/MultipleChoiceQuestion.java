6eaefd87741643efed27b69644175e86155cb5e9
nsenave <nicolas.senave@gmail.com>
2022-10-21 10:09:23 +0200

    rework MultipleChoiceQuestion object

--- eno-core/src/main/java/fr/insee/eno/core/model/question/MultipleChoiceQuestion.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/question/MultipleChoiceQuestion.java
@@ -1,8 +1,8 @@
 package fr.insee.eno.core.model.question;
 
 import datacollection33.QuestionGridType;
-import datacollection33.QuestionItemType;
 import fr.insee.eno.core.annotations.DDI;
+import fr.insee.eno.core.annotations.Format;
 import fr.insee.eno.core.annotations.Lunatic;
 import fr.insee.eno.core.model.CodeResponse;
 import fr.insee.lunatic.model.flat.CheckboxGroup;
@@ -12,12 +12,17 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
+import static fr.insee.eno.core.annotations.Contexts.Context;
+
 @Getter
 @Setter
-public class MultipleChoiceQuestion extends MultipleResponseQuestion {
+public abstract class MultipleChoiceQuestion extends MultipleResponseQuestion {
 
-    @DDI(contextType = QuestionGridType.class, field = "getOutParameterList()")
-    @Lunatic(contextType = CheckboxGroup.class, field = "getResponses()")
-    List<CodeResponse> codeList = new ArrayList<>();
+    public static class Simple extends MultipleResponseQuestion {
+        @DDI(contextType = QuestionGridType.class, field = "getOutParameterList()")
+        @Lunatic(contextType = CheckboxGroup.class, field = "getResponses()")
+        List<CodeResponse> codeList = new ArrayList<>();
+    }
 
+    public static class Complex extends MultipleResponseQuestion {}
 }
