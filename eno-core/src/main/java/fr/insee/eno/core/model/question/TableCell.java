b8c3f715d4099c005c92ce0e4feeb810d1fd5beb
nsenave <nicolas.senave@gmail.com>
2022-10-21 10:08:22 +0200

    Table objects

--- eno-core/src/main/java/fr/insee/eno/core/model/question/TableCell.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/question/TableCell.java
@@ -1,11 +1,86 @@
 package fr.insee.eno.core.model.question;
 
-import fr.insee.eno.core.annotations.Lunatic;
+import datacollection33.GridResponseDomainInMixedType;
+import datacollection33.QuestionItemType;
+import fr.insee.eno.core.annotations.DDI;
+import fr.insee.eno.core.annotations.Format;
+import fr.insee.eno.core.model.CodeItem;
 import fr.insee.eno.core.model.EnoObject;
 
-public class TableCell extends EnoObject {
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
-    //@Lunatic(contextType = CellsType.class, field = "setId(#param)") TODO: changed in Lunatic-Model v2.2.13
-    private String id;
+import static fr.insee.eno.core.annotations.Contexts.Context;
+
+/** A TableCell object is the content of a table.
+ * A cell is neither part of the header nor of the left column. */
+@Context(format = Format.DDI, type = GridResponseDomainInMixedType.class)
+public abstract class TableCell extends EnoObject {
+
+    @DDI(contextType = GridResponseDomainInMixedType.class,
+            field = "T(java.lang.Integer).parseInt(getGridAttachmentArray(0).getCellCoordinatesAsDefinedArray(0).getSelectDimensionList()" +
+                    ".?[#this.getRank().intValue() == 1].get(0).getRangeMinimum())") // range maximum is the same in Insee DDI
+    int rowNumber;
+
+    @DDI(contextType = GridResponseDomainInMixedType.class,
+            field = "T(java.lang.Integer).parseInt(getGridAttachmentArray(0).getCellCoordinatesAsDefinedArray(0).getSelectDimensionList()" +
+                    ".?[#this.getRank().intValue() == 2].get(0).getRangeMinimum())") // range maximum is the same in Insee DDI
+    int columnNumber;
+
+    // TODO: refactor DDI response domain mapping in questions and table cells
+
+    public static class BooleanCell extends TableCell {}
+
+    public static class TextCell extends TableCell {
+        @DDI(contextType = GridResponseDomainInMixedType.class, field = "getResponseDomain().getMaxLength().intValue()")
+        BigInteger maxLength;
+    }
+
+    public static class NumericCell extends TableCell {
+        @DDI(contextType = QuestionItemType.class,
+                field = "getResponseDomain()?.getNumberRangeList()?.get(0)?.getLow()?.getStringValue() != null ? " +
+                        "T(java.lang.Double).valueOf(getResponseDomain().getNumberRangeArray(0).getLow().getStringValue()) : null")
+        double minValue;
+
+        @DDI(contextType = QuestionItemType.class,
+                field = "getResponseDomain()?.getNumberRangeList()?.get(0)?.getLow()?.getStringValue() != null ? " +
+                        "T(java.lang.Double).valueOf(getResponseDomain().getNumberRangeArray(0).getLow().getStringValue()) : null")
+        double maxValue;
+
+        @DDI(contextType = QuestionItemType.class,
+                field = "getResponseDomain()?.getDecimalPositions() ?: T(java.math.BigInteger).valueOf('0')")
+        BigInteger numberOfDecimals;
+
+        /** Unit not accessible here in DDI.
+         * In Lunatic, the value is set there during the table processing. */
+        String unit;
+    }
+
+    public static class DateCell extends TableCell {
+        @DDI(contextType = QuestionItemType.class, field = "getResponseDomain().getRangeArray(0).getMinimumValue().getStringValue()")
+        private String minValue;
+
+        @DDI(contextType = QuestionItemType.class, field = "getResponseDomain().getRangeArray(0).getMaximumValue().getStringValue()")
+        private String maxValue;
+
+        @DDI(contextType = QuestionItemType.class, field = "getResponseDomain().getDateFieldFormat().getStringValue()")
+        private String format;
+    }
+
+    public static class UniqueChoiceCell extends TableCell {
+        @DDI(contextType = QuestionItemType.class,
+                field = "getResponseDomain().getGenericOutputFormat().getStringValue().equals('radio-button') ? " +
+                        "T(fr.insee.eno.core.model.question.UniqueChoiceQuestion.DisplayFormat).RADIO : " +
+                        "getResponseDomain().getGenericOutputFormat().getStringValue().equals('checkbox') ? " +
+                        "T(fr.insee.eno.core.model.question.UniqueChoiceQuestion.DisplayFormat).CHECKBOX : " +
+                        "getResponseDomain().getGenericOutputFormat().getStringValue().equals('drop-down-list') ? " +
+                        "T(fr.insee.eno.core.model.question.UniqueChoiceQuestion.DisplayFormat).DROPDOWN : null")
+        UniqueChoiceQuestion.DisplayFormat displayFormat;
+
+        @DDI(contextType = QuestionItemType.class,
+                field = "#index.get(#this.getResponseDomain().getCodeListReference().getIDArray(0).getStringValue()).getCodeList()")
+        List<CodeItem> codeList = new ArrayList<>();
+    }
 
 }
--- eno-core/src/main/java/fr/insee/eno/core/model/question/TableQuestion.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/question/TableQuestion.java
@@ -3,21 +3,53 @@ package fr.insee.eno.core.model.question;
 import datacollection33.QuestionGridType;
 import fr.insee.eno.core.annotations.DDI;
 import fr.insee.eno.core.annotations.Lunatic;
+import fr.insee.eno.core.model.CodeList;
 import fr.insee.lunatic.model.flat.Table;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <p>
+ * In DDI a table question is modeled as such:
+ * - one code list for the header content
+ * - one code list for the left column content
+ * - a list of response domain element (one for each cell of the table)
+ * </p><p>
+ * In Lunatic, the modeling is significantly different:
+ * - a list of HeaderType which is the header (and is optional)
+ * - a list of BodyType, one BodyType is one row, and a BodyType contains a list of BodyLine which are the cells of the line
+ * </p><p>
+ * This class is designed to be close to DDI modeling.
+ * Header and left column are code lists. The left hand corner cell is in neither of these two (so it is implicitly an empty cell).
+ * The conversion of tables to Lunatic is not done via annotations,
+ * but mostly in a dedicated class that carries the logic of this conversion.
+ * </p>
+ */
 public class TableQuestion extends MultipleResponseQuestion {
 
-    /*
-    @DDI(contextType = QuestionGridType.class,
-            field = "getStructuredMixedGridResponseDomain().getGridResponseDomainInMixedList()")
-    @Lunatic(contextType = Table.class, field = "getCells()")
-    private final List<TableLine> lines = new ArrayList<>();*
-    */
-
+    /** Parameter that exists in Lunatic but that has a fixed value for now. */
     @Lunatic(contextType = Table.class, field = "setPositioning(#param)")
     private final String positioning = "HORIZONTAL";
+
+    /** Code list that contain header info. */
+    @DDI(contextType = QuestionGridType.class,
+            field = "#index.get(getGridDimensionList().?[#this.getRank().intValue() == 1].get(0)" +
+                    ".getCodeListReference().getIDArray(0).getStringValue())")
+    CodeList header;
+
+    @DDI(contextType = QuestionGridType.class,
+            field = "#index.get(getGridDimensionList().?[#this.getRank().intValue() == 2].get(0)" +
+                    ".getCodeListReference().getIDArray(0).getStringValue())")
+    CodeList leftColumn;
+
+    /** Considering that out parameters are sorted in the same order as GridResponseDomainInMixed objects in DDI. */
+    @DDI(contextType = QuestionGridType.class,
+            field = "getOutParameterList().![#this.getParameterNameArray(0).getStringArray(0).getStringValue()]")
+    List<String> variableNames;
+
+    /** Table cells. */
+    @DDI(contextType = QuestionGridType.class, field = "getStructuredMixedGridResponseDomain().getGridResponseDomainInMixedList()")
+    List<TableCell> tableCells = new ArrayList<>();
 
 }
