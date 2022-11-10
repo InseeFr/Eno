1cabbcabe9ebd0cda031eb7ab0102781bcdc04af
nsenave <nicolas.senave@gmail.com>
2022-11-10 17:04:14 +0100

    Create DDIParsingException.java

---
+++ eno-core/src/main/java/fr/insee/eno/core/exceptions/DDIParsingException.java
@@ -0,0 +1,17 @@
+package fr.insee.eno.core.exceptions;
+
+public class DDIParsingException extends Exception {
+
+    public DDIParsingException(String message) {
+        super(message);
+    }
+
+    public DDIParsingException(String message, Exception e) {
+        super(message, e);
+    }
+
+    public DDIParsingException(Exception e) {
+        super(e);
+    }
+
+}
