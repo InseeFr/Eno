bb94aed0bd9f0e769e38e0cfe46bb141dd02fd02
nsenave <nicolas.senave@gmail.com>
2022-11-25 18:34:30 +0100

    migrate Eno-WS as a Eno module

---
+++ eno-ws/src/main/java/fr/insee/eno/ws/EnoWsApplication.java
@@ -0,0 +1,13 @@
+package fr.insee.eno.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnoWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnoWsApplication.class, args);
+	}
+
+}
---
+++ eno-ws/src/test/java/fr/insee/eno/ws/EnoWsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insee.eno.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnoWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
