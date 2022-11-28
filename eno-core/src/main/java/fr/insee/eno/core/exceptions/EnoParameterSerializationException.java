1fdd05fffdae38527f333313edaddfd9b256440f
nsenave <nicolas.senave@gmail.com>
2022-11-28 15:58:51 +0100

    create parameters serialization exception
    todo: use it or delete it

---
+++ eno-core/src/main/java/fr/insee/eno/core/exceptions/EnoParameterSerializationException.java
@@ -0,0 +1,17 @@
+package fr.insee.eno.core.exceptions;
+
+public class EnoParameterSerializationException extends Exception {
+
+    public EnoParameterSerializationException(String message) {
+        super(message);
+    }
+
+    public EnoParameterSerializationException(String message, Exception e) {
+        super(message, e);
+    }
+
+    public EnoParameterSerializationException(Exception e) {
+        super(e);
+    }
+
+}
