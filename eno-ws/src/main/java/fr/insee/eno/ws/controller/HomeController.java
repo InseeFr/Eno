8a011a22856301a9ab7c041d6e203ad60626e00a
nsenave <nicolas.senave@gmail.com>
2022-12-02 08:26:12 +0100

    fix HomeController redirect

--- eno-ws/src/main/java/fr/insee/eno/ws/controller/HomeController.java
+++ eno-ws/src/main/java/fr/insee/eno/ws/controller/HomeController.java
@@ -9,6 +9,6 @@ public class HomeController {
 	
 	@RequestMapping("/")
 	public String redirectToSwaggerUI() {
-		return "redirect:/swagger-ui/index.html";
+		return "redirect:/swagger-ui.html";
 	}
 }
