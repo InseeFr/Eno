4fbf9d2c5825afc4a96470560427517b9fa230cb
nsenave <nicolas.senave@gmail.com>
2022-12-08 15:05:27 +0100

    remove local paths in tests

--- eno-core/src/test/java/fr/insee/eno/core/processing/impl/LunaticAddHierarchyTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/processing/impl/LunaticAddHierarchyTest.java
@@ -5,18 +5,25 @@ import fr.insee.lunatic.model.flat.Questionnaire;
 import org.junit.jupiter.api.Test;
 
 import javax.xml.bind.JAXBException;
-import java.nio.file.Path;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class LunaticAddHierarchyTest {
 
     @Test
-    public void testLunaticAddHierarchy() throws JAXBException {
+    public void testLunaticAddHierarchy() throws JAXBException, URISyntaxException {
+        //
+        URL url = this.getClass().getClassLoader().getResource("out/lunatic/sandbox.json");
+        assert url != null;
+        //
         JSONDeserializer lunaticDeserializer = new JSONDeserializer();
         Questionnaire lunaticQuestionnaire = lunaticDeserializer.deserialize(
-                Path.of("src/test/resources/out/lunatic/sandbox.json").toAbsolutePath().toString());
+                new File(url.toURI()).getAbsolutePath());
         //
         assertNotNull(lunaticQuestionnaire); //TODO: I didn't manage to make Lunatic deserializer work so far
     }
+
 }
