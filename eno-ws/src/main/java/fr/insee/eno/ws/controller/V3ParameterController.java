6a57902855ce1c19df0ab265c7897aaf7df845d9
nsenave <nicolas.senave@gmail.com>
2023-01-31 18:16:47 +0100

    feat: enhance V3 parameter controller

--- eno-ws/src/main/java/fr/insee/eno/ws/controller/V3ParameterController.java
+++ eno-ws/src/main/java/fr/insee/eno/ws/controller/V3ParameterController.java
@@ -1,24 +1,32 @@
 package fr.insee.eno.ws.controller;
 
 import fr.insee.eno.ws.service.ParameterService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-@RestController
+@Tag(name="Parameters (V3)")
+@RestController()
+@RequestMapping("/v3/parameter")
 public class V3ParameterController {
 
-    public final static String PARAMETERS_V3_FILE_NAME = "v3-default-parameters.json";
+    public static final String PARAMETERS_V3_FILE_NAME = "default-parameters-v3.json";
 
     @Autowired
     ParameterService parameterService;
 
-    @GetMapping(value = "v3/default-parameters", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @Operation(
+            summary="Get V3 default parameters.",
+            description="Return default parameters file, to be used in V3 endpoints that require a parameter file.")
+    @GetMapping(value = "default", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     public Mono<ResponseEntity<String>> v3DefaultParameters() {
 
         HttpHeaders headers = new HttpHeaders();
