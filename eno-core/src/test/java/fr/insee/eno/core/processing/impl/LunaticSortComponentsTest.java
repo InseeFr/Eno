84a1881c0a54920f78d61e82d7d76e0190791c50
nsenave <nicolas.senave@gmail.com>
2023-04-18 10:59:38 +0200

    test: integration test for Lunatic sorting

--- eno-core/src/test/java/fr/insee/eno/core/processing/impl/LunaticSortComponentsTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/processing/impl/LunaticSortComponentsTest.java
@@ -1,8 +1,14 @@
 package fr.insee.eno.core.processing.impl;
 
+import fr.insee.eno.core.annotations.Format;
+import fr.insee.eno.core.exceptions.business.DDIParsingException;
+import fr.insee.eno.core.mappers.DDIMapper;
+import fr.insee.eno.core.mappers.LunaticMapper;
 import fr.insee.eno.core.model.EnoQuestionnaire;
 import fr.insee.eno.core.model.question.NumericQuestion;
 import fr.insee.eno.core.model.sequence.Sequence;
+import fr.insee.eno.core.parsers.DDIParser;
+import fr.insee.eno.core.processing.EnoProcessing;
 import fr.insee.eno.core.reference.EnoIndex;
 import fr.insee.eno.core.reference.LunaticCatalog;
 import fr.insee.lunatic.model.flat.Questionnaire;
@@ -10,8 +16,9 @@ import fr.insee.lunatic.model.flat.SequenceType;
 import fr.insee.lunatic.model.flat.Textarea;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class LunaticSortComponentsTest {
 
@@ -52,5 +59,28 @@ class LunaticSortComponentsTest {
         assertTrue(lunaticQuestionnaire.getComponents().get(0) instanceof SequenceType);
         assertEquals(QUESTION_ID, lunaticQuestionnaire.getComponents().get(1).getId());
         assertTrue(lunaticQuestionnaire.getComponents().get(1) instanceof Textarea);
+    }
+
+    static class IntegrationTests {
+
+        @Test
+        void largeCoverageQuestionnaire() throws DDIParsingException, IOException {
+            //
+            EnoQuestionnaire enoQuestionnaire = new EnoQuestionnaire();
+            DDIMapper ddiMapper = new DDIMapper();
+            ddiMapper.mapDDI(
+                    DDIParser.parse(this.getClass().getClassLoader().getResourceAsStream("in/ddi/l20g2ba7.xml")),
+                    enoQuestionnaire);
+            EnoProcessing enoProcessing = new EnoProcessing();
+            enoProcessing.applyProcessing(enoQuestionnaire, Format.DDI);
+            Questionnaire lunaticQuestionnaire = new Questionnaire();
+            LunaticMapper lunaticMapper = new LunaticMapper();
+            lunaticMapper.mapQuestionnaire(enoQuestionnaire, lunaticQuestionnaire);
+            //
+            LunaticCatalog lunaticCatalog = new LunaticCatalog(lunaticQuestionnaire);
+            LunaticSortComponents processing = new LunaticSortComponents(enoQuestionnaire, lunaticCatalog);
+            assertDoesNotThrow(() -> processing.apply(lunaticQuestionnaire));
+        }
+
     }
 }
