9dc5dc9abb133da624e1c86606a881bb49e2462c
nsenave <nicolas.senave@gmail.com>
2023-07-12 17:08:45 +0200

    test: integration test for Lunatic component filters

--- eno-core/src/test/java/fr/insee/eno/core/mapping/out/lunatic/ComponentFilterTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/mapping/out/lunatic/ComponentFilterTest.java
@@ -1,12 +1,19 @@
 package fr.insee.eno.core.mapping.out.lunatic;
 
 import fr.insee.eno.core.Constant;
+import fr.insee.eno.core.DDIToLunatic;
+import fr.insee.eno.core.exceptions.business.DDIParsingException;
 import fr.insee.eno.core.mappers.LunaticMapper;
 import fr.insee.eno.core.model.calculated.BindingReference;
 import fr.insee.eno.core.model.navigation.ComponentFilter;
 import fr.insee.lunatic.model.flat.ConditionFilterType;
+import fr.insee.lunatic.model.flat.Questionnaire;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -47,6 +54,49 @@ class ComponentFilterTest {
         assertEquals(Constant.LUNATIC_LABEL_VTL, lunaticConditionFilter.getType());
         assertEquals(1, lunaticConditionFilter.getBindingDependencies().size());
         assertEquals("FOO_VARIABLE", lunaticConditionFilter.getBindingDependencies().get(0));
+    }
+
+    @Nested
+    class IntegrationTest {
+
+        private static Questionnaire lunaticQuestionnaire;
+
+        @BeforeAll
+        static void mapQuestionnaire() throws DDIParsingException {
+            lunaticQuestionnaire = DDIToLunatic.transform(
+                    ComponentFilterTest.class.getClassLoader().getResourceAsStream(
+                            "integration/ddi/ddi-filters-simple.xml"));
+        }
+
+        @Test
+        void componentsWithDefaultFilter() {
+            List.of(0, 1, 2, 3, 6, 9).forEach(index ->
+                    assertEquals("true",
+                            lunaticQuestionnaire.getComponents().get(index).getConditionFilter().getValue()));
+        }
+
+        @Test
+        void sequenceFilter() {
+            // Sequence component
+            assertEquals("(Q11)", lunaticQuestionnaire.getComponents().get(4).getConditionFilter().getValue());
+            // Question in the sequence
+            assertEquals("(Q11)", lunaticQuestionnaire.getComponents().get(5).getConditionFilter().getValue());
+        }
+
+        @Test
+        void subsequenceFilter() {
+            // Subsequence component
+            assertEquals("(Q12)", lunaticQuestionnaire.getComponents().get(7).getConditionFilter().getValue());
+            // Question in the subsequence
+            assertEquals("(Q12)", lunaticQuestionnaire.getComponents().get(8).getConditionFilter().getValue());
+        }
+
+        @Test
+        void questionFilter() {
+            // Question component
+            assertEquals("(Q13)", lunaticQuestionnaire.getComponents().get(10).getConditionFilter().getValue());
+        }
+
     }
 
 }
