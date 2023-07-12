5ce516aee2bc50043cacfe1d705c28682a4291e1
David Darras <david.darras@insee.fr>
2023-07-12 11:56:30 +0200

    fix: [#582] resolve controls attributes for lunatic
    - reverse control VTL expression value for lunatic
    - add typeOfControl attribute
    - resolve variable references in error messages

--- eno-core/src/main/java/fr/insee/eno/core/model/navigation/Control.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/navigation/Control.java
@@ -10,6 +10,7 @@ import fr.insee.eno.core.model.calculated.CalculatedExpression;
 import fr.insee.eno.core.model.label.DynamicLabel;
 import fr.insee.lunatic.model.flat.ControlCriticityEnum;
 import fr.insee.lunatic.model.flat.ControlType;
+import fr.insee.lunatic.model.flat.ControlTypeOfControlEnum;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,6 +20,8 @@ import lombok.Setter;
 public class Control extends EnoIdentifiableObject implements EnoObjectWithExpression {
 
     public enum Criticality {INFO, WARN, ERROR}
+
+    public enum TypeOfControl { CONSISTENCY, FORMAT }
 
     public static Criticality convertDDICriticality(String ddiCriticality) {
         return switch (ddiCriticality) {
@@ -37,6 +40,13 @@ public class Control extends EnoIdentifiableObject implements EnoObjectWithExpre
         };
     }
 
+    public static ControlTypeOfControlEnum convertTypeOfControlToLunatic(TypeOfControl typeOfControl) {
+        return switch (typeOfControl) {
+            case FORMAT -> ControlTypeOfControlEnum.FORMAT;
+            case CONSISTENCY -> ControlTypeOfControlEnum.CONSISTENCY;
+        };
+    }
+
     /** Control criticality. */
     @DDI(contextType = ComputationItemType.class,
             field = "T(fr.insee.eno.core.model.navigation.Control).convertDDICriticality(" +
@@ -44,6 +54,12 @@ public class Control extends EnoIdentifiableObject implements EnoObjectWithExpre
     @Lunatic(contextType = ControlType.class,
             field = "setCriticality(T(fr.insee.eno.core.model.navigation.Control).convertCriticalityToLunatic(#param))")
     private Criticality criticality;
+
+    @DDI(contextType = ComputationItemType.class,
+            field = "T(fr.insee.eno.core.model.navigation.Control.TypeOfControl).CONSISTENCY")
+    @Lunatic(contextType = ControlType.class,
+            field = "setTypeOfControl(T(fr.insee.eno.core.model.navigation.Control).convertTypeOfControlToLunatic(#param))")
+    private TypeOfControl typeOfControl;
 
     /** Label typed in Pogues, unused in Lunatic. */
     @DDI(contextType = ComputationItemType.class,
