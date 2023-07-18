181ae18f46305cc821156493f0094e4d73c6b108
nsenave <nicolas.senave@gmail.com>
2023-07-18 16:18:58 +0200

    refactor: remove previous draft for binding dependencies

--- eno-core/src/main/java/fr/insee/eno/core/processing/LunaticProcessing.java
+++ eno-core/src/main/java/fr/insee/eno/core/processing/LunaticProcessing.java
@@ -38,8 +38,6 @@ public class LunaticProcessing {
         //
         new LunaticSortComponents(enoQuestionnaire).apply(lunaticQuestionnaire);
         new LunaticLoopResolution(enoQuestionnaire).apply(lunaticQuestionnaire);
-
-        new LunaticAddBindingDependencies(lunaticCatalog, enoIndex).apply(lunaticQuestionnaire);
         new LunaticAddMissingVariables(parameters.isMissingVariables()).apply(lunaticQuestionnaire);
         new LunaticAddHierarchy().apply(lunaticQuestionnaire);
         new LunaticAddPageNumbers(parameters.getLunaticPaginationMode()).apply(lunaticQuestionnaire);
