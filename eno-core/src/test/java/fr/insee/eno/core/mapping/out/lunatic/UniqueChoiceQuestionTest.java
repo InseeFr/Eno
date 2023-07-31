d6f3bcf1f027d77683aa8e1749ca24a8e55738b9
nsenave <nicolas.senave@gmail.com>
2023-07-31 10:13:55 +0200

    test: refactor mapping tests (mapper + annotations)

--- eno-core/src/test/java/fr/insee/eno/core/mapping/out/lunatic/UniqueChoiceQuestionTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/mapping/out/lunatic/UniqueChoiceQuestionTest.java
@@ -1,6 +1,6 @@
-package fr.insee.eno.core.mappers.lunatic;
+package fr.insee.eno.core.mapping.out.lunatic;
 
-import fr.insee.eno.core.mappers.MapperTestUtils;
+import fr.insee.eno.core.mappers.LunaticMapper;
 import fr.insee.eno.core.model.EnoQuestionnaire;
 import fr.insee.eno.core.model.question.UniqueChoiceQuestion;
 import fr.insee.lunatic.model.flat.ComponentTypeEnum;
@@ -11,8 +11,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UniqueChoiceQuestionTest {
 
-    private final MapperTestUtils testUtils = new MapperTestUtils();
-
     @Test
     void radio_lunaticComponentType() {
         // With the current implementation, this can only be tested starting at the questionnaire level.
@@ -22,7 +20,8 @@ class UniqueChoiceQuestionTest {
         enoQuestionnaire.getSingleResponseQuestions().add(enoUniqueChoiceQuestion);
         Questionnaire lunaticQuestionnaire = new Questionnaire();
         //
-        testUtils.mapLunaticProperty(enoQuestionnaire, lunaticQuestionnaire, "singleResponseQuestions");
+        LunaticMapper lunaticMapper = new LunaticMapper();
+        lunaticMapper.mapEnoObject(enoQuestionnaire, lunaticQuestionnaire);
         //
         assertEquals(ComponentTypeEnum.RADIO, lunaticQuestionnaire.getComponents().get(0).getComponentType());
     }
@@ -36,7 +35,8 @@ class UniqueChoiceQuestionTest {
         enoQuestionnaire.getSingleResponseQuestions().add(enoUniqueChoiceQuestion);
         Questionnaire lunaticQuestionnaire = new Questionnaire();
         //
-        testUtils.mapLunaticProperty(enoQuestionnaire, lunaticQuestionnaire, "singleResponseQuestions");
+        LunaticMapper lunaticMapper = new LunaticMapper();
+        lunaticMapper.mapEnoObject(enoQuestionnaire, lunaticQuestionnaire);
         //
         assertEquals(ComponentTypeEnum.CHECKBOX_ONE, lunaticQuestionnaire.getComponents().get(0).getComponentType());
     }
@@ -50,7 +50,8 @@ class UniqueChoiceQuestionTest {
         enoQuestionnaire.getSingleResponseQuestions().add(enoUniqueChoiceQuestion);
         Questionnaire lunaticQuestionnaire = new Questionnaire();
         //
-        testUtils.mapLunaticProperty(enoQuestionnaire, lunaticQuestionnaire, "singleResponseQuestions");
+        LunaticMapper lunaticMapper = new LunaticMapper();
+        lunaticMapper.mapEnoObject(enoQuestionnaire, lunaticQuestionnaire);
         //
         assertEquals(ComponentTypeEnum.DROPDOWN, lunaticQuestionnaire.getComponents().get(0).getComponentType());
     }
