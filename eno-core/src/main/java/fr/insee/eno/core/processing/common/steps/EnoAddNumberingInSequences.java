3496a542b5aca71cbfe1ac081e151a871aaa9e3a
Nicolas Senave <nicolas.senave@gmail.com>
2023-08-11 10:57:23 +0200

    fix: use VTL concatenation in sequence numbering

--- eno-core/src/main/java/fr/insee/eno/core/processing/common/steps/EnoAddNumberingInSequences.java
+++ eno-core/src/main/java/fr/insee/eno/core/processing/common/steps/EnoAddNumberingInSequences.java
@@ -5,18 +5,24 @@ import fr.insee.eno.core.model.label.Label;
 import fr.insee.eno.core.model.sequence.Sequence;
 import fr.insee.eno.core.processing.ProcessingStep;
 import fr.insee.eno.core.utils.RomanNumber;
+import fr.insee.eno.core.utils.VtlSyntaxUtils;
 
 public class EnoAddNumberingInSequences implements ProcessingStep<EnoQuestionnaire> {
 
-    public static final String SEQUENCE_NUMBERING_SEPARATOR = " -";
+    public static final String SEQUENCE_NUMBERING_SEPARATOR = "-";
 
     public void apply(EnoQuestionnaire enoQuestionnaire) {
         int sequenceNumber = 1;
         for (Sequence sequence : enoQuestionnaire.getSequences()) {
             Label sequenceLabel = sequence.getLabel();
-            sequenceLabel.setValue(RomanNumber.toRoman(sequenceNumber) + SEQUENCE_NUMBERING_SEPARATOR + " " + sequenceLabel.getValue());
+            sequenceLabel.setValue(addNumberInLabel(sequenceNumber, sequenceLabel));
             sequenceNumber ++;
         }
+    }
+
+    private static String addNumberInLabel(int sequenceNumber, Label sequenceLabel) {
+        String numberingPrefix = RomanNumber.toRoman(sequenceNumber) + " " + SEQUENCE_NUMBERING_SEPARATOR + " ";
+        return VtlSyntaxUtils.concatenateStrings(numberingPrefix, sequenceLabel.getValue());
     }
 
 }
