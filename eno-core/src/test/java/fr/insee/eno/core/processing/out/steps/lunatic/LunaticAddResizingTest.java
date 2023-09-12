27a5ad90f0ddeb40450eb7024ca90176689d4209
Nicolas Senave <nicolas.senave@gmail.com>
2023-09-12 14:15:11 +0200

    refactor: Lunatic resizing test (wip)

--- eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/LunaticAddResizingTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/LunaticAddResizingTest.java
@@ -1,9 +1,11 @@
-package fr.insee.eno.core.processing.impl;
+package fr.insee.eno.core.processing.out.steps.lunatic;
 
 import fr.insee.eno.core.model.EnoQuestionnaire;
 import fr.insee.eno.core.model.lunatic.LunaticResizingLoopVariable;
 import fr.insee.eno.core.model.lunatic.LunaticResizingPairWiseVariable;
+import fr.insee.eno.core.model.navigation.StandaloneLoop;
 import fr.insee.eno.core.processing.out.steps.lunatic.LunaticAddResizing;
+import fr.insee.eno.core.reference.EnoIndex;
 import fr.insee.lunatic.model.flat.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,88 +18,90 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class LunaticAddResizingTest {
 
     private Questionnaire lunaticQuestionnaire;
-    private Sequence s1, s4, s8, s9;
-    private InputNumber n2, n3, n72, n73, n5;
-    private Textarea t10;
-    private Loop l6, l7;
-    private Subsequence ss6, ss71;
-    private Input i6;
-    private CheckboxOne co71, co73;
-    private PairwiseLinks p73;
-    private List<ComponentType> components;
-    private EnoQuestionnaire enoQuestionnaire;
     private LunaticAddResizing processing;
 
     @BeforeEach
     void init() {
-        enoQuestionnaire = new EnoQuestionnaire();
         lunaticQuestionnaire = buildLunaticQuestionnaire();
-        processing = new LunaticAddResizing(enoQuestionnaire);
+        processing = new LunaticAddResizing(createEnoQuestionnaire());
     }
 
     @Test
     void whenProcessingLoopVariableIsConsideredAsLoopVariable() {
+        //
         processing.apply(lunaticQuestionnaire);
         List<Object> resizings = lunaticQuestionnaire.getResizing().getAny();
-
+        //
         assertTrue(resizings.get(0) instanceof LunaticResizingLoopVariable);
     }
 
     @Test
     void whenProcessingLoopVariableIsConsideredAsPairwiseVariable() {
+        //
         processing.apply(lunaticQuestionnaire);
         List<Object> resizings = lunaticQuestionnaire.getResizing().getAny();
+        //
         assertTrue(resizings.get(1) instanceof LunaticResizingPairWiseVariable);
+    }
+
+    private EnoQuestionnaire createEnoQuestionnaire() {
+        EnoQuestionnaire enoQuestionnaire = new EnoQuestionnaire();
+        EnoIndex enoIndex = new EnoIndex();
+        //
+        // TODO: eno questionnaire content
+        //
+        enoQuestionnaire.setIndex(enoIndex);
+        return enoQuestionnaire;
     }
 
     private Questionnaire buildLunaticQuestionnaire() {
         Questionnaire questionnaire = new Questionnaire();
-        components = new ArrayList<>();
+        List<ComponentType> components = new ArrayList<>();
         List<ComponentType> l6Components = new ArrayList<>();
         List<ComponentType> l7Components = new ArrayList<>();
         List<ComponentType> p73Components = new ArrayList<>();
 
-        s1 = buildSequence("jfaz9kv9");
+        Sequence s1 = buildSequence("jfaz9kv9");
         components.add(s1);
 
-        n2 = buildNumber("jfazk91m");
+        InputNumber n2 = buildNumber("jfazk91m");
         components.add(n2);
 
-        n3 = buildNumber("lhpz37kh");
+        InputNumber n3 = buildNumber("lhpz37kh");
         components.add(n3);
 
-        s4 = buildSequence("li1w5tqk");
+        Sequence s4 = buildSequence("li1w5tqk");
         components.add(s4);
 
-        n5 = buildNumber("li1w3tmf");
+        InputNumber n5 = buildNumber("li1w3tmf");
         components.add(n5);
 
-        l6 = buildEmptyLoop("li1wjxs2");
+        Loop l6 = buildEmptyLoop("li1wjxs2");
 
         // build loop 6 components
-        ss6 = buildSubsequence("li1wbv47");
+        Subsequence ss6 = buildSubsequence("li1wbv47");
         l6Components.add(ss6);
 
-        i6 = buildInput("li1wptdt");
+        Input i6 = buildInput("li1wptdt");
         l6Components.add(i6);
 
         l6.getComponents().addAll(l6Components);
         components.add(l6);
 
-        l7 = buildEmptyLoop("li1wsotd");
+        Loop l7 = buildEmptyLoop("li1wsotd");
 
-        ss71 = buildSubsequence("li1wfnbk");
+        Subsequence ss71 = buildSubsequence("li1wfnbk");
         l7Components.add(ss71);
 
-        co71 = buildCheckboxOne("lhpyz9b0");
+        CheckboxOne co71 = buildCheckboxOne("lhpyz9b0");
         l7Components.add(co71);
 
-        n72 = buildNumber("lhpzan4t");
+        InputNumber n72 = buildNumber("lhpzan4t");
         l7Components.add(n72);
 
-        p73 = buildEmptyPairWiseLinks("pairwise-links");
-        co73 = buildCheckboxOne("lhpyz9b73");
-        n73 = buildNumber("lhpzan73");
+        PairwiseLinks p73 = buildEmptyPairWiseLinks("pairwise-links");
+        CheckboxOne co73 = buildCheckboxOne("lhpyz9b73");
+        InputNumber n73 = buildNumber("lhpzan73");
 
         p73Components.add(co73);
         p73Components.add(n73);
@@ -107,13 +111,13 @@ class LunaticAddResizingTest {
         l7.getComponents().addAll(l7Components);
         components.add(l7);
 
-        s8 = buildSequence("li1wjpqw");
+        Sequence s8 = buildSequence("li1wjpqw");
         components.add(s8);
 
-        s9 = buildSequence("COMMENT-SEQ");
+        Sequence s9 = buildSequence("COMMENT-SEQ");
         components.add(s9);
 
-        t10 = buildTextarea("COMMENT-QUESTION");
+        Textarea t10 = buildTextarea("COMMENT-QUESTION");
         components.add(t10);
 
         questionnaire.getComponents().addAll(components);
