b6b0311c67c31e291fbedb1dc8a532684879edf1
Nicolas Senave <nicolas.senave@gmail.com>
2023-09-27 10:22:10 +0200

    test: tidy up deserializers tests

--- eno-core/src/test/java/fr/insee/eno/core/serialize/PoguesDeserializerTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/serialize/PoguesDeserializerTest.java
@@ -1,25 +1,37 @@
 package fr.insee.eno.core.serialize;
 
 import fr.insee.eno.core.exceptions.business.PoguesDeserializationException;
-import fr.insee.eno.core.serialize.PoguesDeserializer;
 import fr.insee.pogues.model.Questionnaire;
 import org.junit.jupiter.api.Test;
 
 import java.net.URISyntaxException;
 import java.net.URL;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PoguesDeserializerTest {
 
     @Test
-    void poguesParserTest() throws URISyntaxException, PoguesDeserializationException {
+    void deserialize_simpleQuestionnaire() throws PoguesDeserializationException, URISyntaxException {
         //
-        URL testPoguesFileUrl = this.getClass().getClassLoader().getResource("end-to-end/pogues/pogues-l20g2ba7.json");
+        URL testPoguesFileUrl = this.getClass().getClassLoader().getResource(
+                "integration/pogues/pogues-simple.json");
         assert testPoguesFileUrl != null;
+        //
         Questionnaire poguesQuestionnaire = PoguesDeserializer.deserialize(testPoguesFileUrl);
+        //
+        assertNotNull(poguesQuestionnaire);
+        assertFalse(poguesQuestionnaire.getChild().isEmpty());
+    }
 
+    @Test
+    void deserialize_largeQuestionnaire() throws URISyntaxException, PoguesDeserializationException {
+        //
+        URL testPoguesFileUrl = this.getClass().getClassLoader().getResource(
+                "end-to-end/pogues/pogues-l20g2ba7.json");
+        assert testPoguesFileUrl != null;
+        //
+        Questionnaire poguesQuestionnaire = PoguesDeserializer.deserialize(testPoguesFileUrl);
         //
         assertNotNull(poguesQuestionnaire);
         assertEquals("l20g2ba7", poguesQuestionnaire.getId());
