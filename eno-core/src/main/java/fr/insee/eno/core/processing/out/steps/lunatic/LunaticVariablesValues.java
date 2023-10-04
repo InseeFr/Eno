b24c854904e787630f713e95a19b1de4e86b2f4c
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2023-10-04 10:14:38 +0200

    fix: lunatic variable values (#744)
    * build: bump Lunatic-Model version
    
    Version that contains changes that allows making direct serialization of "values"
    
    * refactor: make 'getCollectedVariablesInLoop' reusable
    
    * fix(values): processing for Lunatic loop values
    
    * test: unit test for Lunatic loop values
    
    * refactor: getPairwiseResponseVariable method
    
    * build: bump Lunatic-Model version
    
    Contains two-dimension variable object for pairwise.
    
    * refactor: cleaner return value for getPairwiseResponseVariable
    
    * fix(values): init values in loop variables
    
    * fix(values): processing for Lunatic pairwise values
    
    * test: unit test for Lunatic pairwise values
    
    * build: bump snapshot version
    
    * build: Lunatic-Model patched version
    
    logging binding stuff
    
    * fix: side effect with new variables
    
    * chore: release version

---
+++ eno-core/src/main/java/fr/insee/eno/core/processing/out/steps/lunatic/LunaticVariablesValues.java
@@ -0,0 +1,70 @@
+package fr.insee.eno.core.processing.out.steps.lunatic;
+
+import fr.insee.eno.core.processing.ProcessingStep;
+import fr.insee.eno.core.utils.LunaticUtils;
+import fr.insee.lunatic.model.flat.*;
+
+import java.util.List;
+
+/**
+ * <p>Processing step aimed to replace Lunatic simple variable objects by:</p>
+ *  <ul>
+ *   <li>"one dimension" array variable objects for variables that belong to a loop</li>
+ *   <li>"two dimensions" array variable object for the pairwise variable</li>
+ * </ul>
+ * <p>By doing this, the "values" part of the Lunatic variables will have the correct format when serializing
+ * the questionnaire.</p>
+ */
+public class LunaticVariablesValues implements ProcessingStep<Questionnaire> {
+
+    private Questionnaire lunaticQuestionnaire;
+
+    @Override
+    public void apply(Questionnaire lunaticQuestionnaire) {
+        //
+        this.lunaticQuestionnaire = lunaticQuestionnaire;
+        //
+        lunaticQuestionnaire.getComponents().stream()
+                .filter(Loop.class::isInstance)
+                .map(Loop.class::cast)
+                .forEach(this::replaceLoopVariables);
+        //
+        lunaticQuestionnaire.getComponents().stream()
+                .filter(PairwiseLinks.class::isInstance)
+                .map(PairwiseLinks.class::cast)
+                .forEach(this::replacePairwiseVariable);
+    }
+
+    /**
+     * Iterates on the loop components to find the variables that are collected within the loop.
+     * Then, replaces corresponding variable objects by new variable array objects.
+     * Note: with the current implementation, the replacement is not "in-place".
+     * @param loop A Lunatic loop components.
+     */
+    private void replaceLoopVariables(Loop loop) {
+        List<String> collectedVariables = LunaticUtils.getCollectedVariablesInLoop(loop);
+        //
+        lunaticQuestionnaire.getVariables().removeIf(variable -> collectedVariables.contains(variable.getName()));
+        //
+        collectedVariables.forEach(variableName -> {
+            VariableTypeArray variableTypeArray = new VariableTypeArray();
+            variableTypeArray.setVariableType(VariableTypeEnum.COLLECTED);
+            variableTypeArray.setName(variableName);
+            variableTypeArray.setValues(new ValuesTypeArray());
+            lunaticQuestionnaire.getVariables().add(variableTypeArray);
+        });
+    }
+
+    private void replacePairwiseVariable(PairwiseLinks pairwiseLinks) {
+        String pairwiseVariableName = LunaticUtils.getPairwiseResponseVariable(pairwiseLinks);
+        //
+        lunaticQuestionnaire.getVariables().removeIf(variable -> pairwiseVariableName.equals(variable.getName()));
+        //
+        VariableTypeTwoDimensionsArray variableTypeTwoDimensionsArray = new VariableTypeTwoDimensionsArray();
+        variableTypeTwoDimensionsArray.setVariableType(VariableTypeEnum.COLLECTED);
+        variableTypeTwoDimensionsArray.setName(pairwiseVariableName);
+        variableTypeTwoDimensionsArray.setValues(new ValuesTypeTwoDimensionsArray());
+        lunaticQuestionnaire.getVariables().add(variableTypeTwoDimensionsArray);
+    }
+
+}
