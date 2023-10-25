065007187c90cebd0588b15cdc2832c8bf808858
nsenave <nicolas.senave@gmail.com>
2023-10-25 23:58:48 +0200

    test: wip

--- eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/table/ComplexMultipleChoiceQuestionProcessingTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/table/ComplexMultipleChoiceQuestionProcessingTest.java
@@ -5,15 +5,20 @@ import fr.insee.eno.core.exceptions.business.DDIParsingException;
 import fr.insee.eno.core.model.EnoQuestionnaire;
 import fr.insee.eno.core.model.question.ComplexMultipleChoiceQuestion;
 import fr.insee.eno.core.parameter.EnoParameters;
+import fr.insee.lunatic.model.flat.ComponentTypeEnum;
 import fr.insee.lunatic.model.flat.Table;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ComplexMultipleChoiceQuestionProcessingTest {
 
@@ -39,21 +44,33 @@ class ComplexMultipleChoiceQuestionProcessingTest {
         lunaticTable = new Table();
     }
 
-    @Test
-    void simpleHeaderCodeList() {
-        assertDoesNotThrow(() -> {
-            ComplexMultipleChoiceQuestionProcessing.process(lunaticTable, enoMCQList.get(0));
-            ComplexMultipleChoiceQuestionProcessing.process(lunaticTable, enoMCQList.get(1));
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1}) // First two MCQ are identical except first has radio format, second dropdown
+    void simpleHeaderCodeList(int i) {
+        //
+        ComplexMultipleChoiceQuestionProcessing.process(lunaticTable, enoMCQList.get(i));
+        //
+        assertTrue(lunaticTable.getHeader().isEmpty());
+        assertEquals(4, lunaticTable.getBodyLines().size());
+        lunaticTable.getBodyLines().forEach(bodyLine -> {
+            assertEquals(2, bodyLine.getBodyCells().size());
+            assertNotNull(bodyLine.getBodyCells().get(0).getValue());
+            assertNotNull(bodyLine.getBodyCells().get(0).getLabel());
+            assertEquals(i == 0 ? ComponentTypeEnum.RADIO : ComponentTypeEnum.DROPDOWN,
+                    bodyLine.getBodyCells().get(1).getComponentType());
+            assertEquals(5, bodyLine.getBodyCells().get(1).getOptions().size());
         });
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {2, 3}) // same idea as other test
     @Disabled("being debugged")
-    void nestedHeaderCodeList() {
-        assertDoesNotThrow(() -> {
-            ComplexMultipleChoiceQuestionProcessing.process(lunaticTable, enoMCQList.get(2));
-            ComplexMultipleChoiceQuestionProcessing.process(lunaticTable, enoMCQList.get(3));
-        });
+    void nestedHeaderCodeList(int i) {
+        //
+        ComplexMultipleChoiceQuestionProcessing.process(lunaticTable, enoMCQList.get(i));
+        //
+        assertTrue(lunaticTable.getHeader().isEmpty());
+        assertEquals(7, lunaticTable.getBodyLines().size());
     }
     
 }
