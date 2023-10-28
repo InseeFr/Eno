3cae00fa9302bf693a38ea6f3d78299bc5479120
nsenave <nicolas.senave@gmail.com>
2023-10-28 13:18:22 +0200

    test(pairwise): integration test ddi to lunatic

--- eno-core/src/test/java/fr/insee/eno/core/mapping/out/lunatic/PairwiseQuestionTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/mapping/out/lunatic/PairwiseQuestionTest.java
@@ -1,32 +1,80 @@
 package fr.insee.eno.core.mapping.out.lunatic;
 
+import fr.insee.eno.core.Constant;
+import fr.insee.eno.core.DDIToEno;
+import fr.insee.eno.core.exceptions.business.DDIParsingException;
 import fr.insee.eno.core.mappers.LunaticMapper;
+import fr.insee.eno.core.model.EnoQuestionnaire;
 import fr.insee.eno.core.model.question.PairwiseQuestion;
-import fr.insee.lunatic.model.flat.ComponentTypeEnum;
-import fr.insee.lunatic.model.flat.PairwiseLinks;
+import fr.insee.eno.core.parameter.EnoParameters;
+import fr.insee.lunatic.model.flat.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class PairwiseQuestionTest {
 
-    private PairwiseQuestion enoPairwiseQuestion;
-    private PairwiseLinks lunaticPairwiseLinks;
+    @Nested
+    class UnitTestsPairwise {
+        private PairwiseQuestion enoPairwiseQuestion;
+        private PairwiseLinks lunaticPairwiseLinks;
 
-    @BeforeEach
-    void pairwiseObjects() {
-        enoPairwiseQuestion = new PairwiseQuestion();
-        lunaticPairwiseLinks = new PairwiseLinks();
+        @BeforeEach
+        void pairwiseObjects() {
+            enoPairwiseQuestion = new PairwiseQuestion();
+            lunaticPairwiseLinks = new PairwiseLinks();
+        }
+
+        @Test
+        void lunaticComponentType() {
+            //
+            LunaticMapper lunaticMapper = new LunaticMapper();
+            lunaticMapper.mapEnoObject(enoPairwiseQuestion, lunaticPairwiseLinks);
+            //
+            assertEquals(ComponentTypeEnum.PAIRWISE_LINKS, lunaticPairwiseLinks.getComponentType());
+        }
     }
 
     @Test
-    void lunaticComponentType() {
-        //
+    void pairwiseMapping_integrationTest() throws DDIParsingException {
+        // Given
+        EnoQuestionnaire enoQuestionnaire = DDIToEno.transform(
+                this.getClass().getClassLoader().getResourceAsStream("integration/ddi/ddi-pairwise.xml"),
+                EnoParameters.of(EnoParameters.Context.HOUSEHOLD, EnoParameters.ModeParameter.CAWI));
+        Optional<PairwiseQuestion> enoPairwise = enoQuestionnaire.getSingleResponseQuestions().stream()
+                .filter(PairwiseQuestion.class::isInstance)
+                .map(PairwiseQuestion.class::cast)
+                .findAny();
+        assert enoPairwise.isPresent();
+
+        // When
+        PairwiseLinks lunaticPairwise = new PairwiseLinks();
         LunaticMapper lunaticMapper = new LunaticMapper();
-        lunaticMapper.mapEnoObject(enoPairwiseQuestion, lunaticPairwiseLinks);
+        lunaticMapper.mapEnoObject(enoPairwise.get(), lunaticPairwise);
+
+        // Then
+        assertEquals("lo9tyy1v", lunaticPairwise.getId());
+        assertEquals("count(PAIRWISE_SOURCE)", lunaticPairwise.getXAxisIterations().getValue());
+        assertEquals("count(PAIRWISE_SOURCE)", lunaticPairwise.getYAxisIterations().getValue());
+        assertEquals(Constant.LUNATIC_LABEL_VTL, lunaticPairwise.getXAxisIterations().getType());
+        assertEquals(Constant.LUNATIC_LABEL_VTL, lunaticPairwise.getYAxisIterations().getType());
+        assertEquals(1, lunaticPairwise.getComponents().size());
         //
-        assertEquals(ComponentTypeEnum.PAIRWISE_LINKS, lunaticPairwiseLinks.getComponentType());
+        ComponentType pairwiseInnerComponent = lunaticPairwise.getComponents().get(0);
+        assertTrue(pairwiseInnerComponent instanceof Dropdown);
+        assertEquals(ComponentTypeEnum.DROPDOWN, pairwiseInnerComponent.getComponentType());
+        Dropdown lunaticPairwiseDropDown = (Dropdown) pairwiseInnerComponent;
+        assertEquals("lo9tyy1v-pairwise-dropdown", lunaticPairwiseDropDown.getId());
+        assertEquals("PAIRWISE_QUESTION", lunaticPairwiseDropDown.getResponse().getName());
+        assertEquals(4, lunaticPairwiseDropDown.getOptions().size());
+        assertEquals(List.of("linkA", "linkB", "linkC", "linkD"),
+                lunaticPairwiseDropDown.getOptions().stream().map(Options::getValue).toList());
+        // Note: for now the symLinks property is added (hard-coded values) by Lunatic-Model
     }
 
 }
