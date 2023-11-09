ef46c4992b8f938f6872198071df72c191e8ac12
Nicolas Senave <nicolas.senave@gmail.com>
2023-11-09 09:40:24 +0100

    refactor: improve suggesters json schema (wip)

--- eno-treatments/src/test/java/fr/insee/eno/treatments/LunaticSuggesterProcessingTest.java
+++ eno-treatments/src/test/java/fr/insee/eno/treatments/LunaticSuggesterProcessingTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-public class LunaticSuggesterProcessingTest {
+class LunaticSuggesterProcessingTest {
 
     @Test
     void suggesterTest() throws DDIParsingException {
