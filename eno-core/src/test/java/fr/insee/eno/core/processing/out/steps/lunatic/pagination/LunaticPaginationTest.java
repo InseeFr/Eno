01879bb18e80b525df8775c0e2b3807aea157258
Nicolas Senave <nicolas.senave@gmail.com>
2023-11-22 17:25:07 +0100

    test(pagination): add unit test cases

--- eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/pagination/LunaticPaginationTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/pagination/LunaticPaginationTest.java
@@ -20,7 +20,6 @@ class LunaticPaginationTest {
 
         @BeforeEach
         void createLoopWithSequence() {
-            //
             questionnaire = new Questionnaire();
             Sequence sequence = new Sequence();
             sequence.setComponentType(ComponentTypeEnum.SEQUENCE);
@@ -30,6 +29,7 @@ class LunaticPaginationTest {
             input2.setComponentType(ComponentTypeEnum.INPUT);
             Loop loop = new Loop();
             loop.setComponentType(ComponentTypeEnum.LOOP);
+            loop.setLines(new LinesLoop());
             loop.getComponents().add(sequence);
             loop.getComponents().add(input1);
             loop.getComponents().add(input2);
@@ -44,11 +44,12 @@ class LunaticPaginationTest {
             assertEquals("question", questionnaire.getPagination());
             assertEquals("1", questionnaire.getMaxPage());
             Loop loop = (Loop) questionnaire.getComponents().get(0);
-            assertTrue(loop.getPaginatedLoop());
-            assertEquals("3", loop.getMaxPage());
-            assertEquals("1.1", loop.getComponents().get(0).getPage());
-            assertEquals("1.2", loop.getComponents().get(1).getPage());
-            assertEquals("1.3", loop.getComponents().get(2).getPage());
+            assertFalse(loop.getPaginatedLoop());
+            // Non paginated loops don't have a "max page" property
+            assertNull(loop.getMaxPage());
+            // Non paginated loops  don't use "dotted" numbering
+            loop.getComponents().forEach(loopComponent ->
+                    assertEquals("1", loopComponent.getPage()));
         }
 
         @Test
@@ -60,9 +61,72 @@ class LunaticPaginationTest {
             assertEquals("1", questionnaire.getMaxPage());
             Loop loop = (Loop) questionnaire.getComponents().get(0);
             assertTrue(loop.getPaginatedLoop());
+            // Paginated loops have a "max page"
             assertEquals("1", loop.getMaxPage());
+            // Sequence mode: each component of the sequence is on the same page
             loop.getComponents().forEach(loopComponent ->
                     assertEquals("1.1", loopComponent.getPage()));
+        }
+    }
+
+    @Nested
+    class LoopOnTwoSequences {
+
+        private Questionnaire questionnaire;
+
+        @BeforeEach
+        void createLoopWithTwoSequences() {
+            questionnaire = new Questionnaire();
+            Sequence sequence1 = new Sequence();
+            sequence1.setComponentType(ComponentTypeEnum.SEQUENCE);
+            Input input1 = new Input();
+            input1.setComponentType(ComponentTypeEnum.INPUT);
+            Sequence sequence2 = new Sequence();
+            sequence2.setComponentType(ComponentTypeEnum.SEQUENCE);
+            Input input2 = new Input();
+            input2.setComponentType(ComponentTypeEnum.INPUT);
+            Loop loop = new Loop();
+            loop.setComponentType(ComponentTypeEnum.LOOP);
+            loop.setLines(new LinesLoop());
+            loop.getComponents().add(sequence1);
+            loop.getComponents().add(input1);
+            loop.getComponents().add(sequence2);
+            loop.getComponents().add(input2);
+            questionnaire.getComponents().add(loop);
+        }
+
+        @Test
+        void questionMode() {
+            //
+            new LunaticAddPageNumbers(LunaticParameters.LunaticPaginationMode.QUESTION).apply(questionnaire);
+            //
+            assertEquals("question", questionnaire.getPagination());
+            assertEquals("1", questionnaire.getMaxPage());
+            Loop loop = (Loop) questionnaire.getComponents().get(0);
+            assertFalse(loop.getPaginatedLoop());
+            // Non paginated loops don't have a "max page" property
+            assertNull(loop.getMaxPage());
+            // Non paginated loops  don't use "dotted" numbering
+            loop.getComponents().forEach(loopComponent ->
+                    assertEquals("1", loopComponent.getPage()));
+        }
+
+        @Test
+        void sequenceMode() {
+            //
+            new LunaticAddPageNumbers(LunaticParameters.LunaticPaginationMode.SEQUENCE).apply(questionnaire);
+            //
+            assertEquals("sequence", questionnaire.getPagination());
+            assertEquals("1", questionnaire.getMaxPage());
+            Loop loop = (Loop) questionnaire.getComponents().get(0);
+            assertTrue(loop.getPaginatedLoop());
+            // Paginated loops have a "max page"
+            assertEquals("2", loop.getMaxPage());
+            // Sequence mode: each component of the sequence is on the same page
+            assertEquals("1.1", loop.getComponents().get(0).getPage());
+            assertEquals("1.1", loop.getComponents().get(1).getPage());
+            assertEquals("1.2", loop.getComponents().get(2).getPage());
+            assertEquals("1.2", loop.getComponents().get(3).getPage());
         }
     }
 
