7f24b390bb26b7902ad7d4555ec8e5b4497c5a96
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2024-01-19 15:21:42 +0100

    fix: lunatic shape from (#869)
    * refactor(ddi): add loop references in variable groups mapping
    
    * refactor(loops): make some methods re-usable
    
    * fix: lunatic shape from property
    
    The previous implementation returned the the first variable of the corresponding variable group. Yet the order of variables in a variable group is not strictly enforced in current DDI specification. So this information has to be retrieved another way.
    
    * test(shape-from): update unit tests
    
    * chore: version 3.15.8

--- eno-core/src/main/java/fr/insee/eno/core/processing/out/steps/lunatic/calculatedvariable/ShapefromAttributeRetrievalFromVariableGroups.java
+++ eno-core/src/main/java/fr/insee/eno/core/processing/out/steps/lunatic/calculatedvariable/ShapefromAttributeRetrievalFromVariableGroups.java
@@ -1,8 +1,13 @@
 package fr.insee.eno.core.processing.out.steps.lunatic.calculatedvariable;
 
+import fr.insee.eno.core.exceptions.technical.MappingException;
 import fr.insee.eno.core.model.EnoQuestionnaire;
+import fr.insee.eno.core.model.navigation.Loop;
+import fr.insee.eno.core.model.question.DynamicTableQuestion;
 import fr.insee.eno.core.model.variable.Variable;
 import fr.insee.eno.core.model.variable.VariableGroup;
+import fr.insee.eno.core.processing.out.steps.lunatic.LunaticLoopResolution;
+import fr.insee.eno.core.reference.EnoIndex;
 
 import java.util.Optional;
 
@@ -14,23 +19,87 @@ import java.util.Optional;
 public class ShapefromAttributeRetrievalFromVariableGroups implements ShapefromAttributeRetrieval {
 
     /**
-     *
+     * Retrieves the Lunatic "shapeFrom" attribute. It is empty if the calculated variable with given name is not in a
+     * loop. Otherwise, it is the name of the first collected variable that is in the scope of the corresponding loop.
+     * This method uses the Eno questionnaire to retrieve this information.
      * @param lunaticCalculatedVariableName name of the calculated variable
      * @param enoQuestionnaire eno questionnaire
      * @return the first collected variable if the calculated variable is in a loop, nothing otherwise
      */
     public Optional<Variable> getShapeFrom(String lunaticCalculatedVariableName, EnoQuestionnaire enoQuestionnaire) {
+        EnoIndex enoIndex = enoQuestionnaire.getIndex();
+        return getShapeFrom(lunaticCalculatedVariableName, enoQuestionnaire, enoIndex);
+    }
+
+    // Note: the pattern with eno questionnaire and eno index is not satisfying as it currently is.
+    // It would probably better to separate the two.
+    // The first method is there so to not change the signature of the "shape from retrieval" interface for now.
+
+    // Note 2: using the eno index as little as possible to improve code maintainability in exchange
+    // for a slight loss of performance.
+
+    private Optional<Variable> getShapeFrom(String lunaticCalculatedVariableName, EnoQuestionnaire enoQuestionnaire,
+                                           EnoIndex enoIndex) {
         // retrieve the variable group in which the variable is present, excluding the "Questionnaire"
-        // variable group which does not corresponds to a "loop" variable group
+        // variable group which does not correspond to a "loop" variable group
         Optional<VariableGroup> variableGroup = enoQuestionnaire.getVariableGroups().stream()
-                .filter(vGroup -> !vGroup.getType().equals("Questionnaire"))
+                .filter(vGroup -> !vGroup.getType().equals(VariableGroup.DDI_QUESTIONNAIRE_TYPE))
                 .filter(vGroup -> vGroup.getVariableByName(lunaticCalculatedVariableName).isPresent())
                 .findFirst();
 
-        return variableGroup.flatMap(
-                        vGroup -> vGroup.getVariables().stream()
-                                .filter(variable -> variable.getCollectionType().equals(Variable.CollectionType.COLLECTED))
-                                .findFirst()
-                );
+        if (variableGroup.isEmpty())
+            return Optional.empty();
+
+        // then retrieve the "main" loop or dynamic table that corresponds to this variable group
+        String mainIterableReference = variableGroup.get().getLoopReferences().get(0);
+
+        // loop case
+        Optional<Loop> mainLoop = enoQuestionnaire.getLoops().stream()
+                .filter(loop -> mainIterableReference.equals(loop.getId()))
+                .findAny();
+        if (mainLoop.isPresent())
+            return Optional.of(findFirstCollectedVariableOfLoop(mainLoop.get(), enoQuestionnaire, enoIndex));
+
+        // dynamic table case
+        Optional<DynamicTableQuestion> dynamicTable = enoQuestionnaire.getMultipleResponseQuestions().stream()
+                .filter(DynamicTableQuestion.class::isInstance)
+                .map(DynamicTableQuestion.class::cast)
+                .filter(loop -> mainIterableReference.equals(loop.getId()))
+                .findAny();
+        if (dynamicTable.isPresent())
+            return Optional.of(findFirstCollectedVariableOfDynamicTable(dynamicTable.get(), enoQuestionnaire));
+
+        throw new MappingException(String.format(
+                "Unable to retrieve 'shapeFrom' property for the calculated variable '%s'. " +
+                        "This probably means that some information has not been mapped as expected during mapping.",
+                lunaticCalculatedVariableName));
     }
+
+    // Note: not sure why this method returns a Variable object and not a CollectedVariable, maybe it could be improved
+
+    private Variable findFirstCollectedVariableOfLoop(Loop enoLoop, EnoQuestionnaire enoQuestionnaire, EnoIndex enoIndex) {
+        String contextErrorMessage = "Unable to find its first question to retrieve Lunatic \"shapeFrom\" property.";
+        String firstQuestionVariableName = LunaticLoopResolution.findFirstResponseNameOfLoop(
+                enoLoop, enoIndex, contextErrorMessage);
+        return getVariableFromName(enoQuestionnaire, firstQuestionVariableName);
+    }
+
+    private Variable findFirstCollectedVariableOfDynamicTable(DynamicTableQuestion enoDynamicTable,
+                                                              EnoQuestionnaire enoQuestionnaire) {
+        String variableName =  enoDynamicTable.getVariableNames().get(0);
+        return getVariableFromName(enoQuestionnaire, variableName);
+    }
+
+    private static Variable getVariableFromName(EnoQuestionnaire enoQuestionnaire, String variableName) {
+        Optional<Variable> searchedVariable = enoQuestionnaire.getVariables()
+                .stream()
+                .filter(variable -> variableName.equals(variable.getName())).findAny();
+        if (searchedVariable.isEmpty())
+            throw new MappingException(String.format(
+                    "Unable to find variable with name '%s' in the Eno questionnaire variables " +
+                            "(while trying to retrieve Lunatic 'shapeFrom' property).",
+                    variableName));
+        return searchedVariable.get();
+    }
+
 }
--- eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/calculatedvariable/ShapefromAttributeRetrievalFromVariableGroupsTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/calculatedvariable/ShapefromAttributeRetrievalFromVariableGroupsTest.java
@@ -1,10 +1,17 @@
 package fr.insee.eno.core.processing.out.steps.lunatic.calculatedvariable;
 
 import fr.insee.eno.core.model.EnoQuestionnaire;
+import fr.insee.eno.core.model.navigation.StandaloneLoop;
+import fr.insee.eno.core.model.question.DynamicTableQuestion;
+import fr.insee.eno.core.model.question.TextQuestion;
+import fr.insee.eno.core.model.response.Response;
+import fr.insee.eno.core.model.sequence.Sequence;
+import fr.insee.eno.core.model.sequence.StructureItemReference;
 import fr.insee.eno.core.model.variable.CalculatedVariable;
 import fr.insee.eno.core.model.variable.CollectedVariable;
 import fr.insee.eno.core.model.variable.Variable;
 import fr.insee.eno.core.model.variable.VariableGroup;
+import fr.insee.eno.core.reference.EnoIndex;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -28,9 +35,34 @@ class ShapefromAttributeRetrievalFromVariableGroupsTest {
     void init() {
         shapefromAttributeRetrieval = new ShapefromAttributeRetrievalFromVariableGroups();
         enoQuestionnaire = new EnoQuestionnaire();
+        EnoIndex enoIndex = new EnoIndex();
+
+        StandaloneLoop loop = new StandaloneLoop();
+        loop.setId("loop-id");
+        loop.getLoopScope().add(StructureItemReference.builder()
+                .id("sequence-id")
+                .type(StructureItemReference.StructureItemType.SEQUENCE)
+                .build());
+        Sequence sequence = new Sequence();
+        sequence.setId("sequence-id");
+        sequence.getSequenceStructure().add(StructureItemReference.builder()
+                .id("question-id")
+                .type(StructureItemReference.StructureItemType.QUESTION)
+                .build());
+        TextQuestion textQuestion = new TextQuestion();
+        textQuestion.setId("question-id");
+        textQuestion.setResponse(new Response());
+        textQuestion.getResponse().setVariableName("variable2");
+
+        enoIndex.put("sequence-id", sequence);
+        enoIndex.put("question-id", textQuestion);
+        enoQuestionnaire.setIndex(enoIndex);
+        enoQuestionnaire.getLoops().add(loop);
 
         variableGroup1 = new VariableGroup();
         variableGroup1.setType("Loop");
+        variableGroup1.getLoopReferences().addAll(List.of("loop-id", "linked-loop-id"));
+        // (the linked loop reference should be ignored as only the "main" loop counts for retrieving the 'shapeFrom')
         variable1 = new CalculatedVariable();
         variable1.setName("variable1");
         variable2 = new CollectedVariable();
@@ -43,14 +75,21 @@ class ShapefromAttributeRetrievalFromVariableGroupsTest {
         variable3.setName("variable3");
         variableGroup2.getVariables().add(variable3);
 
+        DynamicTableQuestion dynamicTableQuestion = new DynamicTableQuestion();
+        dynamicTableQuestion.setId("dynamic-table-id");
+        dynamicTableQuestion.getVariableNames().add("variable4");
+        enoQuestionnaire.getMultipleResponseQuestions().add(dynamicTableQuestion);
+
         variableGroup3 = new VariableGroup();
         variableGroup3.setType("Loop");
+        variableGroup3.getLoopReferences().add("dynamic-table-id");
         variable4 = new CollectedVariable();
         variable4.setName("variable4");
         variable5 = new CalculatedVariable();
         variable5.setName("variable5");
         variableGroup3.getVariables().addAll(List.of(variable4, variable5));
 
+        enoQuestionnaire.getVariables().addAll(List.of(variable1, variable2, variable3, variable4, variable5));
         enoQuestionnaire.getVariableGroups().addAll(List.of(variableGroup1, variableGroup2, variableGroup3));
     }
 
@@ -67,13 +106,17 @@ class ShapefromAttributeRetrievalFromVariableGroupsTest {
     }
 
     @Test
-    void whenVariableInLoopVariableGroupReturnFirstCollectedVariable() {
-        Optional<Variable> variable = shapefromAttributeRetrieval.getShapeFrom("variable5", enoQuestionnaire);
-        assertTrue(variable.isPresent());
-        assertEquals("variable4", variable.get().getName());
-
-        variable = shapefromAttributeRetrieval.getShapeFrom("variable1", enoQuestionnaire);
+    void whenVariableInLoopVariableGroup_returnFirstCollectedVariable() {
+        Optional<Variable> variable = shapefromAttributeRetrieval.getShapeFrom("variable1", enoQuestionnaire);
         assertTrue(variable.isPresent());
         assertEquals("variable2", variable.get().getName());
     }
+
+    @Test
+    void whenVariableInDynamicTableVariableGroup_returnFirstCollectedVariable() {
+        Optional<Variable> variable = shapefromAttributeRetrieval.getShapeFrom("variable5", enoQuestionnaire);
+        assertTrue(variable.isPresent());
+        assertEquals("variable4", variable.get().getName());
+    }
+
 }
