968b9252c3208122a0e0d64d603f17c7395c58ec
Nicolas Senave <nicolas.senave@gmail.com>
2024-01-26 11:07:40 +0100

    docs(model): add javadoc in label objects

--- eno-core/src/main/java/fr/insee/eno/core/model/label/DynamicLabel.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/label/DynamicLabel.java
@@ -12,17 +12,26 @@ import lombok.Setter;
 
 import static fr.insee.eno.core.annotations.Contexts.Context;
 
+/**
+ * Label object used e.g. in questions, declarations or control messages.
+ * This class is designed to map DDI "DynamicText" content.
+ * @see Label
+ */
 @Getter
 @Setter
 @Context(format = Format.DDI, type = DynamicTextType.class)
 @Context(format = Format.LUNATIC, type = LabelType.class)
 public class DynamicLabel extends EnoObject implements EnoLabel {
 
+    /** Label content.
+     * @see Label for details.
+     */
     @DDI("getTextContentArray(0).getText().getStringValue()")
     @Lunatic("setValue(#param)")
     String value;
 
-    /** For now, Lunatic type in label objects does not come from metadata, but is hardcoded here in Eno.
+    /** Property that is specific to Lunatic.
+     * For now, Lunatic type in label objects does not come from metadata, but is hardcoded here in Eno.
      * See labels documentation. */
     @Lunatic("setType(#param)")
     String type = LabelTypeEnum.VTL_MD.value();
--- eno-core/src/main/java/fr/insee/eno/core/model/label/QuestionnaireLabel.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/label/QuestionnaireLabel.java
@@ -12,18 +12,24 @@ import reusable33.InternationalStringType;
 
 import static fr.insee.eno.core.annotations.Contexts.Context;
 
-/** Label object at the questionnaire level. */
+/** Label object used at the questionnaire level.
+ * This class is designed to map DDI "InternationalString" content.
+ * @see Label */
 @Getter
 @Setter
 @Context(format = Format.DDI, type = InternationalStringType.class)
 @Context(format = Format.LUNATIC, type = LabelType.class)
 public class QuestionnaireLabel extends EnoObject implements EnoLabel {
 
+    /** Label content.
+     * @see Label for details.
+     */
     @DDI("getStringArray(0).getStringValue()")
     @Lunatic("setValue(#param)")
     String value;
 
-    /** For now, Lunatic type does not come from metadata, but is hardcoded here in Eno.
+    /** Property that is specific to Lunatic.
+     * For now, Lunatic type does not come from metadata, but is hardcoded here in Eno.
      * See labels documentation. */
     @Lunatic("setType(T(fr.insee.lunatic.model.flat.LabelTypeEnum).fromValue(#param))")
     String type = LabelTypeEnum.VTL_MD.value();
