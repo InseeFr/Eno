04f383fafec8bd2cc6428b87c22231137a791ec2
Nicolas Senave <nicolas.senave@gmail.com>
2024-01-26 11:07:40 +0100

    docs(model): add javadoc in label objects

--- eno-core/src/main/java/fr/insee/eno/core/model/label/EnoLabel.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/label/EnoLabel.java
@@ -1,5 +1,7 @@
 package fr.insee.eno.core.model.label;
 
+/** Interface to allow polymorphism between label objects.
+ * @see Label */
 public interface EnoLabel {
 
     String getValue();
--- eno-core/src/main/java/fr/insee/eno/core/model/label/Label.java
+++ eno-core/src/main/java/fr/insee/eno/core/model/label/Label.java
@@ -11,18 +11,29 @@ import reusable33.LabelType;
 
 import static fr.insee.eno.core.annotations.Contexts.Context;
 
-/** Label object that is used in components */
+/** Label object that is used e.g. in sequences or code lists.
+ * This class is designed to map DDI "Label" objects.
+ * There are several classes for different kinds of label in DDI, but use cases are the same for each of them.
+ * In Lunatic, a label is always interpreted as a VTL expression (with eventually Markdown syntax being also
+ * interpreted afterward).
+ * Note that each kind of label correspond to a VTL expression in Lunatic. Thus, the value of a label is expected to
+ * be a valid VTL expression. This especially implies that a static label should start and end with a quote character.
+ */
 @Getter
 @Setter
 @Context(format = Format.DDI, type = LabelType.class)
 @Context(format = Format.LUNATIC, type = fr.insee.lunatic.model.flat.LabelType.class)
 public class Label extends EnoObject implements EnoLabel {
 
+    /** Text content of the label, which can be either static or dynamic.
+     * In DDI, if the label contains variables, their names are replaced by references.
+     * There is a processing class to resolve the references and put back variable names instead. */
     @DDI("getContentArray(0).getStringValue()")
     @Lunatic("setValue(#param)")
     String value;
 
-    /** For now, Lunatic type in label objects does not come from metadata, but is hardcoded here in Eno.
+    /** Property that is specific to Lunatic.
+     * For now, Lunatic type in label objects does not come from metadata, but is hardcoded here in Eno.
      * See labels documentation. */
     @Lunatic("setType(T(fr.insee.lunatic.model.flat.LabelTypeEnum).fromValue(#param))")
     String type = LabelTypeEnum.VTL_MD.value();
