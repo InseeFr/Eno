936084d88f69632b4f5ca45b02c43731eb927347
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2024-03-20 10:58:20 +0100

    feat: suggester (#925)
    * chore: fix local dependencies resolution
    
    For some reason putting maven local first causes issues with jaxb exposed by pogues-model
    
    * build: bump lunatic-model
    
    To have the Lunatic Suggester component.
    
    * test(suggester): example questionnaire
    
    * feat(suggester): ddi mapping (wip)
    
    * build(suggester): add jackson xml serialization lib
    
    * refactor: add exception for DDI illegal content
    
    * feat(suggester): add dto classes to deserialize suggester configuration
    
    * feat(suggester): deserializer for suggester configuration
    
    * feat(suggester): suggester configuration ddi mapping
    
    * test(suggester): more tests on ddi mapping
    
    * feat(suggester): lunatic suggester component mapping
    
    * test(suggester): suggester components tests
    
    * feat(suggester): lunatic suggesters configuration
    
    * fix(suggester): update lunatic and pogues model libs
    
    * refactor(suggester): remove specific treatment
    
    * fix: ddi code list label and name
    
    * chore: snapshot version
    
    Note: Suggester specific treatment has not been removed yet to ensure backward compatibility while Pogues UI doesn't provide the integrated suggester feature.

---
+++ eno-core/src/main/java/fr/insee/eno/core/exceptions/business/IllegalDDIElementException.java
@@ -0,0 +1,9 @@
+package fr.insee.eno.core.exceptions.business;
+
+public class IllegalDDIElementException extends RuntimeException {
+
+    public IllegalDDIElementException(String message) {
+        super(message);
+    }
+
+}
---
+++ eno-core/src/test/java/fr/insee/eno/core/mapping/out/lunatic/SuggesterQuestionTest.java
@@ -0,0 +1,67 @@
+package fr.insee.eno.core.mapping.out.lunatic;
+
+import fr.insee.eno.core.exceptions.business.DDIParsingException;
+import fr.insee.eno.core.mappers.DDIMapper;
+import fr.insee.eno.core.mappers.LunaticMapper;
+import fr.insee.eno.core.model.EnoQuestionnaire;
+import fr.insee.eno.core.processing.in.steps.ddi.DDIDeserializeSuggesterConfiguration;
+import fr.insee.eno.core.serialize.DDIDeserializer;
+import fr.insee.lunatic.model.flat.ComponentTypeEnum;
+import fr.insee.lunatic.model.flat.Questionnaire;
+import fr.insee.lunatic.model.flat.Suggester;
+import instance33.DDIInstanceDocument;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SuggesterQuestionTest {
+
+    @Test
+    void suggester_integrationTest() throws DDIParsingException {
+        //
+        DDIInstanceDocument ddiInstanceDocument = DDIDeserializer.deserialize(
+                this.getClass().getClassLoader().getResourceAsStream(
+                        "integration/ddi/ddi-suggester.xml"));
+        //
+        DDIMapper ddiMapper = new DDIMapper();
+        EnoQuestionnaire enoQuestionnaire = new EnoQuestionnaire();
+        ddiMapper.mapDDI(ddiInstanceDocument, enoQuestionnaire);
+        new DDIDeserializeSuggesterConfiguration().apply(enoQuestionnaire);
+        //
+        LunaticMapper lunaticMapper = new LunaticMapper();
+        Questionnaire lunaticQuestionnaire = new Questionnaire();
+        lunaticMapper.mapQuestionnaire(enoQuestionnaire, lunaticQuestionnaire);
+
+        //
+        Map<String, Suggester> suggesterComponents = new HashMap<>();
+        lunaticQuestionnaire.getComponents().stream()
+                .filter(Suggester.class::isInstance)
+                .map(Suggester.class::cast)
+                .forEach(suggester -> {
+                    assertEquals(ComponentTypeEnum.SUGGESTER, suggester.getComponentType());
+                    suggesterComponents.put(suggester.getId(), suggester);
+                });
+        //
+        assertEquals(4, suggesterComponents.size());
+        //
+        Suggester suggester1 = suggesterComponents.get("lrueihko");
+        assertEquals("L_ACTIVITES-1-0-0", suggester1.getStoreName());
+        assertEquals("ACTIVITE", suggester1.getResponse().getName());
+        //
+        Suggester suggester2 = suggesterComponents.get("lruent2x");
+        assertEquals("L_COMMUNEPASSEE-1-2-0", suggester2.getStoreName());
+        assertEquals("COMMUNE", suggester2.getResponse().getName());
+        //
+        Suggester suggester3 = suggesterComponents.get("lrueotzf");
+        assertEquals("L_DEPNAIS-1-1-0", suggester3.getStoreName());
+        assertEquals("DPARTEMENT", suggester3.getResponse().getName());
+        //
+        Suggester suggester4 = suggesterComponents.get("lruexn64");
+        assertEquals("L_DIPLOMES-1-0-0", suggester4.getStoreName());
+        assertEquals("DIPLOMES", suggester4.getResponse().getName());
+    }
+
+}
