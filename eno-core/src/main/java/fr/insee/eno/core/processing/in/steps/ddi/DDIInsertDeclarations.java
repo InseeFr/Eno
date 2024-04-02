194a9c4f8625ea8521d1b20934c9f86867978fdd
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2024-04-02 15:31:14 +0200

    * fix: ddi before question declarations insertion (#948)
    When a question is in the scope of a loop or a filter, before question declarations were not inserted before this.

--- eno-core/src/main/java/fr/insee/eno/core/processing/in/steps/ddi/DDIInsertDeclarations.java
+++ eno-core/src/main/java/fr/insee/eno/core/processing/in/steps/ddi/DDIInsertDeclarations.java
@@ -1,58 +1,97 @@
 package fr.insee.eno.core.processing.in.steps.ddi;
 
+import fr.insee.eno.core.exceptions.business.IllegalDDIElementException;
+import fr.insee.eno.core.exceptions.technical.MappingException;
 import fr.insee.eno.core.model.EnoQuestionnaire;
 import fr.insee.eno.core.model.declaration.Declaration;
+import fr.insee.eno.core.model.navigation.Filter;
+import fr.insee.eno.core.model.navigation.Loop;
 import fr.insee.eno.core.model.question.Question;
+import fr.insee.eno.core.model.sequence.AbstractSequence;
 import fr.insee.eno.core.model.sequence.ItemReference;
 import fr.insee.eno.core.model.sequence.Sequence;
 import fr.insee.eno.core.model.sequence.Subsequence;
 import fr.insee.eno.core.processing.ProcessingStep;
+import fr.insee.eno.core.reference.EnoIndex;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class DDIInsertDeclarations implements ProcessingStep<EnoQuestionnaire> {
 
+    private final EnoIndex enoIndex;
+
+    public DDIInsertDeclarations(EnoIndex enoIndex) {
+        this.enoIndex = enoIndex;
+    }
+
     /** Same idea as in the DDI insert controls processing, but for declarations.
-     * (Declarations are placed before the object they belong to in the sequence items lists.)
-     * Concerned objects : subsequences and questions. */
-    public void apply(EnoQuestionnaire enoQuestionnaire) { // Note: code is a bit clumsy but works
-        //
-        assert enoQuestionnaire.getIndex() != null;
-        //
+     * (Declarations are placed before the object they belong to in the sequence items lists.) */
+    @Override
+    public void apply(EnoQuestionnaire enoQuestionnaire) {
         for (Sequence sequence : enoQuestionnaire.getSequences()) {
             List<String> declarationIdStack = new ArrayList<>();
-            for (ItemReference sequenceItem : sequence.getSequenceItems()) {
-                if (sequenceItem.getType() == ItemReference.ItemType.DECLARATION) {
-                    declarationIdStack.add(sequenceItem.getId());
-                }
-                if (sequenceItem.getType() == ItemReference.ItemType.QUESTION) {
-                    Question question = (Question) enoQuestionnaire.get(sequenceItem.getId());
-                    declarationIdStack.forEach(declarationId ->
-                            question.getDeclarations().add(
-                                    (Declaration) enoQuestionnaire.get(declarationId)));
-                    declarationIdStack = new ArrayList<>();
-                }
-                if (sequenceItem.getType() == ItemReference.ItemType.SUBSEQUENCE) {
-                    Subsequence subsequence = (Subsequence) enoQuestionnaire.get(sequenceItem.getId());
-                    declarationIdStack.forEach(declarationId ->
-                            subsequence.getDeclarations().add(
-                                    (Declaration) enoQuestionnaire.get(declarationId)));
-                    declarationIdStack = new ArrayList<>();
-                    for (ItemReference subsequenceItem : subsequence.getSequenceItems()) {
-                        if (subsequenceItem.getType() == ItemReference.ItemType.DECLARATION) {
-                            declarationIdStack.add(subsequenceItem.getId());
-                        }
-                        if (subsequenceItem.getType() == ItemReference.ItemType.QUESTION) {
-                            Question question = (Question) enoQuestionnaire.get(subsequenceItem.getId());
-                            declarationIdStack.forEach(declarationId ->
-                                    question.getDeclarations().add(
-                                            (Declaration) enoQuestionnaire.get(declarationId)));
-                            declarationIdStack = new ArrayList<>();
-                        }
-                    }
-                }
+            insertDeclarationsFromSequence(sequence, declarationIdStack);
+        }
+    }
+
+    private void insertDeclarationsFromSequence(AbstractSequence sequence, List<String> declarationIdStack) {
+        for (ItemReference sequenceItem : sequence.getSequenceItems()) {
+            insertDeclarations(sequenceItem, declarationIdStack);
+        }
+    }
+
+    private void insertDeclarationsFromFilter(Filter filter, List<String> declarationIdStack) {
+        for (ItemReference filterItem : filter.getFilterItems()) {
+            insertDeclarations(filterItem, declarationIdStack);
+        }
+    }
+
+    private void insertDeclarationsFromLoop(Loop loop, List<String> declarationIdStack) {
+        for (ItemReference loopItem : loop.getLoopItems()) {
+            insertDeclarations(loopItem, declarationIdStack);
+        }
+    }
+
+    private void insertDeclarations(ItemReference itemReference, List<String> declarationIdStack) {
+        switch (itemReference.getType()) {
+            case SEQUENCE -> {
+                log.error("Error while inserting DDI StatementItem declarations.");
+                throw new MappingException(
+                        "A DDI sequence cannot contain another sequence (id=" + itemReference.getId() + ").");
             }
+            case SUBSEQUENCE -> {
+                noDeclarationCheck(declarationIdStack);
+                Subsequence subsequence = (Subsequence) enoIndex.get(itemReference.getId());
+                insertDeclarationsFromSequence(subsequence, declarationIdStack);
+            }
+            case LOOP -> {
+                noDeclarationCheck(declarationIdStack);
+                Loop loop = (Loop) enoIndex.get(itemReference.getId());
+                insertDeclarationsFromLoop(loop, declarationIdStack);
+            }
+            case FILTER -> {
+                noDeclarationCheck(declarationIdStack);
+                Filter filter = (Filter) enoIndex.get(itemReference.getId());
+                insertDeclarationsFromFilter(filter, declarationIdStack);
+            }
+            case QUESTION -> {
+                Question question = (Question) enoIndex.get(itemReference.getId());
+                declarationIdStack.forEach(declarationId ->
+                        question.getDeclarations().add(
+                                (Declaration) enoIndex.get(declarationId)));
+                declarationIdStack.clear();
+            }
+            case DECLARATION -> declarationIdStack.add(itemReference.getId());
+            case CONTROL -> log.debug("Control are ignored while inserting declarations in questions.");
+        }
+    }
+
+    private void noDeclarationCheck(List<String> declarationIdStack) {
+        if (! declarationIdStack.isEmpty()) {
+            throw new IllegalDDIElementException("A DDI StatementItem can only be used in a question.");
         }
     }
 
