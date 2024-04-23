0732daf87be05c778fe75b2f2672b17eb8a62544
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2024-04-23 10:51:35 +0200

    feat: add cors for Eno-WS (#973)
    * feat: add cors for Eno-WS
    
    * Update application.properties

---
+++ eno-ws/src/main/java/fr/insee/eno/ws/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package fr.insee.eno.ws.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.List;
+
+/** Cors configuration */
+@Configuration
+@AllArgsConstructor
+public class CorsConfig {
+    @Value("${eno.corsOrigins}")
+    private List<String> corsOrigins;
+    @Bean
+    protected CorsWebFilter corsWebFilter () {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(corsOrigins);
+        configuration.setAllowedMethods(List.of("GET", "PUT", "POST", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.addExposedHeader("Content-Disposition");
+        configuration.setMaxAge(3600L);
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsWebFilter(source);
+    }
+}
