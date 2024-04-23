b070645047f69896402c3384829a5de7311e55b1
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2024-04-23 09:56:18 +0200

    feat: other specify modality (#980)
    * refactor: clean up DDIConverter
    
    * feat(other specify): ddi unique choice conversion
    
    * test(other specify): multiple choice conversion
    
    * feat(other specify): update ddi unique choice question mapping
    
    Code response domain is in a mixed response domain list when there are modalities with detail response.
    
    * feat(other specify): ddi unique choice mapping
    
    * feat(other specify): unique choice ddi detail response
    
    * feat(other specify): ddi multiple choice details mapping
    
    Also includes quite a lot of refactoring on the mapping and processing of multiple choice questions.
    
    * chore: bump version
    
    * feat(other specify): lunatic mapping

---
+++ eno-core/src/main/java/fr/insee/eno/core/converter/DDIVariableConversion.java
@@ -0,0 +1,38 @@
+package fr.insee.eno.core.converter;
+
+import fr.insee.eno.core.model.EnoObject;
+import fr.insee.eno.core.model.variable.CalculatedVariable;
+import fr.insee.eno.core.model.variable.CollectedVariable;
+import fr.insee.eno.core.model.variable.ExternalVariable;
+import logicalproduct33.VariableType;
+
+public class DDIVariableConversion {
+
+    private DDIVariableConversion() {}
+
+    /** <p>In "Insee" DDI:</p>
+     * <ul>
+     *   <li>collected variables are characterized by having a "question reference"</li>
+     *   <li>calculated variables are characterized by having a "processing instruction reference"
+     *   in their "variable representation"</li>
+     *   <li>external variables have no specific characteristics so these are the remaining cases. </li>
+     * </ul>
+     * */
+    static EnoObject instantiateFrom(VariableType variableType) {
+        if (isCollectedVariable(variableType))
+            return new CollectedVariable();
+        if (isCalculatedVariable(variableType))
+            return new CalculatedVariable();
+        return new ExternalVariable();
+    }
+
+    private static boolean isCollectedVariable(VariableType ddiVariable) {
+        return ! ddiVariable.getQuestionReferenceList().isEmpty();
+    }
+
+    private static boolean isCalculatedVariable(VariableType ddiVariable) {
+        return ddiVariable.getVariableRepresentation() != null &&
+                ddiVariable.getVariableRepresentation().getProcessingInstructionReference() != null;
+    }
+
+}
