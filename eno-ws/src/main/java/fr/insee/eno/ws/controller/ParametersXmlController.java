00e7b7b79fd8a0757b4330a7a7522202ada64cb3
nsenave <nicolas.senave@gmail.com>
2024-07-18 01:30:47 +0200

    refactor: synchronous calls for parameter endpoints (wip)

--- eno-ws/src/main/java/fr/insee/eno/ws/controller/ParametersXmlController.java
+++ eno-ws/src/main/java/fr/insee/eno/ws/controller/ParametersXmlController.java
@@ -2,20 +2,18 @@ package fr.insee.eno.ws.controller;
 
 import fr.insee.eno.legacy.parameters.Context;
 import fr.insee.eno.legacy.parameters.Mode;
-import fr.insee.eno.ws.PassThrough;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import reactor.core.publisher.Mono;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Tag(name="Parameters (Eno Xml)")
 @Controller
@@ -25,10 +23,10 @@ public class ParametersXmlController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ParametersXmlController.class);
 
-	private final PassThrough passePlat;
+	private final WebClient webClient;
 
-	public ParametersXmlController(PassThrough passePlat) {
-		this.passePlat = passePlat;
+	public ParametersXmlController(WebClient webClient) {
+		this.webClient = webClient;
 	}
 
 	@Operation(
@@ -36,8 +34,13 @@ public class ParametersXmlController {
 			description= "Return the default parameters file for Eno Xml. This file cannot be used directly: " +
 					"you have to fill the `Pipeline` section according to the desired transformation.")
 	@GetMapping(value="all", produces=MediaType.APPLICATION_OCTET_STREAM_VALUE)
-	public Mono<Void> getAllXmlParameters(ServerHttpRequest request, ServerHttpResponse response) {
-		return passePlat.passePlatGet(request, response);
+	public ResponseEntity<String> getAllXmlParameters() {
+		String responseBody = webClient.get()
+				.uri("parameters/xml/all")
+				.accept(MediaType.APPLICATION_OCTET_STREAM)
+				.exchangeToMono(clientResponse -> clientResponse.bodyToMono(String.class))
+				.block();
+		return ResponseEntity.ok(responseBody);
 	}
 
 	@Operation(
@@ -45,12 +48,19 @@ public class ParametersXmlController {
 			description = "Returns a `xml` parameters file with standard values, in function of context and mode, " +
 					"for the concerned out format, to be used in _Eno Xml_ services that require a parameters file.")
 	@GetMapping(value="{context}/{outFormat}", produces=MediaType.APPLICATION_OCTET_STREAM_VALUE)
-	public Mono<Void> getXmlParameters(
+	public ResponseEntity<String> getXmlParameters(
 			@PathVariable Context context,
 			@PathVariable fr.insee.eno.legacy.parameters.OutFormat outFormat,
-			@RequestParam(value="Mode",required=false) Mode mode,
-			ServerHttpRequest request, ServerHttpResponse response) {
-		return passePlat.passePlatGet(request, response);
+			@RequestParam(value="Mode",required=false) Mode mode) {
+		String responseBody = webClient.get()
+				.uri(uriBuilder -> uriBuilder
+						.path("parameters/xml/{context}/{outFormat}")
+						.queryParam("Mode", mode)
+						.build(context, outFormat))
+				.accept(MediaType.APPLICATION_OCTET_STREAM)
+				.exchangeToMono(clientResponse -> clientResponse.bodyToMono(String.class))
+				.block();
+		return ResponseEntity.ok(responseBody);
 	}
 
 }
