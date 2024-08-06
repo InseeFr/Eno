bc8eabdbc2043fb15ddacb6a14a4c09d6e6137aa
Nicolas Senave <nicolas.senave@gmail.com>
2024-08-06 11:47:23 +0200

    feat: improve generation of filter result variables

--- eno-core/src/main/java/fr/insee/eno/core/processing/out/steps/lunatic/LunaticFilterResult.java
+++ eno-core/src/main/java/fr/insee/eno/core/processing/out/steps/lunatic/LunaticFilterResult.java
@@ -1,90 +1,176 @@
 package fr.insee.eno.core.processing.out.steps.lunatic;
 
+import fr.insee.eno.core.exceptions.technical.MappingException;
 import fr.insee.eno.core.model.EnoQuestionnaire;
 import fr.insee.eno.core.model.calculated.BindingReference;
 import fr.insee.eno.core.model.navigation.ComponentFilter;
 import fr.insee.eno.core.model.question.Question;
 import fr.insee.eno.core.processing.ProcessingStep;
-import fr.insee.eno.core.processing.out.steps.lunatic.calculatedvariable.ShapefromAttributeRetrieval;
+import fr.insee.eno.core.reference.EnoIndex;
 import fr.insee.lunatic.model.flat.*;
 import fr.insee.lunatic.model.flat.variable.CalculatedVariableType;
+import fr.insee.lunatic.model.flat.variable.CollectedVariableType;
 import fr.insee.lunatic.model.flat.variable.VariableTypeEnum;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
+
+import static fr.insee.eno.core.utils.LunaticUtils.getDirectResponseNames;
 
 public class LunaticFilterResult implements ProcessingStep<Questionnaire> {
 
-    private final EnoQuestionnaire enoQuestionnaire;
-    private final ShapefromAttributeRetrieval shapefromAttributeRetrieval;
-    public static final String FILTER_RESULT_PREFIX = "FILTER_RESULT_";
+    private static final String FILTER_RESULT_PREFIX = "FILTER_RESULT_";
 
-    public LunaticFilterResult(EnoQuestionnaire enoQuestionnaire, ShapefromAttributeRetrieval shapefromAttributeRetrieval) {
-        this.enoQuestionnaire = enoQuestionnaire;
-        this.shapefromAttributeRetrieval = shapefromAttributeRetrieval;
+    private final EnoIndex enoIndex;
+
+    private Map<String, CollectedVariableType> collectedVariableMap;
+
+    public LunaticFilterResult(EnoQuestionnaire enoQuestionnaire) {
+        this.enoIndex = enoQuestionnaire.getIndex();
     }
 
     @Override
     public void apply(Questionnaire lunaticQuestionnaire) {
-        List<CalculatedVariableType> calculatedVariables = generateFilterVariables(lunaticQuestionnaire.getComponents());
-        lunaticQuestionnaire.getVariables().addAll(calculatedVariables);
+        //
+        indexVariables(lunaticQuestionnaire);
+        //
+        lunaticQuestionnaire.getVariables().addAll(
+                generateFilterResultVariables(lunaticQuestionnaire.getComponents()));
     }
 
     /**
-     * Generate filter result calculated variables from a component list
-     * @param components list of components
-     * @return filter result calculated variables
+     * Indexes the collected variables of the given questionnaire in the local map object.
+     * @param lunaticQuestionnaire Lunatic questionnaire.
      */
-    private List<CalculatedVariableType> generateFilterVariables(List<ComponentType> components) {
-        List<CalculatedVariableType> filterVariables = new ArrayList<>();
+    private void indexVariables(Questionnaire lunaticQuestionnaire) {
+        collectedVariableMap = new HashMap<>();
+        lunaticQuestionnaire.getVariables().stream()
+                .filter(CollectedVariableType.class::isInstance).map(CollectedVariableType.class::cast)
+                .forEach(collectedVariable -> collectedVariableMap.put(collectedVariable.getName(), collectedVariable));
+    }
+
+    /**
+     * Iterates on components given to generate the "filter result" variables. If a component is nesting other
+     * components, the method iterates recursively on the inner components.
+     * @param components List of Lunatic components.
+     * @return The list of filter result variables for the components.
+     */
+    private List<CalculatedVariableType> generateFilterResultVariables(List<ComponentType> components) {
+        List<CalculatedVariableType> filterResultVariables = new ArrayList<>();
         for(ComponentType component : components) {
-            generateFilterVariable(component)
-                    .ifPresent(filterVariables::add);
-            if(component instanceof ComponentNestingType nestingComponent) {
-                filterVariables.addAll(generateFilterVariables(nestingComponent.getComponents()));
+            // If the component is a nesting component, recursion on its inner components.
+            if (component instanceof ComponentNestingType nestingComponent) {
+                filterResultVariables.addAll(generateFilterResultVariables(nestingComponent.getComponents()));
+                continue;
             }
+            //
+            filterResultVariables.addAll(generateFilterResultVariables(component));
         }
-        return filterVariables;
+        return filterResultVariables;
     }
 
     /**
-     * generate a filter result calculated variable for a component
-     * @param component generate a filter variable from this component
-     * @return a filter variable if the component is a question with condition filter, otherwise none
+     * Iterates on responses contained in the component to generate its "filter result" variables.
+     * @param component Lunatic response component.
+     * @return List of filter result variables for the component.
      */
-    private Optional<CalculatedVariableType> generateFilterVariable(ComponentType component) {
-        if(!isQuestionWithConditionFilter(component)) {
-            return Optional.empty();
-        }
+    private List<CalculatedVariableType> generateFilterResultVariables(ComponentType component) {
+        assert !(component instanceof ComponentNestingType); // Safety check (should never happen by design)
+        // If the component has no condition filter, there is no filter variable to generate
+        if (component.getConditionFilter() == null)
+            return new ArrayList<>();
+        //
+        return getDirectResponseNames(component).stream()
+                .map(responseName -> generateFilterResultVariable(responseName, component))
+                .toList();
+    }
 
-        CalculatedVariableType filterVariable = new CalculatedVariableType();
-        filterVariable.setVariableType(VariableTypeEnum.CALCULATED);
-        Question question = (Question) enoQuestionnaire.getIndex().get(component.getId());
-        String questionName = question.getName();
-        filterVariable.setName(FILTER_RESULT_PREFIX + questionName);
-        shapefromAttributeRetrieval.getShapeFrom(questionName, enoQuestionnaire)
-                .ifPresent(shapeFromVariable -> filterVariable.setShapeFrom(shapeFromVariable.getName()));
-        ConditionFilterType conditionFilter = component.getConditionFilter();
-        ComponentFilter componentFilter = question.getComponentFilter();
-        if(componentFilter != null && !componentFilter.getBindingReferences().isEmpty()) {
-            componentFilter.getBindingReferences().stream()
-                    .map(BindingReference::getVariableName)
-                    .forEach(variableName -> filterVariable.getBindingDependencies().add(variableName));
-        }
+    /**
+     * Generates the "filter result" variable corresponding to the given response. The corresponding component is
+     * passed since it is used to find the necessary information.
+     * @param responseName Response name.
+     * @param component Lunatic component that holds the given response name.
+     * @return The filter result variable for the given response name.
+     */
+    private CalculatedVariableType generateFilterResultVariable(String responseName, ComponentType component) {
+        assert component.getConditionFilter() != null ; // Safety check (should never happen by design)
+        CalculatedVariableType filterResultVariable = new CalculatedVariableType();
+        filterResultVariable.setVariableType(VariableTypeEnum.CALCULATED);
+        setName(filterResultVariable, responseName);
+        setExpression(filterResultVariable, component.getConditionFilter());
+        setBindingDependencies(filterResultVariable, findEnoFilter(component));
+        setDimensionProperties(filterResultVariable, findResponseVariable(responseName));
+        return filterResultVariable;
+    }
+
+    private CollectedVariableType findResponseVariable(String responseName) {
+        CollectedVariableType responseVariable = collectedVariableMap.get(responseName);
+        if (responseVariable == null)
+            throw new MappingException(String.format(
+                    "Unable to find variable corresponding to response '%s'.", responseName));
+        return responseVariable;
+    }
+
+    private ComponentFilter findEnoFilter(ComponentType lunaticComponent) {
+        String questionId = lunaticComponent.getId();
+        var enoObject = enoIndex.get(questionId);
+        if (enoObject == null)
+            throw new MappingException(String.format(
+                    "Unable to retrieve Eno object corresponding to Lunatic component '%s'.", questionId));
+        if (! (enoObject instanceof Question enoQuestion))
+            throw new MappingException(String.format(
+                    "Eno object of type '%s' associated with Lunatic component '%s' is not a question.",
+                    enoObject.getClass(), lunaticComponent));
+        return enoQuestion.getComponentFilter();
+    }
+
+    /**
+     * Sets the name of the filter result variable.
+     * @param filterResultVariable Lunatic filter result variable.
+     * @param responseName Response name.
+     */
+    private static void setName(CalculatedVariableType filterResultVariable, String responseName) {
+        filterResultVariable.setName(FILTER_RESULT_PREFIX + responseName);
+    }
+
+    /**
+     * Sets the filter result variable expression, which is the expression defined in the condition filter object.
+     * @param filterResultVariable Lunatic filter result variable.
+     * @param lunaticFilter Lunatic condition filter object.
+     */
+    private static void setExpression(CalculatedVariableType filterResultVariable, ConditionFilterType lunaticFilter) {
         LabelType expression = new LabelType();
-        expression.setType(conditionFilter.getType());
-        expression.setValue(conditionFilter.getValue());
-        filterVariable.setExpression(expression);
-        return Optional.of(filterVariable);
+        expression.setValue(lunaticFilter.getValue());
+        expression.setType(lunaticFilter.getType());
+        filterResultVariable.setExpression(expression);
     }
 
     /**
-     * check if a component is a question with a condition filter
-     * @param component component to check
-     * @return true if it is a question with condition filter, false otherwise
+     * Sets the filter result variable binding dependencies, using the information in the Eno filter object.
+     * @param filterResultVariable Lunatic filter result variable.
+     * @param enoFilter Eno component filter object.
      */
-    private boolean isQuestionWithConditionFilter(ComponentType component) {
-        return component.getConditionFilter() != null && (component instanceof ComponentSimpleResponseType || component instanceof ComponentMultipleResponseType);
+    private static void setBindingDependencies(CalculatedVariableType filterResultVariable, ComponentFilter enoFilter) {
+        enoFilter.getBindingReferences().stream()
+                .map(BindingReference::getVariableName)
+                .forEach(variableName -> filterResultVariable.getBindingDependencies().add(variableName));
     }
+
+    // Note: both expression and binding dependencies could be retrieved from the Eno filter object,
+    // but I try to use information that is directly in the Lunatic questionnaire as much as possible.
+
+    /**
+     * Sets dimension properties of the filter result variable, that are the same as the corresponding response
+     * variable.
+     * @param filterResultVariable Lunatic filter result variable.
+     * @param responseVariable Lunatic response variable corresponding to the filter result variable.
+     */
+    private static void setDimensionProperties(CalculatedVariableType filterResultVariable, CollectedVariableType responseVariable) {
+        filterResultVariable.setDimension(responseVariable.getDimension());
+        filterResultVariable.setIterationReference(responseVariable.getIterationReference());
+        // Note: the shape from property is not computed here, see the "shape from" processing step.
+    }
+
 }
--- eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/LunaticFilterResultTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/LunaticFilterResultTest.java
@@ -5,13 +5,9 @@ import fr.insee.eno.core.model.calculated.BindingReference;
 import fr.insee.eno.core.model.navigation.ComponentFilter;
 import fr.insee.eno.core.model.question.TableQuestion;
 import fr.insee.eno.core.model.question.TextQuestion;
-import fr.insee.eno.core.model.variable.VariableGroup;
-import fr.insee.eno.core.processing.out.steps.lunatic.calculatedvariable.ShapefromAttributeRetrievalReturnVariableNameInVariable;
 import fr.insee.eno.core.reference.EnoIndex;
 import fr.insee.lunatic.model.flat.*;
-import fr.insee.lunatic.model.flat.variable.CalculatedVariableType;
-import fr.insee.lunatic.model.flat.variable.VariableType;
-import fr.insee.lunatic.model.flat.variable.VariableTypeEnum;
+import fr.insee.lunatic.model.flat.variable.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,67 +19,74 @@ class LunaticFilterResultTest {
 
     LunaticFilterResult processing;
     Questionnaire lunaticQuestionnaire;
-    EnoQuestionnaire enoQuestionnaire;
-    EnoIndex enoIndex;
     Input input;
-    TextQuestion textQuestion;
     Table table;
-    TableQuestion tableQuestion;
-    VariableGroup variableGroup;
+    CollectedVariableType inputVariable;
+    CollectedVariableType tableVariable1;
+    CollectedVariableType tableVariable2;
 
     @BeforeEach
     void init() {
-        enoQuestionnaire = new EnoQuestionnaire();
-        enoIndex = new EnoIndex();
-        textQuestion = new TextQuestion();
-        textQuestion.setName("input-name");
-        textQuestion.setId("lxcsdfg");
-        ComponentFilter textQuestionFilter = new ComponentFilter();
-        textQuestionFilter.setValue("count(CALCULATED_VARIABLE) < 2");
-        textQuestionFilter.setType(LabelTypeEnum.VTL.value());
-        BindingReference textFilterBindingRef = new BindingReference("idref", "CALCULATED_VARIABLE");
-        textQuestionFilter.getBindingReferences().add(textFilterBindingRef);
-        textQuestion.setComponentFilter(textQuestionFilter);
-        enoIndex.put(textQuestion.getId(), textQuestion);
+        EnoQuestionnaire enoQuestionnaire = new EnoQuestionnaire();
+        EnoIndex enoIndex = new EnoIndex();
 
-        tableQuestion = new TableQuestion();
-        tableQuestion.setName("table-name");
-        tableQuestion.setId("xdsfgh54");
-        BindingReference tableFilterBindingRef1 = new BindingReference("idreft1", "CALCULATED_VARIABLE_2");
-        BindingReference tableFilterBindingRef2 = new BindingReference("idreft2", "NUMERIC_USED_FOR_CALCULATED_VARIABLE_2");
+        TextQuestion textQuestion = new TextQuestion();
+        textQuestion.setId("input-id");
+        ComponentFilter textQuestionFilter = new ComponentFilter();
+        textQuestionFilter.getBindingReferences().add(new BindingReference("ref-1", "VAR1"));
+        textQuestion.setComponentFilter(textQuestionFilter);
+        enoIndex.put(textQuestion);
+
+        TableQuestion tableQuestion = new TableQuestion();
+        tableQuestion.setId("table-id");
         ComponentFilter tableQuestionFilter = new ComponentFilter();
-        tableQuestionFilter.getBindingReferences().add(tableFilterBindingRef1);
-        tableQuestionFilter.getBindingReferences().add(tableFilterBindingRef2);
+        tableQuestionFilter.getBindingReferences().add(new BindingReference("ref-2-1", "VAR21"));
+        tableQuestionFilter.getBindingReferences().add(new BindingReference("ref-2-2", "VAR22"));
         tableQuestion.setComponentFilter(tableQuestionFilter);
-        enoIndex.put(tableQuestion.getId(), tableQuestion);
+        enoIndex.put(tableQuestion);
         enoQuestionnaire.setIndex(enoIndex);
 
         lunaticQuestionnaire = new Questionnaire();
 
         input = new Input();
         input.setComponentType(ComponentTypeEnum.INPUT);
-        input.setId(textQuestion.getId());
+        input.setId("input-id");
+        input.setResponse(new ResponseType());
+        input.getResponse().setName("INPUT_QUESTION");
 
         table = new Table();
         table.setComponentType(ComponentTypeEnum.TABLE);
-        table.setId(tableQuestion.getId());
+        table.setId("table-id");
+        table.getBodyLines().add(new BodyLine());
+        BodyCell tableCell1 = new BodyCell();
+        tableCell1.setResponse(new ResponseType());
+        tableCell1.getResponse().setName("TABLE_QUESTION_1");
+        BodyCell tableCell2 = new BodyCell();
+        tableCell2.setResponse(new ResponseType());
+        tableCell2.getResponse().setName("TABLE_QUESTION_2");
+        table.getBodyLines().getFirst().getBodyCells().add(tableCell1);
+        table.getBodyLines().getFirst().getBodyCells().add(tableCell2);
 
         ConditionFilterType inputConditionFilterType = new ConditionFilterType();
         inputConditionFilterType.setType(LabelTypeEnum.VTL);
-        inputConditionFilterType.setValue(textQuestionFilter.getValue());
+        inputConditionFilterType.setValue("VAR1 < 10");
         input.setConditionFilter(inputConditionFilterType);
 
         ConditionFilterType tableConditionFilterType = new ConditionFilterType();
         tableConditionFilterType.setType(LabelTypeEnum.VTL);
-        tableConditionFilterType.setValue("count(CALCULATED_VARIABLE_2) + NUMERIC_USED_FOR_CALCULATED_VARIABLE_2 < 4");
+        tableConditionFilterType.setValue("VAR21 + VAR22 < 100");
         table.setConditionFilter(tableConditionFilterType);
-/*
-        VariableGroup variableGroup = new VariableGroup();
-        Variable inputVariable = new CalculatedVariable();
-        inputVariable.setId(textQuestion.getId());
-        variableGroup.getVariables().add(inputVariable);
-*/
-        processing = new LunaticFilterResult(enoQuestionnaire, new ShapefromAttributeRetrievalReturnVariableNameInVariable());
+
+        inputVariable = new CollectedVariableType();
+        inputVariable.setName("INPUT_QUESTION");
+
+        tableVariable1 = new CollectedVariableType();
+        tableVariable1.setName("TABLE_QUESTION_1");
+
+        tableVariable2 = new CollectedVariableType();
+        tableVariable2.setName("TABLE_QUESTION_2");
+
+        processing = new LunaticFilterResult(enoQuestionnaire);
     }
 
     @Test
@@ -92,68 +95,117 @@ class LunaticFilterResultTest {
         table.setConditionFilter(null);
         lunaticQuestionnaire.getComponents().addAll(List.of(input, table));
 
+        inputVariable.setDimension(VariableDimension.SCALAR);
+        lunaticQuestionnaire.getVariables().add(inputVariable);
+        tableVariable1.setDimension(VariableDimension.SCALAR);
+        lunaticQuestionnaire.getVariables().add(tableVariable1);
+        tableVariable2.setDimension(VariableDimension.SCALAR);
+        lunaticQuestionnaire.getVariables().add(tableVariable2);
+
         processing.apply(lunaticQuestionnaire);
-        assertEquals(0, lunaticQuestionnaire.getVariables().size());
+
+        assertEquals(3, lunaticQuestionnaire.getVariables().size());
+        lunaticQuestionnaire.getVariables().forEach(variable ->
+                assertFalse(variable.getName().startsWith("FILTER_RESULT_")));
     }
 
     @Test
     void whenApplyingShouldProcessSimpleComponentsWithFilter() {
         lunaticQuestionnaire.getComponents().add(input);
 
+        inputVariable.setDimension(VariableDimension.SCALAR);
+        lunaticQuestionnaire.getVariables().add(inputVariable);
+
         processing.apply(lunaticQuestionnaire);
-        assertEquals(1, lunaticQuestionnaire.getVariables().size());
-        VariableType variable = lunaticQuestionnaire.getVariables().getFirst();
-        testInputCalculatedVariable(variable);
+
+        assertEquals(2, lunaticQuestionnaire.getVariables().size());
+        VariableType inputFilterResult = lunaticQuestionnaire.getVariables().get(1);
+        testInputFilterResult(inputFilterResult);
+
+        assertEquals(VariableDimension.SCALAR, inputFilterResult.getDimension());
+        assertNull(inputFilterResult.getIterationReference());
     }
 
     @Test
     void whenApplyingShouldProcessComplexComponentsWithFilter() {
         lunaticQuestionnaire.getComponents().add(table);
 
-        enoQuestionnaire.getVariableGroups().add(variableGroup);
+        tableVariable1.setDimension(VariableDimension.SCALAR);
+        lunaticQuestionnaire.getVariables().add(tableVariable1);
+        tableVariable2.setDimension(VariableDimension.SCALAR);
+        lunaticQuestionnaire.getVariables().add(tableVariable2);
 
         processing.apply(lunaticQuestionnaire);
-        assertEquals(1, lunaticQuestionnaire.getVariables().size());
-        VariableType variable = lunaticQuestionnaire.getVariables().getFirst();
-        testTableCalculatedVariable(variable);
+
+        assertEquals(4, lunaticQuestionnaire.getVariables().size());
+        VariableType tableFilterResult1 = lunaticQuestionnaire.getVariables().get(2);
+        VariableType tableFilterResult2 = lunaticQuestionnaire.getVariables().get(3);
+        testTableFilterResult(tableFilterResult1, "TABLE_QUESTION_1");
+        testTableFilterResult(tableFilterResult2, "TABLE_QUESTION_2");
+
+        assertEquals(VariableDimension.SCALAR, tableFilterResult1.getDimension());
+        assertNull(tableFilterResult1.getIterationReference());
+        assertEquals(VariableDimension.SCALAR, tableFilterResult2.getDimension());
+        assertNull(tableFilterResult2.getIterationReference());
     }
 
     @Test
     void whenApplyingShouldProcessNestingComponents() {
         Loop loop = new Loop();
+        loop.setId("loop-id");
         loop.setComponentType(ComponentTypeEnum.LOOP);
         loop.getComponents().add(input);
 
         PairwiseLinks pairwise = new PairwiseLinks();
+        pairwise.setId("pairwise-id");
         pairwise.setComponentType(ComponentTypeEnum.PAIRWISE_LINKS);
         pairwise.getComponents().add(table);
+        // A table in a pairwise should not happen in practice, but the logic is the same
+
+        inputVariable.setDimension(VariableDimension.ARRAY);
+        inputVariable.setIterationReference("loop-id");
+        lunaticQuestionnaire.getVariables().add(inputVariable);
+        tableVariable1.setDimension(VariableDimension.DOUBLE_ARRAY);
+        tableVariable1.setIterationReference("pairwise-id");
+        lunaticQuestionnaire.getVariables().add(tableVariable1);
+        tableVariable2.setDimension(VariableDimension.DOUBLE_ARRAY);
+        tableVariable2.setIterationReference("pairwise-id");
+        lunaticQuestionnaire.getVariables().add(tableVariable2);
 
         lunaticQuestionnaire.getComponents().addAll(List.of(loop, pairwise));
 
         processing.apply(lunaticQuestionnaire);
-        assertEquals(2, lunaticQuestionnaire.getVariables().size());
-        testInputCalculatedVariable(lunaticQuestionnaire.getVariables().get(0));
-        testTableCalculatedVariable(lunaticQuestionnaire.getVariables().get(1));
+
+        assertEquals(6, lunaticQuestionnaire.getVariables().size());
+        VariableType inputFilterResult = lunaticQuestionnaire.getVariables().get(3);
+        VariableType tableFilterResult1 = lunaticQuestionnaire.getVariables().get(4);
+        VariableType tableFilterResult2 = lunaticQuestionnaire.getVariables().get(5);
+        testInputFilterResult(inputFilterResult);
+        testTableFilterResult(tableFilterResult1, "TABLE_QUESTION_1");
+        testTableFilterResult(tableFilterResult2, "TABLE_QUESTION_2");
+
+        assertEquals(VariableDimension.ARRAY, inputFilterResult.getDimension());
+        assertEquals("loop-id", inputFilterResult.getIterationReference());
+        assertEquals(VariableDimension.DOUBLE_ARRAY, tableFilterResult1.getDimension());
+        assertEquals("pairwise-id", tableFilterResult1.getIterationReference());
+        assertEquals(VariableDimension.DOUBLE_ARRAY, tableFilterResult2.getDimension());
+        assertEquals("pairwise-id", tableFilterResult2.getIterationReference());
     }
 
-    private void testInputCalculatedVariable(VariableType variable) {
-        assertEquals("FILTER_RESULT_" + textQuestion.getName(), variable.getName());
+    private void testInputFilterResult(VariableType variable) {
+        assertEquals("FILTER_RESULT_INPUT_QUESTION", variable.getName());
         assertEquals(VariableTypeEnum.CALCULATED, variable.getVariableType());
-        CalculatedVariableType calculatedVariable = assertInstanceOf(CalculatedVariableType.class, variable);
-        assertEquals(1, calculatedVariable.getBindingDependencies().size());
-        assertTrue(calculatedVariable.getBindingDependencies().contains("CALCULATED_VARIABLE"));
-        assertEquals("input-name", calculatedVariable.getShapeFrom());
-        assertEquals("count(CALCULATED_VARIABLE) < 2", calculatedVariable.getExpression().getValue());
+        CalculatedVariableType filterResultVariable = assertInstanceOf(CalculatedVariableType.class, variable);
+        assertEquals(List.of("VAR1"), filterResultVariable.getBindingDependencies());
+        assertEquals("VAR1 < 10", filterResultVariable.getExpression().getValue());
     }
 
-    private void testTableCalculatedVariable(VariableType variable) {
-        assertEquals("FILTER_RESULT_" + tableQuestion.getName(), variable.getName());
+    private void testTableFilterResult(VariableType variable, String responseName) {
+        assertEquals("FILTER_RESULT_" + responseName, variable.getName());
         assertEquals(VariableTypeEnum.CALCULATED, variable.getVariableType());
-        CalculatedVariableType calculatedVariable = assertInstanceOf(CalculatedVariableType.class, variable);
-        assertEquals(2, calculatedVariable.getBindingDependencies().size());
-        assertTrue(calculatedVariable.getBindingDependencies().contains("CALCULATED_VARIABLE_2"));
-        assertTrue(calculatedVariable.getBindingDependencies().contains("NUMERIC_USED_FOR_CALCULATED_VARIABLE_2"));
-        assertEquals("table-name", calculatedVariable.getShapeFrom());
-        assertEquals("count(CALCULATED_VARIABLE_2) + NUMERIC_USED_FOR_CALCULATED_VARIABLE_2 < 4", calculatedVariable.getExpression().getValue());
+        CalculatedVariableType filterResultVariable = assertInstanceOf(CalculatedVariableType.class, variable);
+        assertEquals(List.of("VAR21", "VAR22"), filterResultVariable.getBindingDependencies());
+        assertEquals("VAR21 + VAR22 < 100", filterResultVariable.getExpression().getValue());
     }
+
 }
