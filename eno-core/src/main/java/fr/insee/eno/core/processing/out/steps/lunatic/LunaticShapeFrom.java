c644d13e7693c138391ca7ba29354f0c43d0580c
Nicolas Senave <nicolas.senave@gmail.com>
2024-08-12 17:15:32 +0200

    docs: javadoc on filter result and shape from processing

--- eno-core/src/main/java/fr/insee/eno/core/processing/out/steps/lunatic/LunaticShapeFrom.java
+++ eno-core/src/main/java/fr/insee/eno/core/processing/out/steps/lunatic/LunaticShapeFrom.java
@@ -10,10 +10,18 @@ import fr.insee.lunatic.model.flat.variable.CalculatedVariableType;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Processing step to set the "shape from" property of Lunatic calculated variables.
+ * Needs to be called <b>after</b> the "dimension" processing, since it uses the "iteration reference" property.
+ */
 public class LunaticShapeFrom implements ProcessingStep<Questionnaire> {
 
     private Questionnaire lunaticQuestionnaire;
 
+    /**
+     * Sets the "shape from" property on calculated variables of the current questionaire.
+     * @param lunaticQuestionnaire A Lunatic questionnaire.
+     */
     @Override
     public void apply(Questionnaire lunaticQuestionnaire) {
         this.lunaticQuestionnaire = lunaticQuestionnaire;
