a9f038a5d3aa8433caf4922e5fc3b88fb3d0c7ce
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2024-08-22 13:41:18 +0200

    build: switch to gradle kotlin dsl (#1098)
    * build: migrate to kotlin dsl
    
    * ci: refactor jacoco and sonar setup
    
    * chore: refactor sonar properties in build file
    
    Makes -1 file in the root directory
    
    * chore: bump version

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+import org.sonarqube.gradle.SonarTask
+
+plugins {
+    id("org.springframework.boot") version "3.3.1" apply false
+    id("io.spring.dependency-management") version "1.1.6" apply false
+    id("application")
+    id("jacoco-report-aggregation")
+    id("org.sonarqube") version "5.1.0.4882"
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+allprojects {
+    group = "fr.insee.eno"
+    version = "3.26.2-SNAPSHOT"
+}
+
+subprojects {
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+            mavenContent {
+                snapshotsOnly()
+            }
+        }
+        mavenLocal()
+    }
+}
+
+sonar {
+    properties {
+        // The Jacoco coverage report is aggreated in the eno-ws module
+        val codeCoveragePath = "$projectDir/eno-ws/build/reports/jacoco/testCodeCoverageReport/testCodeCoverageReport.xml"
+        println("Aggregated code coverage report location:$codeCoveragePath")
+        property("sonar.coverage.jacoco.xmlReportPaths", codeCoveragePath)
+    }
+}
+
+tasks.named<SonarTask>("sonar") {
+    dependsOn(tasks.named("testCodeCoverageReport"))
+}
+
+tasks.register("printVersion") {
+    group = "versioning"
+    description = "Prints the project version"
+    doLast {
+        println(project.version)
+    }
+}
