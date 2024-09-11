a8489b13bb199df7b73392f1de3cae3445fed738
Nicolas Senave <nicolas.senave@gmail.com>
2024-09-11 08:54:48 +0200

    test: pogues mapping on real questionnaires

--- eno-core/src/test/java/fr/insee/eno/core/PoguesToLunaticTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/PoguesToLunaticTest.java
@@ -5,6 +5,8 @@ import fr.insee.eno.core.parameter.EnoParameters;
 import fr.insee.eno.core.parameter.Format;
 import fr.insee.lunatic.model.flat.Questionnaire;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.ByteArrayInputStream;
 
@@ -22,6 +24,20 @@ class PoguesToLunaticTest {
                 EnoParameters.of(EnoParameters.Context.HOUSEHOLD, EnoParameters.ModeParameter.PROCESS, Format.LUNATIC));
         //
         assertEquals("foo-id", lunaticQuestionnaire.getId());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "integration/pogues/pogues-simple.json,lmyoceix",
+            "functional/pogues/pogues-l20g2ba7.json,l20g2ba7",
+    })
+    void testIdMapping(String relativePath, String expectedId) throws PoguesDeserializationException {
+        //
+        Questionnaire lunaticQuestionnaire = PoguesToLunatic.transform(
+                this.getClass().getClassLoader().getResourceAsStream(relativePath),
+                EnoParameters.of(EnoParameters.Context.HOUSEHOLD, EnoParameters.ModeParameter.PROCESS, Format.LUNATIC));
+        //
+        assertEquals(expectedId, lunaticQuestionnaire.getId());
     }
 
 }
