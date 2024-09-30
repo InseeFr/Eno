3b76acfd0bd3408260e2674775cff1a427b70ae1
Nicolas Senave <nicolas.senave@gmail.com>
2024-09-30 14:18:40 +0200

    fix: restrict to get requests for swagger redirection

--- eno-ws/src/main/java/fr/insee/eno/ws/controller/HomeController.java
+++ eno-ws/src/main/java/fr/insee/eno/ws/controller/HomeController.java
@@ -1,12 +1,12 @@
 package fr.insee.eno.ws.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String redirectToSwaggerUI() {
 		return "redirect:/swagger-ui.html";
 	}
