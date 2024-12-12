69f2304436eda8370ef2db6e4bd69ac1a3c9bfc6
Nicolas Senave <nicolas.senave@gmail.com>
2024-12-12 17:50:44 +0100

    test(dynamic unit): add test integration test in ddi step

--- eno-core/src/test/java/fr/insee/eno/core/processing/in/steps/ddi/DDIMoveUnitInQuestionsTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/processing/in/steps/ddi/DDIMoveUnitInQuestionsTest.java
@@ -1,17 +1,25 @@
 package fr.insee.eno.core.processing.in.steps.ddi;
 
+import fr.insee.eno.core.exceptions.business.DDIParsingException;
+import fr.insee.eno.core.mappers.DDIMapper;
 import fr.insee.eno.core.model.EnoQuestionnaire;
 import fr.insee.eno.core.model.navigation.Binding;
 import fr.insee.eno.core.model.question.DynamicTableQuestion;
 import fr.insee.eno.core.model.question.NumericQuestion;
+import fr.insee.eno.core.model.question.Question;
 import fr.insee.eno.core.model.question.TableQuestion;
 import fr.insee.eno.core.model.question.table.NumericCell;
 import fr.insee.eno.core.model.variable.CollectedVariable;
+import fr.insee.eno.core.model.variable.Variable;
 import fr.insee.eno.core.reference.EnoIndex;
+import fr.insee.eno.core.serialize.DDIDeserializer;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class DDIMoveUnitInQuestionsTest {
 
@@ -103,6 +111,36 @@ class DDIMoveUnitInQuestionsTest {
         //
         assertEquals("%", numericCell.getUnit().getValue());
         assertEquals("%", numericCell.getUnit().getValue());
+    }
+
+    @Test
+    void integrationtest() throws DDIParsingException {
+        // Given
+        EnoQuestionnaire enoQuestionnaire = new EnoQuestionnaire();
+        DDIMapper ddiMapper = new DDIMapper();
+        ddiMapper.mapDDI(
+                DDIDeserializer.deserialize(this.getClass().getClassLoader().getResourceAsStream(
+                        "integration/ddi/ddi-dynamic-unit.xml")),
+                enoQuestionnaire);
+        EnoIndex enoIndex = enoQuestionnaire.getIndex();
+
+        // When
+        new DDIMoveUnitInQuestions(enoIndex).apply(enoQuestionnaire);
+
+        // Then
+        assertEquals("€", ((NumericQuestion) enoQuestionnaire.getSingleResponseQuestions().get(0)).getUnit().getValue());
+        assertTrue(((NumericQuestion) enoQuestionnaire.getSingleResponseQuestions().get(2)).getUnit().getValue().contains("¤"));
+
+        TableQuestion tableQuestion = (TableQuestion) enoQuestionnaire.getMultipleResponseQuestions().get(0);
+        DynamicTableQuestion dynamicTableQuestion = (DynamicTableQuestion) enoQuestionnaire.getMultipleResponseQuestions().get(1);
+
+        assertEquals("%", ((NumericCell) tableQuestion.getResponseCells().get(0)).getUnit().getValue());
+        assertEquals("%", ((NumericCell) tableQuestion.getResponseCells().get(1)).getUnit().getValue());
+        assertTrue(((NumericCell) tableQuestion.getResponseCells().get(2)).getUnit().getValue().contains("¤"));
+        assertTrue(((NumericCell) tableQuestion.getResponseCells().get(3)).getUnit().getValue().contains("¤"));
+
+        assertEquals("€", ((NumericCell) dynamicTableQuestion.getResponseCells().get(0)).getUnit().getValue());
+        assertTrue(((NumericCell) dynamicTableQuestion.getResponseCells().get(1)).getUnit().getValue().contains("¤"));
     }
 
 }
