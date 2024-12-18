8ad732a2eb1b2c8814e4bcd578cfbe0f05fc9592
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2024-12-18 10:05:57 +0100

    feat(ddi to lunatic): dynamic units (#1175)
    * test: add test questionnaire
    
    * feat(dynamic unit): ddi mapping
    
    * test: unit on variables mapped from ddi
    
    * feat(dynamic unit): update ddi insert unit in questions step
    
    * test: add test integration test in ddi step
    
    * feat(dynamic unit): finish mapping and test lunatic output
    
    * fix: input number description with unit object
    
    * fix: set unit label type to 'vtl'
    
    * fix(dynamic unit): bump pogues-model version
    
    * chore: release version 3.31.0

--- eno-core/src/test/java/fr/insee/eno/core/mapping/out/lunatic/NumberQuestionTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/mapping/out/lunatic/NumberQuestionTest.java
@@ -1,12 +1,16 @@
 package fr.insee.eno.core.mapping.out.lunatic;
 
+import fr.insee.eno.core.DDIToEno;
 import fr.insee.eno.core.DDIToLunatic;
 import fr.insee.eno.core.exceptions.business.DDIParsingException;
+import fr.insee.eno.core.mappers.LunaticMapper;
+import fr.insee.eno.core.model.EnoQuestionnaire;
 import fr.insee.eno.core.parameter.EnoParameters;
 import fr.insee.eno.core.parameter.Format;
-import fr.insee.lunatic.model.flat.ComponentTypeEnum;
-import fr.insee.lunatic.model.flat.InputNumber;
-import fr.insee.lunatic.model.flat.Questionnaire;
+import fr.insee.eno.core.processing.out.steps.lunatic.LunaticEditLabelTypes;
+import fr.insee.eno.core.processing.out.steps.lunatic.LunaticSortComponents;
+import fr.insee.eno.core.processing.out.steps.lunatic.table.LunaticTableProcessing;
+import fr.insee.lunatic.model.flat.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -36,10 +40,45 @@ class NumberQuestionTest {
                 .findAny();
         // assertions
         assertEquals(3, inputNumbersNoUnit.size());
-        inputNumbersNoUnit.forEach(inputNumber -> assertNull(inputNumber.getUnit()));
+        inputNumbersNoUnit.forEach(inputNumber -> assertNull(inputNumber.getUnitLabel()));
         assertTrue(inputNumberWithUnit.isPresent());
-        assertNotNull(inputNumberWithUnit.get().getUnit());
-        assertEquals("kg", inputNumberWithUnit.get().getUnit());
+        assertNotNull(inputNumberWithUnit.get().getUnitLabel());
+        assertEquals("\"kg\"", inputNumberWithUnit.get().getUnitLabel().getValue());
+    }
+
+    @Test
+    void integrationTest_dynamicUnit() throws DDIParsingException {
+        // Given + When
+        EnoQuestionnaire enoQuestionnaire = DDIToEno.transform(
+                this.getClass().getClassLoader().getResourceAsStream("integration/ddi/ddi-dynamic-unit.xml"),
+                EnoParameters.of(EnoParameters.Context.DEFAULT, EnoParameters.ModeParameter.CAWI));
+        Questionnaire lunaticQuestionnaire = new Questionnaire();
+        new LunaticMapper().mapQuestionnaire(enoQuestionnaire, lunaticQuestionnaire);
+        new LunaticSortComponents(enoQuestionnaire).apply(lunaticQuestionnaire);
+        new LunaticTableProcessing(enoQuestionnaire).apply(lunaticQuestionnaire);
+        new LunaticEditLabelTypes().apply(lunaticQuestionnaire); // to set unit type to VTL
+
+        // Then
+        InputNumber inputNumber1 = (InputNumber) lunaticQuestionnaire.getComponents().get(1);
+        InputNumber inputNumber2 = (InputNumber) lunaticQuestionnaire.getComponents().get(3);
+        Table table = (Table) lunaticQuestionnaire.getComponents().get(4);
+        RosterForLoop rosterForLoop = (RosterForLoop) lunaticQuestionnaire.getComponents().get(5);
+
+        testUnitContent("\"€\"", inputNumber1.getUnitLabel());
+        testUnitContent("WHICH_UNIT", inputNumber2.getUnitLabel());
+
+        // Reminder: in Lunatic tables there is a left column so responses cells start at column of index 1
+        testUnitContent("\"%\"", table.getBodyLines().get(0).getBodyCells().get(1).getUnitLabel());
+        testUnitContent("\"%\"", table.getBodyLines().get(1).getBodyCells().get(1).getUnitLabel());
+        testUnitContent("WHICH_UNIT", table.getBodyLines().get(0).getBodyCells().get(2).getUnitLabel());
+        testUnitContent("WHICH_UNIT", table.getBodyLines().get(1).getBodyCells().get(2).getUnitLabel());
+
+        testUnitContent("\"€\"", rosterForLoop.getComponents().get(0).getUnitLabel());
+        testUnitContent("WHICH_UNIT", rosterForLoop.getComponents().get(1).getUnitLabel());
+    }
+    private void testUnitContent(String expectedValue, LabelType unitLabel) {
+        assertEquals(expectedValue, unitLabel.getValue().trim());
+        assertEquals(LabelTypeEnum.VTL, unitLabel.getType());
     }
 
 }
---
+++ eno-core/src/test/java/fr/insee/eno/core/processing/in/steps/ddi/DDIMoveUnitInQuestionsTest.java
@@ -0,0 +1,140 @@
+package fr.insee.eno.core.processing.in.steps.ddi;
+
+import fr.insee.eno.core.exceptions.business.DDIParsingException;
+import fr.insee.eno.core.mappers.DDIMapper;
+import fr.insee.eno.core.model.EnoQuestionnaire;
+import fr.insee.eno.core.model.navigation.Binding;
+import fr.insee.eno.core.model.question.DynamicTableQuestion;
+import fr.insee.eno.core.model.question.NumericQuestion;
+import fr.insee.eno.core.model.question.TableQuestion;
+import fr.insee.eno.core.model.question.table.NumericCell;
+import fr.insee.eno.core.model.variable.CollectedVariable;
+import fr.insee.eno.core.reference.EnoIndex;
+import fr.insee.eno.core.serialize.DDIDeserializer;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DDIMoveUnitInQuestionsTest {
+
+    @Test
+    void numericQuestionNoUnit() {
+        //
+        EnoQuestionnaire enoQuestionnaire = new EnoQuestionnaire();
+        CollectedVariable variable = new CollectedVariable();
+        variable.setQuestionReference("numeric-question-id");
+        enoQuestionnaire.getVariables().add(variable);
+        EnoIndex enoIndex = new EnoIndex();
+        NumericQuestion numericQuestion = new NumericQuestion();
+        numericQuestion.setId("numeric-question-id");
+        enoIndex.put(numericQuestion);
+        //
+        new DDIMoveUnitInQuestions(enoIndex).apply(enoQuestionnaire);
+        //
+        assertNull(numericQuestion.getUnit());
+    }
+
+    @Test
+    void numericQuestionWithUnit() {
+        //
+        EnoQuestionnaire enoQuestionnaire = new EnoQuestionnaire();
+        CollectedVariable variable = new CollectedVariable();
+        variable.setUnit("%");
+        variable.setQuestionReference("numeric-question-id");
+        enoQuestionnaire.getVariables().add(variable);
+        EnoIndex enoIndex = new EnoIndex();
+        NumericQuestion numericQuestion = new NumericQuestion();
+        numericQuestion.setId("numeric-question-id");
+        enoIndex.put(numericQuestion);
+        //
+        new DDIMoveUnitInQuestions(enoIndex).apply(enoQuestionnaire);
+        //
+        assertEquals("%", numericQuestion.getUnit().getValue());
+        assertEquals("%", numericQuestion.getUnit().getValue());
+    }
+
+    @Test
+    void numericCell_table() {
+        //
+        EnoQuestionnaire enoQuestionnaire = new EnoQuestionnaire();
+        CollectedVariable variable = new CollectedVariable();
+        variable.setUnit("%");
+        variable.setQuestionReference("table-question-id");
+        variable.setReference("variable-ref");
+        enoQuestionnaire.getVariables().add(variable);
+        EnoIndex enoIndex = new EnoIndex();
+        TableQuestion tableQuestion = new TableQuestion();
+        tableQuestion.setId("table-question-id");
+        Binding binding = new Binding();
+        binding.setSourceParameterId("cell-ref");
+        binding.setTargetParameterId("variable-ref");
+        tableQuestion.getBindings().add(binding);
+        NumericCell numericCell = new NumericCell();
+        numericCell.setId("cell-ref");
+        tableQuestion.getResponseCells().add(numericCell);
+        enoIndex.put(tableQuestion);
+        //
+        new DDIMoveUnitInQuestions(enoIndex).apply(enoQuestionnaire);
+        //
+        assertEquals("%", numericCell.getUnit().getValue());
+        assertEquals("%", numericCell.getUnit().getValue());
+    }
+
+    @Test
+    void numericCell_dynamicTable() {
+        //
+        EnoQuestionnaire enoQuestionnaire = new EnoQuestionnaire();
+        CollectedVariable variable = new CollectedVariable();
+        variable.setUnit("%");
+        variable.setQuestionReference("table-question-id");
+        variable.setReference("variable-ref");
+        enoQuestionnaire.getVariables().add(variable);
+        EnoIndex enoIndex = new EnoIndex();
+        DynamicTableQuestion dynamicTableQuestion = new DynamicTableQuestion();
+        dynamicTableQuestion.setId("table-question-id");
+        Binding binding = new Binding();
+        binding.setSourceParameterId("cell-ref");
+        binding.setTargetParameterId("variable-ref");
+        dynamicTableQuestion.getBindings().add(binding);
+        NumericCell numericCell = new NumericCell();
+        numericCell.setId("cell-ref");
+        dynamicTableQuestion.getResponseCells().add(numericCell);
+        enoIndex.put(dynamicTableQuestion);
+        //
+        new DDIMoveUnitInQuestions(enoIndex).apply(enoQuestionnaire);
+        //
+        assertEquals("%", numericCell.getUnit().getValue());
+        assertEquals("%", numericCell.getUnit().getValue());
+    }
+
+    @Test
+    void integrationtest() throws DDIParsingException {
+        // Given
+        EnoQuestionnaire enoQuestionnaire = new EnoQuestionnaire();
+        DDIMapper ddiMapper = new DDIMapper();
+        ddiMapper.mapDDI(
+                DDIDeserializer.deserialize(this.getClass().getClassLoader().getResourceAsStream(
+                        "integration/ddi/ddi-dynamic-unit.xml")),
+                enoQuestionnaire);
+        EnoIndex enoIndex = enoQuestionnaire.getIndex();
+
+        // When
+        new DDIMoveUnitInQuestions(enoIndex).apply(enoQuestionnaire);
+
+        // Then
+        assertEquals("€", ((NumericQuestion) enoQuestionnaire.getSingleResponseQuestions().get(0)).getUnit().getValue());
+        assertTrue(((NumericQuestion) enoQuestionnaire.getSingleResponseQuestions().get(2)).getUnit().getValue().contains("¤"));
+
+        TableQuestion tableQuestion = (TableQuestion) enoQuestionnaire.getMultipleResponseQuestions().get(0);
+        DynamicTableQuestion dynamicTableQuestion = (DynamicTableQuestion) enoQuestionnaire.getMultipleResponseQuestions().get(1);
+
+        assertEquals("%", ((NumericCell) tableQuestion.getResponseCells().get(0)).getUnit().getValue());
+        assertEquals("%", ((NumericCell) tableQuestion.getResponseCells().get(1)).getUnit().getValue());
+        assertTrue(((NumericCell) tableQuestion.getResponseCells().get(2)).getUnit().getValue().contains("¤"));
+        assertTrue(((NumericCell) tableQuestion.getResponseCells().get(3)).getUnit().getValue().contains("¤"));
+
+        assertEquals("€", ((NumericCell) dynamicTableQuestion.getResponseCells().get(0)).getUnit().getValue());
+        assertTrue(((NumericCell) dynamicTableQuestion.getResponseCells().get(1)).getUnit().getValue().contains("¤"));
+    }
+
+}
