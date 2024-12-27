4012e62e9ea8decc0eeccb00506eab2de7740acb
nsenave <nicolas.senave@gmail.com>
2024-12-27 19:08:52 +0100

    chore: remove useless public modifier

--- eno-core/src/test/java/fr/insee/eno/core/mapping/in/pogues/CodeListTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/mapping/in/pogues/CodeListTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class CodeListTest {
+class CodeListTest {
 
     @Test
     void simpleCodeList() {
