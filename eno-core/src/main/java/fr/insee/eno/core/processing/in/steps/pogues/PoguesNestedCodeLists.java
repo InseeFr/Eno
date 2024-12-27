dc43f135c506018237e3d38f4aa36e2c57ead534
nsenave <nicolas.senave@gmail.com>
2024-12-27 16:30:00 +0100

    feat(pogues mapping): processing pipeline canvas

---
+++ eno-core/src/main/java/fr/insee/eno/core/processing/in/steps/pogues/PoguesNestedCodeLists.java
@@ -0,0 +1,13 @@
+package fr.insee.eno.core.processing.in.steps.pogues;
+
+import fr.insee.eno.core.model.EnoQuestionnaire;
+import fr.insee.eno.core.processing.ProcessingStep;
+
+public class PoguesNestedCodeLists implements ProcessingStep<EnoQuestionnaire> {
+
+    @Override
+    public void apply(EnoQuestionnaire processedObject) {
+
+    }
+
+}
