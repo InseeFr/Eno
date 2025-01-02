f922e87f39efc8470cc8f527e07cdfe3946b7780
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2025-01-02 17:20:20 +0100

    feat(pogues mapping): multiple response questions conversion (#1192)

--- eno-core/src/main/java/fr/insee/eno/core/converter/PoguesConverter.java
+++ eno-core/src/main/java/fr/insee/eno/core/converter/PoguesConverter.java
@@ -2,8 +2,8 @@ package fr.insee.eno.core.converter;
 
 import fr.insee.eno.core.exceptions.technical.ConversionException;
 import fr.insee.eno.core.model.EnoObject;
+import fr.insee.eno.core.model.EnoQuestionnaire;
 import fr.insee.pogues.model.QuestionType;
-import fr.insee.pogues.model.QuestionTypeEnum;
 import fr.insee.pogues.model.VariableType;
 
 public class PoguesConverter implements InConverter {
@@ -13,8 +13,11 @@ public class PoguesConverter implements InConverter {
         if (poguesObject instanceof VariableType poguesVariable)
             return PoguesVariableConversion.instantiateFrom(poguesVariable);
         if (poguesObject instanceof QuestionType poguesQuestion
-                && QuestionTypeEnum.SIMPLE.equals(poguesQuestion.getQuestionType()))
+                && EnoQuestionnaire.isSingleResponseQuestion(poguesQuestion))
             return PoguesSingleResponseQuestionConversion.instantiateFrom(poguesQuestion);
+        if (poguesObject instanceof QuestionType poguesQuestion
+                && EnoQuestionnaire.isMultipleResponseQuestion(poguesQuestion))
+            return PoguesMultipleChoiceQuestionConverter.instantiateFrom(poguesQuestion);
         throw new ConversionException("Eno conversion for Pogues type " + poguesObject.getClass() + " not implemented.");
     }
 
