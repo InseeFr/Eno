84921bc9ff058d62fc12a02278a7879400b4c51e
Remi Verriez <remi.verriez@insee.fr>
2025-02-27 10:01:21 +0100

    test: ajout de tests contrôles sur les durées

--- eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/control/LunaticDurationControlTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/control/LunaticDurationControlTest.java
@@ -37,7 +37,7 @@ class LunaticDurationControlTest {
         YearMonthValue minValue = new YearMonthValue(1, 6);
         YearMonthValue maxValue = new YearMonthValue(3, 1);
         //
-        String result = generateControlMessage(minValue, maxValue)    ;
+        String result = generateControlMessage(minValue, maxValue);
         //
         String expected = "\"La durée saisie doit être comprise entre 1 an et 6 mois et 3 ans et 1 mois.\"";
         assertEquals(expected, result);
@@ -96,52 +96,25 @@ class LunaticDurationControlTest {
         assertTrue(result.contains(expected), "L'expression ne contient pas l'extraction des minutes attendue");
     }
 
-//    @Test
-//    void testCastExpressionYear() {
-//        //
-//        String responseName = "DURATION_VAR";
-//        //
-//        String result = ""; // TODO
-//        //
-//        String expected = """
-//                cast(substr(DURATION_VAR, 2, instr(VAR_DUREE, "Y") - 2), integer)""";
-//        assertEquals(expected, result);
-//    }
-//
-//    @Test
-//    void testCastExpressionMonth() {
-//        //
-//        String responseName = "DURATION_VAR";
-//        //
-//        String result = ""; // TODO
-//        //
-//        String expected = """
-//                cast(substr(VAR_DUREE, instr(VAR_DUREE, "Y") + 1, instr(VAR_DUREE, "M") - instr(VAR_DUREE, "Y") - 1 ), integer)""";
-//        assertEquals(expected, result);
-//    }
-//
-//    @Test
-//    void testCastExpressionHour() {
-//        //
-//        String responseName = "DURATION_VAR";
-//        //
-//        String result = ""; // TODO
-//        //
-//        String expected = """
-//                cast(substr(DURATION_VAR, 3, instr(VAR_DUREE, "H") - 3), integer)""";
-//        assertEquals(expected, result);
-//    }
-//
-//    @Test
-//    void testCastExpressionMinute() {
-//        //
-//        String responseName = "DURATION_VAR";
-//        //
-//        String result = ""; // TODO
-//        //
-//        String expected = """
-//                cast(substr(VAR_DUREE, instr(VAR_DUREE, "H") + 1, instr(VAR_DUREE, "M") - instr(VAR_DUREE, "H") - 1 ), integer)""";
-//        assertEquals(expected, result);
-//    }
+    @Test
+    void testCastExpressionYearAndMonth() {
+        String responseName = "VAR_DUREE";
+        String result = generateControlExpression(new YearMonthValue(1, 6), new YearMonthValue(10, 11));
+        String expectedYear = String.format("cast(substr(%s, 2, instr(%s, \"Y\") - 2), integer)", responseName, responseName);
+        String expectedMonth = String.format("cast(substr(%s, instr(%s, \"Y\") + 1, instr(%s, \"M\") - instr(%s, \"Y\") - 1), integer)", responseName, responseName, responseName, responseName);
+        assertTrue(result.contains(expectedYear), "L'expression ne contient pas l'extraction de l'année attendue");
+        assertTrue(result.contains(expectedMonth), "L'expression ne contient pas l'extraction du mois attendue");
+    }
+
+    @Test
+    void testCastExpressionHourAndMinute() {
+        String responseName = "VAR_DUREE";
+        String result = generateControlExpression(new HourMinuteValue(1, 30), new HourMinuteValue(23, 45));
+        String expectedHour = String.format("cast(substr(%s, 3, instr(%s, \"H\") - 3), integer)", responseName, responseName);
+        String expectedMinute = String.format("cast(substr(%s, instr(%s, \"H\") + 1, instr(%s, \"M\") - instr(%s, \"H\") - 1), integer)", responseName, responseName, responseName, responseName);
+        assertTrue(result.contains(expectedHour), "L'expression ne contient pas l'extraction de l'heure attendue");
+        assertTrue(result.contains(expectedMinute), "L'expression ne contient pas l'extraction des minutes attendue");
+    }
 
 }
+
