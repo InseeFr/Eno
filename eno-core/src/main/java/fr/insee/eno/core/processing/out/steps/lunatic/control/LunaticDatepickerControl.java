232a3550b3da883784764251cdc3ef6f860a69ee
Nicolas Senave <nicolas.senave@gmail.com>
2025-03-04 11:14:57 +0100

    fix: initialization for datepicker format controls

--- eno-core/src/main/java/fr/insee/eno/core/processing/out/steps/lunatic/control/LunaticDatepickerControl.java
+++ eno-core/src/main/java/fr/insee/eno/core/processing/out/steps/lunatic/control/LunaticDatepickerControl.java
@@ -10,6 +10,7 @@ import fr.insee.lunatic.model.flat.Datepicker;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,7 +36,7 @@ public class LunaticDatepickerControl implements LunaticFormatControl<Datepicker
         String format = datepicker.getDateFormat();
         String responseName = datepicker.getResponse().getName();
 
-        List<ControlType> controls = datepicker.getControls();
+        List<ControlType> controls = new ArrayList<>();
 
         Optional<ControlType> controlYearFormat = generateDatepickerYearControl(id, format, responseName);
         Optional<ControlType> controlBounds = getFormatControlFromDatepickerAttributes(id, minValue, maxValue, format, responseName);
