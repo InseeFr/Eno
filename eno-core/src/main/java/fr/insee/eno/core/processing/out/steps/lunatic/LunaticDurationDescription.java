323a938d76df11122c08d5a90b664940d58b7286
Remi Verriez <remi.verriez@insee.fr>
2025-03-05 17:06:24 +0100

    feat: retrait des  dans les descriptions

--- eno-core/src/main/java/fr/insee/eno/core/processing/out/steps/lunatic/LunaticDurationDescription.java
+++ eno-core/src/main/java/fr/insee/eno/core/processing/out/steps/lunatic/LunaticDurationDescription.java
@@ -56,9 +56,9 @@ public class LunaticDurationDescription implements ProcessingStep<Questionnaire>
         YearMonthValue maxValue = LunaticDurationControl.parseYearMonth(max);
 
         if (minValue.years() == 0 && minValue.months() == 0) {
-            return String.format("\"Jusqu'à %s.\"", formatYearMonth(maxValue));
+            return String.format("Jusqu'à %s.", formatYearMonth(maxValue));
         }
-        return String.format("\"De %s à %s.\"", formatYearMonth(minValue), formatYearMonth(maxValue));
+        return String.format("De %s à %s.", formatYearMonth(minValue), formatYearMonth(maxValue));
     }
 
     private String generateHourMinuteDescription(String min, String max) {
@@ -66,9 +66,9 @@ public class LunaticDurationDescription implements ProcessingStep<Questionnaire>
         HourMinuteValue maxValue = LunaticDurationControl.parseHourMinute(max);
 
         if (minValue.hours() == 0 && minValue.minutes() == 0) {
-            return String.format("\"Jusqu'à %s.\"", formatHourMinute(maxValue));
+            return String.format("Jusqu'à %s.", formatHourMinute(maxValue));
         }
-        return String.format("\"De %s à %s.\"", formatHourMinute(minValue), formatHourMinute(maxValue));
+        return String.format("De %s à %s.", formatHourMinute(minValue), formatHourMinute(maxValue));
     }
 
     String formatYearMonth(YearMonthValue value) {
--- eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/LunaticDurationDescriptionTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/LunaticDurationDescriptionTest.java
@@ -1,8 +1,6 @@
 package fr.insee.eno.core.processing.out.steps.lunatic;
 
-import fr.insee.eno.core.DDIToEno;
 import fr.insee.eno.core.PoguesToEno;
-import fr.insee.eno.core.exceptions.business.DDIParsingException;
 import fr.insee.eno.core.exceptions.business.ParsingException;
 import fr.insee.eno.core.mappers.LunaticMapper;
 import fr.insee.eno.core.model.EnoQuestionnaire;
@@ -29,7 +27,7 @@ class LunaticDurationDescriptionTest {
         processing.apply(lunaticQuestionnaire);
         //
         LabelType description = lunaticQuestionnaire.getComponents().getFirst().getDescription();
-        assertEquals("\"De 1 an et 3 mois à 3 ans et 4 mois.\"", description.getValue());
+        assertEquals("De 1 an et 3 mois à 3 ans et 4 mois.", description.getValue());
         assertEquals(LabelTypeEnum.TXT, description.getType());
     }
 
@@ -47,7 +45,7 @@ class LunaticDurationDescriptionTest {
         processing.apply(lunaticQuestionnaire);
         //
         LabelType description = lunaticQuestionnaire.getComponents().getFirst().getDescription();
-        assertEquals("\"De 3 minutes à 5 heures et 4 minutes.\"", description.getValue());
+        assertEquals("De 3 minutes à 5 heures et 4 minutes.", description.getValue());
         assertEquals(LabelTypeEnum.TXT, description.getType());
     }
 
@@ -65,7 +63,7 @@ class LunaticDurationDescriptionTest {
         processing.apply(lunaticQuestionnaire);
         //
         LabelType description = lunaticQuestionnaire.getComponents().getFirst().getDescription();
-        assertEquals("\"Jusqu'à 3 ans et 4 mois.\"", description.getValue());
+        assertEquals("Jusqu'à 3 ans et 4 mois.", description.getValue());
         assertEquals(LabelTypeEnum.TXT, description.getType());
     }
 
@@ -83,7 +81,7 @@ class LunaticDurationDescriptionTest {
         processing.apply(lunaticQuestionnaire);
         //
         LabelType description = lunaticQuestionnaire.getComponents().getFirst().getDescription();
-        assertEquals("\"Jusqu'à 1 heure et 1 minute.\"", description.getValue());
+        assertEquals("Jusqu'à 1 heure et 1 minute.", description.getValue());
         assertEquals(LabelTypeEnum.TXT, description.getType());
     }
 
@@ -104,9 +102,9 @@ class LunaticDurationDescriptionTest {
         // Then
         Duration duration0 = (Duration) lunaticQuestionnaire.getComponents().get(1);
         Duration duration1 = (Duration) lunaticQuestionnaire.getComponents().get(2);
-        assertEquals("\"Jusqu'à 2 ans et 6 mois.\"",
+        assertEquals("Jusqu'à 2 ans et 6 mois.",
                 duration0.getDescription().getValue());
-        assertEquals("\"Jusqu'à 1 heure et 30 minutes.\"",
+        assertEquals("Jusqu'à 1 heure et 30 minutes.",
                 duration1.getDescription().getValue());
         assertEquals(LabelTypeEnum.TXT, duration0.getDescription().getType());
         assertEquals(LabelTypeEnum.TXT, duration1.getDescription().getType());
