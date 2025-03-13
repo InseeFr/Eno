9e3ec6f44e6c829a54bdf140fba83858ab3cb3e0
RemiVerriez <remi.verriez@insee.fr>
2025-03-13 14:14:30 +0100

    feat(lunatic): format control and description for duration (#1229)

---
+++ eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/LunaticDurationDescriptionTest.java
@@ -0,0 +1,113 @@
+package fr.insee.eno.core.processing.out.steps.lunatic;
+
+import fr.insee.eno.core.PoguesToEno;
+import fr.insee.eno.core.exceptions.business.ParsingException;
+import fr.insee.eno.core.mappers.LunaticMapper;
+import fr.insee.eno.core.model.EnoQuestionnaire;
+import fr.insee.eno.core.parameter.EnoParameters;
+import fr.insee.eno.core.processing.out.steps.lunatic.table.LunaticTableProcessing;
+import fr.insee.lunatic.model.flat.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LunaticDurationDescriptionTest {
+
+    @Test
+    void minAndMaxYearMonth() {
+        //
+        Questionnaire lunaticQuestionnaire = new Questionnaire();
+        Duration duration = new Duration();
+        duration.setFormat(DurationFormat.YEARS_MONTHS);
+        duration.setMin("P1Y3M");
+        duration.setMax("P3Y4M");
+        lunaticQuestionnaire.getComponents().add(duration);
+        //
+        LunaticDurationDescription processing = new LunaticDurationDescription();
+        processing.apply(lunaticQuestionnaire);
+        //
+        LabelType description = lunaticQuestionnaire.getComponents().getFirst().getDescription();
+        assertEquals("De 1 an et 3 mois à 3 ans et 4 mois.", description.getValue());
+        assertEquals(LabelTypeEnum.TXT, description.getType());
+    }
+
+    @Test
+    void minAndMaxHourMinute() {
+        //
+        Questionnaire lunaticQuestionnaire = new Questionnaire();
+        Duration duration = new Duration();
+        duration.setFormat(DurationFormat.HOURS_MINUTES);
+        duration.setMin("PT0H3M");
+        duration.setMax("PT5H4M");
+        lunaticQuestionnaire.getComponents().add(duration);
+        //
+        LunaticDurationDescription processing = new LunaticDurationDescription();
+        processing.apply(lunaticQuestionnaire);
+        //
+        LabelType description = lunaticQuestionnaire.getComponents().getFirst().getDescription();
+        assertEquals("De 3 minutes à 5 heures et 4 minutes.", description.getValue());
+        assertEquals(LabelTypeEnum.TXT, description.getType());
+    }
+
+    @Test
+    void withoutMinYearMonth() {
+        //
+        Questionnaire lunaticQuestionnaire = new Questionnaire();
+        Duration duration = new Duration();
+        duration.setFormat(DurationFormat.YEARS_MONTHS);
+        duration.setMin("P0Y0M");
+        duration.setMax("P3Y4M");
+        lunaticQuestionnaire.getComponents().add(duration);
+        //
+        LunaticDurationDescription processing = new LunaticDurationDescription();
+        processing.apply(lunaticQuestionnaire);
+        //
+        LabelType description = lunaticQuestionnaire.getComponents().getFirst().getDescription();
+        assertEquals("Jusqu'à 3 ans et 4 mois.", description.getValue());
+        assertEquals(LabelTypeEnum.TXT, description.getType());
+    }
+
+    @Test
+    void withoutMinHourMinute() {
+        //
+        Questionnaire lunaticQuestionnaire = new Questionnaire();
+        Duration duration = new Duration();
+        duration.setFormat(DurationFormat.HOURS_MINUTES);
+        duration.setMin("PT0H0M");
+        duration.setMax("PT1H1M");
+        lunaticQuestionnaire.getComponents().add(duration);
+        //
+        LunaticDurationDescription processing = new LunaticDurationDescription();
+        processing.apply(lunaticQuestionnaire);
+        //
+        LabelType description = lunaticQuestionnaire.getComponents().getFirst().getDescription();
+        assertEquals("Jusqu'à 1 heure et 1 minute.", description.getValue());
+        assertEquals(LabelTypeEnum.TXT, description.getType());
+    }
+
+    @Test
+    void integrationTest() throws ParsingException {
+        // Given
+        EnoQuestionnaire enoQuestionnaire = PoguesToEno.fromInputStream(
+                        this.getClass().getClassLoader().getResourceAsStream("integration/pogues/pogues-durations-2.json"))
+                .transform(EnoParameters.of(EnoParameters.Context.DEFAULT, EnoParameters.ModeParameter.CAWI));
+        Questionnaire lunaticQuestionnaire = new Questionnaire();
+        new LunaticMapper().mapQuestionnaire(enoQuestionnaire, lunaticQuestionnaire);
+        new LunaticSortComponents(enoQuestionnaire).apply(lunaticQuestionnaire);
+        new LunaticTableProcessing(enoQuestionnaire).apply(lunaticQuestionnaire);
+
+        // When
+        new LunaticDurationDescription().apply(lunaticQuestionnaire);
+
+        // Then
+        Duration duration0 = (Duration) lunaticQuestionnaire.getComponents().get(1);
+        Duration duration1 = (Duration) lunaticQuestionnaire.getComponents().get(2);
+        assertEquals("Jusqu'à 2 ans et 6 mois.",
+                duration0.getDescription().getValue());
+        assertEquals("Jusqu'à 1 heure et 30 minutes.",
+                duration1.getDescription().getValue());
+        assertEquals(LabelTypeEnum.TXT, duration0.getDescription().getType());
+        assertEquals(LabelTypeEnum.TXT, duration1.getDescription().getType());
+    }
+
+}
---
+++ eno-core/src/test/java/fr/insee/eno/core/processing/out/steps/lunatic/control/LunaticDurationControlTest.java
@@ -0,0 +1,127 @@
+package fr.insee.eno.core.processing.out.steps.lunatic.control;
+
+import fr.insee.eno.core.exceptions.business.RequiredPropertyException;
+import fr.insee.lunatic.model.flat.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static fr.insee.eno.core.processing.out.steps.lunatic.control.LunaticDurationControl.HourMinuteValue;
+import static fr.insee.eno.core.processing.out.steps.lunatic.control.LunaticDurationControl.YearMonthValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class LunaticDurationControlTest {
+
+
+    @Test
+    void durationWithNoMin() {
+        Duration lunaticDuration = new Duration();
+        lunaticDuration.setId("duration-id");
+        lunaticDuration.setMax("P1Y0M");
+        lunaticDuration.setFormat(DurationFormat.YEARS_MONTHS);
+        RequiredPropertyException exception = assertThrows(RequiredPropertyException.class, () ->
+                LunaticDurationControl.generateDurationFormatControl(lunaticDuration));
+        assertEquals("Duration component 'duration-id' has a null min.", exception.getMessage());
+    }
+    @Test
+    void durationWithNoMax() {
+        Duration lunaticDuration = new Duration();
+        lunaticDuration.setId("duration-id");
+        lunaticDuration.setMin("P1Y0M");
+        lunaticDuration.setFormat(DurationFormat.YEARS_MONTHS);
+        RequiredPropertyException exception = assertThrows(RequiredPropertyException.class, () ->
+                LunaticDurationControl.generateDurationFormatControl(lunaticDuration));
+        assertEquals("Duration component 'duration-id' has a null max.", exception.getMessage());
+    }
+    @Test
+    void durationWithNoFormat() {
+        Duration lunaticDuration = new Duration();
+        lunaticDuration.setId("duration-id");
+        lunaticDuration.setMin("P0Y0M");
+        lunaticDuration.setMax("P1Y0M");
+        RequiredPropertyException exception = assertThrows(RequiredPropertyException.class, () ->
+                LunaticDurationControl.generateDurationFormatControl(lunaticDuration));
+        assertEquals("Duration component 'duration-id' has a null format.", exception.getMessage());
+    }
+
+
+    @Test
+    void testParseYearMonth() {
+        String yearMonthString = "P1Y6M";
+        YearMonthValue yearMonthValue = LunaticDurationControl.parseYearMonth(yearMonthString);
+        assertEquals(1, yearMonthValue.years());
+        assertEquals(6, yearMonthValue.months());
+    }
+    @Test
+    void testParseHourMinute() {
+        String hourMinuteString = "PT10H39M";
+        HourMinuteValue hourMinuteValue = LunaticDurationControl.parseHourMinute(hourMinuteString);
+        assertEquals(10, hourMinuteValue.hours());
+        assertEquals(39, hourMinuteValue.minutes());
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "PT10H20M" // Incorrect format (here hours and minutes)
+            ,"P0Y" // minute value is missing
+            ,"PY5M" // year value is missing
+            ,"5Y5M" // "P" is missing
+            ,"10Y" // "P" and minute value are missing
+    })
+    void testParseYearMonth_invalidFormat(String input) {
+        assertThrows(IllegalArgumentException.class, () -> LunaticDurationControl.parseYearMonth(input));
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "P1Y2M" // Incorrect format (here years and months)
+            ,"P10H20M" // should be "PT" not "P"
+            ,"PT1HM" // no value for minutes
+            ,"PTH30M" // no value for hours
+    })
+    void testParseHourMinute_invalidFormat(String input) {
+        assertThrows(IllegalArgumentException.class, () -> LunaticDurationControl.parseHourMinute(input));
+    }
+
+
+    @Test
+    void generateControl_yearMonth() {
+        //
+        Duration lunaticDuration = new Duration();
+        lunaticDuration.setId("duration-id");
+        lunaticDuration.setResponse(new ResponseType());
+        lunaticDuration.getResponse().setName("DURATION_VAR");
+        lunaticDuration.setMin("P0Y3M");
+        lunaticDuration.setMax("P1Y6M");
+        lunaticDuration.setFormat(DurationFormat.YEARS_MONTHS);
+        //
+        ControlType generatedControl = LunaticDurationControl.generateDurationFormatControl(lunaticDuration);
+        //
+        commonPropsTest(generatedControl);
+        // Note: message and expression are already thoroughly tested in dedicated classes
+    }
+    @Test
+    void generateControl_hourMinute() {
+        //
+        Duration lunaticDuration = new Duration();
+        lunaticDuration.setId("duration-id");
+        lunaticDuration.setResponse(new ResponseType());
+        lunaticDuration.getResponse().setName("DURATION_VAR");
+        lunaticDuration.setMin("PT0H15M");
+        lunaticDuration.setMax("PT1H30M");
+        lunaticDuration.setFormat(DurationFormat.HOURS_MINUTES);
+        //
+        ControlType generatedControl = LunaticDurationControl.generateDurationFormatControl(lunaticDuration);
+        //
+        commonPropsTest(generatedControl);
+        // Note: message and expression are already thoroughly tested in dedicated classes
+    }
+    private void commonPropsTest(ControlType generatedControl) {
+        assertEquals("duration-id-format", generatedControl.getId());
+        assertEquals(ControlContextType.SIMPLE, generatedControl.getType());
+        assertEquals(ControlTypeEnum.FORMAT, generatedControl.getTypeOfControl());
+        assertEquals(ControlCriticalityEnum.ERROR, generatedControl.getCriticality());
+        assertEquals(LabelTypeEnum.VTL_MD, generatedControl.getErrorMessage().getType());
+        assertEquals(LabelTypeEnum.VTL, generatedControl.getControl().getType());
+    }
+
+}
