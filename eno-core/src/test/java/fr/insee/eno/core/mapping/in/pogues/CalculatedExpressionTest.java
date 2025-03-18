2f819b329c5efdff7fb33f01809c1a64d89ad1f4
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2025-03-18 09:38:30 +0100

    fix(pogues mapping): ghost variables error (#1245)

--- eno-core/src/test/java/fr/insee/eno/core/mapping/in/pogues/CalculatedExpressionTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/mapping/in/pogues/CalculatedExpressionTest.java
@@ -1,10 +1,15 @@
 package fr.insee.eno.core.mapping.in.pogues;
 
+import fr.insee.eno.core.PoguesDDIToLunatic;
 import fr.insee.eno.core.exceptions.business.IllegalPoguesElementException;
+import fr.insee.eno.core.exceptions.business.ParsingException;
 import fr.insee.eno.core.exceptions.technical.MappingException;
 import fr.insee.eno.core.mappers.PoguesMapper;
 import fr.insee.eno.core.model.EnoQuestionnaire;
 import fr.insee.eno.core.model.variable.CalculatedVariable;
+import fr.insee.eno.core.parameter.EnoParameters;
+import fr.insee.eno.core.parameter.Format;
+import fr.insee.lunatic.model.flat.variable.VariableType;
 import fr.insee.pogues.model.CalculatedVariableType;
 import fr.insee.pogues.model.CollectedVariableType;
 import fr.insee.pogues.model.ExpressionType;
@@ -12,6 +17,7 @@ import fr.insee.pogues.model.Questionnaire;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static fr.insee.eno.core.model.calculated.CalculatedExpression.removeSurroundingDollarSigns;
@@ -50,7 +56,7 @@ class CalculatedExpressionTest {
     }
 
     @Test
-    @Disabled("Exception temporary replaced by a warning")
+    @Disabled("Exception temporary replaced by a warning") // may be deleted, cf. below test that has lots of comments
     void unknownVariableCase() {
 
         // Given
@@ -73,6 +79,37 @@ class CalculatedExpressionTest {
                 mappingException.getCause());
         assertTrue(exception.getMessage().startsWith("Name 'FOO' used in expression:"));
         assertTrue(exception.getMessage().contains(expression));
+    }
+
+    /*
+     * Pogues allows the user to reference variables that doesn't exist within the questionnaire.
+     * The reason is that with the composition feature, the user might want to define calculated variables
+     * in the parent/main questionnaire, that use variables from several child questionnaires ("modules"),
+     * but all child questionnaires are not necessarily imported in the parent/main one...
+     */
+    @Test
+    void ghostVariableReferences_integrationTest() throws ParsingException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+
+        // Given
+        // Note: using Pogues+DDI since it is the current "main" way
+        EnoParameters enoParameters = EnoParameters.of(EnoParameters.Context.HOUSEHOLD, EnoParameters.ModeParameter.CAWI, Format.LUNATIC);
+        PoguesDDIToLunatic poguesDDIToLunatic = PoguesDDIToLunatic.fromInputStreams(
+                classLoader.getResourceAsStream("integration/pogues/pogues-ghost-variables.json"),
+                classLoader.getResourceAsStream("integration/ddi/ddi-ghost-variables.xml"));
+
+        // When
+        fr.insee.lunatic.model.flat.Questionnaire lunaticQuestionnaire = poguesDDIToLunatic.transform(enoParameters);
+
+        // Then:
+        // The questionnaire has 1 question with collected variable "Q1", and 1 calculated "CALCULATED1"
+        // The questionnaire also have VTL expressions that references variable names "GHOST" and "ANOTHER_GHOST"
+        // 1. No exception should be thrown (at least for now).
+        // 2. The former ones must be present, but not the latter.
+        List<String> variableNames = lunaticQuestionnaire.getVariables().stream().map(VariableType::getName).toList();
+        assertTrue(variableNames.containsAll(List.of("Q1", "CALCULATED1")));
+        assertFalse(variableNames.contains("GHOST"));
+        assertFalse(variableNames.contains("ANOTHER_GHOST"));
     }
 
     @Test // dynamic label-like expression
