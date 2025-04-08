0ad464c49faad5a00e2d1b3194d7d57792890185
Nicolas Senave <nicolas.senave@gmail.com>
2025-04-08 16:15:17 +0200

    refactor: pogues get table response methods

--- eno-core/src/main/java/fr/insee/eno/core/utils/PoguesUtils.java
+++ eno-core/src/main/java/fr/insee/eno/core/utils/PoguesUtils.java
@@ -5,12 +5,15 @@ import fr.insee.pogues.model.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
  * Utility class that provide some methods for Pogues-Model objects.
  */
 public class PoguesUtils {
+
+    public static final String POGUES_NO_DATA_ATTRIBUTE = "NoDataByDefinition";
 
     private PoguesUtils() {}
 
@@ -34,28 +37,47 @@ public class PoguesUtils {
                 .filter(QuestionType.class::isInstance).map(QuestionType.class::cast);
     }
 
-    private static List<ResponseType> getPoguesTableResponse(QuestionType tableQuestion, boolean noDataCells){
-        List<String> coordinateOfNoDataCells = tableQuestion.getResponseStructure()
-                .getAttribute().stream()
-                .filter(a ->"NoDataByDefinition".equals(a.getAttributeValue()))
-                .map(AttributeType::getAttributeTarget)
-                .toList();
-        List<MappingType> mappings = tableQuestion.getResponseStructure().getMapping();
-        Map<String, String> indexResponse = new HashMap<>();
-        mappings.forEach(mappingType -> indexResponse.put(mappingType.getMappingSource(), mappingType.getMappingTarget()));
-        return tableQuestion.getResponse().stream()
-                .filter(responseType -> {
-                    String cord = indexResponse.get(responseType.getId());
-                    if(noDataCells) return coordinateOfNoDataCells.contains(cord);
-                    return !coordinateOfNoDataCells.contains(cord);
-                }).toList();
+    /**
+     * Returns the list of Pogues response objects that correspond to a collected cell in the given Pogues table
+     * question.
+     * @param tableQuestion A Pogues table question object.
+     * @return List of Pogues response objects that correspond to a collected cell.
+     */
+    public static List<ResponseType> getPoguesTableResponseCells(QuestionType tableQuestion){
+        return getPoguesTableResponse(tableQuestion, false);
     }
 
+    /**
+     * Returns the list of Pogues response objects that correspond to a no data cell in the given Pogues table
+     * question.
+     * @param tableQuestion A Pogues table question object.
+     * @return List of Pogues response objects that correspond to a no data cell.
+     */
     public static List<ResponseType> getPoguesTableNoDataCells(QuestionType tableQuestion){
        return getPoguesTableResponse(tableQuestion, true);
     }
 
-    public static List<ResponseType> getPoguesTableResponseCells(QuestionType tableQuestion){
-        return getPoguesTableResponse(tableQuestion, false);
+    /** Returns either collected cells response or no data cells response object of the given Pogues table question.
+     * @throws IllegalArgumentException if Pogues question object in not a table. */
+    private static List<ResponseType> getPoguesTableResponse(QuestionType tableQuestion, boolean noDataCells) {
+
+        if (! QuestionTypeEnum.TABLE.equals(tableQuestion.getQuestionType()))
+            throw new IllegalArgumentException("Pogues question " + tableQuestion.getId() + " is not a table.");
+
+        List<String> coordinateOfNoDataCells = tableQuestion.getResponseStructure()
+                .getAttribute().stream()
+                .filter(a -> POGUES_NO_DATA_ATTRIBUTE.equals(a.getAttributeValue()))
+                .map(AttributeType::getAttributeTarget)
+                .toList();
+        Map<String, String> coordinateMap = tableQuestion.getResponseStructure().getMapping().stream()
+                .collect(Collectors.toMap(MappingType::getMappingSource, MappingType::getMappingTarget));
+        return tableQuestion.getResponse().stream()
+                .filter(responseType -> {
+                    String responseCoordinate = coordinateMap.get(responseType.getId());
+                    if (noDataCells)
+                        return coordinateOfNoDataCells.contains(responseCoordinate);
+                    return !coordinateOfNoDataCells.contains(responseCoordinate);
+                }).toList();
     }
+
 }
