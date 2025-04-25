545d5538754454565b7f42da80d045ce1b2f369a
Laurent Caouissin <caouissin.laurent@gmail.com>
2025-04-25 11:05:09 +0200

    chore(deps): bump pogues-model to 1.7.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             version("lunatic-model", "5.0.1")
-            version("pogues-model", "1.7.0")
+            version("pogues-model", "1.7.1")
             library("lunatic-model", "fr.insee.lunatic", "lunatic-model").versionRef("lunatic-model")
             library("pogues-model", "fr.insee.pogues", "pogues-model").versionRef("pogues-model")
         }
