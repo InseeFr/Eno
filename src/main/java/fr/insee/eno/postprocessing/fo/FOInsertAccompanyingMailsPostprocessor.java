b0d949a110f6ed1fa1989b9aa1a8abbf18509673
lailabent <laila.bentoudja@insee.fr>
2025-05-16 13:54:58 +0200

    feat: new accompanying mail templates for Platine (#1258)
    * Add files via upload
    
    * refactor: some clean up in accompagning mail post processing
    
    * fix: all your bases are belong to us
    
    * fix: new accompanying mail template names
    
    * chore: bump version
    
    ---------
    
    Co-authored-by: Nicolas Senave <nicolas.senave@gmail.com>

--- src/main/java/fr/insee/eno/postprocessing/fo/FOInsertAccompanyingMailsPostprocessor.java
+++ src/main/java/fr/insee/eno/postprocessing/fo/FOInsertAccompanyingMailsPostprocessor.java
@@ -13,30 +13,31 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
 /**
- * A PDF post processing
+ * A PDF post-processing
  */
 public class FOInsertAccompanyingMailsPostprocessor implements Postprocessor {
 
 	private static final Logger logger = LoggerFactory.getLogger(FOInsertAccompanyingMailsPostprocessor.class);
 
-	private XslTransformation saxonService = new XslTransformation();
-
-	private static final String styleSheetPath = Constants.TRANSFORMATIONS_ACCOMPANYING_MAILS_FO_4PDF;
+	private final XslTransformation saxonService = new XslTransformation();
 
 	@Override
 	public ByteArrayOutputStream process(InputStream inputStream, byte[] parameters, String surveyName) throws Exception {
-		logger.info(String.format("%s Target : START",toString().toLowerCase()));
+		if (logger.isInfoEnabled()) // (otherwise don't compute the toLowerCase method above)
+			logger.info("{} Target : START", this.toString().toLowerCase());
+
+		String styleSheetPath = Constants.TRANSFORMATIONS_ACCOMPANYING_MAILS_FO_4PDF;
 
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
 		try (InputStream xslIS = Constants.getInputStreamFromPath(styleSheetPath);
-			 inputStream;){
+			 inputStream){
 
 			saxonService.transformFOToStep4FO(inputStream, byteArrayOutputStream, xslIS, surveyName, surveyName, parameters);
 
 		}catch(Exception e) {
-			String errorMessage = String.format("An error was occured during the %s transformation. %s : %s",
-					toString(),
+			String errorMessage = String.format("An error has occurred during the %s transformation. %s : %s",
+					this,
 					e.getMessage(),
 					Utils.getErrorLocation(styleSheetPath,e));
 			logger.error(errorMessage);
