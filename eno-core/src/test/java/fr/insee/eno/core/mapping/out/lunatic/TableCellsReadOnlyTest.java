826b93da345d6e0f05a05097e48dcbc72c59b559
Nicolas Senave <nicolas.senave@gmail.com>
2025-05-21 11:59:00 +0200

    test(readonly cells): pogues and lunatic unit tests

--- eno-core/src/test/java/fr/insee/eno/core/mapping/out/lunatic/TableCellsReadOnlyTest.java
+++ eno-core/src/test/java/fr/insee/eno/core/mapping/out/lunatic/TableCellsReadOnlyTest.java
@@ -2,17 +2,32 @@ package fr.insee.eno.core.mapping.out.lunatic;
 
 import fr.insee.eno.core.PoguesDDIToLunatic;
 import fr.insee.eno.core.exceptions.business.ParsingException;
+import fr.insee.eno.core.mappers.LunaticMapper;
+import fr.insee.eno.core.model.navigation.ComponentFilter;
+import fr.insee.eno.core.model.question.table.ResponseCell;
+import fr.insee.eno.core.model.question.table.TextCell;
 import fr.insee.eno.core.parameter.EnoParameters;
 import fr.insee.eno.core.parameter.Format;
-import fr.insee.lunatic.model.flat.Question;
-import fr.insee.lunatic.model.flat.Questionnaire;
-import fr.insee.lunatic.model.flat.RosterForLoop;
+import fr.insee.lunatic.model.flat.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TableCellsReadOnlyTest {
+
+    @Test
+    void unitTest() {
+        ResponseCell enoCell = new TextCell();
+        enoCell.setComponentReadOnly(new ComponentFilter());
+        enoCell.getComponentReadOnly().setValue("<some expression>");
+        BodyCell lunaticCell = new BodyCell();
+
+        new LunaticMapper().mapEnoObject(enoCell, lunaticCell);
+
+        assertEquals("<some expression>", lunaticCell.getConditionReadOnly().getValue());
+        assertEquals(LabelTypeEnum.VTL, lunaticCell.getConditionReadOnly().getType());
+    }
 
     private final ClassLoader classLoader = this.getClass().getClassLoader();
 
