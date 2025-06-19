011ce595d7e21d0d53e846ae4b2c62d021d3d753
Laurent Caouissin <caouissin.laurent@gmail.com>
2025-06-19 17:44:07 +0200

    chore(deps): bump lunatic-model to 5.6.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            version("lunatic-model", "5.5.0")
+            version("lunatic-model", "5.6.0")
             version("pogues-model", "1.9.3")
             library("lunatic-model", "fr.insee.lunatic", "lunatic-model").versionRef("lunatic-model")
             library("pogues-model", "fr.insee.pogues", "pogues-model").versionRef("pogues-model")
