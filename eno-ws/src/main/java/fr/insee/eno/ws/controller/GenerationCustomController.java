38bbc737454d849dafebf387307037b733d28a21
Verriez Rémi <remi.verriez@insee.fr>
2025-07-07 11:44:06 +0200

    refactor: delete import

--- eno-ws/src/main/java/fr/insee/eno/ws/controller/GenerationCustomController.java
+++ eno-ws/src/main/java/fr/insee/eno/ws/controller/GenerationCustomController.java
@@ -18,7 +18,6 @@ import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
