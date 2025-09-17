74ab9e014eb0e228c0c02ba6c8df3f39249cb51b
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2025-09-17 10:26:55 +0200

    ref: init mono-repo (#1336)

---
+++ eno-ws/src/main/java/fr/insee/eno/App.java
@@ -0,0 +1,13 @@
+package fr.insee.eno;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
