ab8fe709e149c7816c14735c6d9abcfed02e2f77
RemiVerriez <remi.verriez@insee.fr>
2026-02-27 10:33:06 +0100

    test: add tests for optionSource in ucq in cell

---
+++ eno-core/src/test/java/fr/insee/eno/core/mapping/out/lunatic/UniqueChoiceCellTest.java
@@ -0,0 +1,39 @@
+package fr.insee.eno.core.mapping.out.lunatic;
+
+import fr.insee.eno.core.mappers.LunaticMapper;
+import fr.insee.eno.core.model.question.UniqueChoiceQuestion;
+import fr.insee.eno.core.model.question.table.UniqueChoiceCell;
+import fr.insee.lunatic.model.flat.BodyCell;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class UniqueChoiceCellTest {
+
+    @Test
+    void dynamic_ucq_cell_sets_option_source() {
+        UniqueChoiceCell enoCell = new UniqueChoiceCell();
+        enoCell.setDisplayFormat(UniqueChoiceQuestion.DisplayFormat.RADIO);
+        enoCell.setOptionSource("FIRST_NAME");
+
+        BodyCell lunaticCell = new BodyCell();
+
+        new LunaticMapper().mapEnoObject(enoCell, lunaticCell);
+
+        assertEquals("FIRST_NAME", lunaticCell.getOptionSource());
+    }
+
+    @Test
+    void dynamic_ucq_cell_has_no_static_options() {
+        UniqueChoiceCell enoCell = new UniqueChoiceCell();
+        enoCell.setDisplayFormat(UniqueChoiceQuestion.DisplayFormat.RADIO);
+        enoCell.setOptionSource("FIRST_NAME");
+
+        BodyCell lunaticCell = new BodyCell();
+
+        new LunaticMapper().mapEnoObject(enoCell, lunaticCell);
+
+        assertTrue(lunaticCell.getOptions().isEmpty());
+    }
+}
